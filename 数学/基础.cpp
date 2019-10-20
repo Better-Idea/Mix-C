844bb81f851f1216589f9356e15e0419173730fd
王晨 <x4495@outlook.com>
2019-10-20 19:41:24 +0800

    凑合着使用的初等数学：）

---
+++ 数学/基础.cpp
@@ -0,0 +1,41 @@
+﻿#include"基础.h"
+#include<math.h>
+
+命名空间 数学{
+    F64 Pow(F64 底数, F64 指数) {
+        回递 pow(底数, 指数);
+    }
+    F64 Mod(F64 被模数, F64 模数) {
+        回递 fmod(被模数, 模数);
+    }
+    F64 Sin(F64 值) {
+        回递 sin(值);
+    }
+    F64 Cos(F64 值) {
+        回递 cos(值);
+    }
+    F64 Tan(F64 值) {
+        回递 tan(值);
+    }
+    F64 Asin(F64 值) {
+        回递 asin(值);
+    }
+    F64 Acos(F64 值) {
+        回递 acos(值);
+    }
+    F64 Atan(F64 值) {
+        回递 atan(值);
+    }
+    F64 Ln(F64 值) {
+        回递 log(值);
+    }
+    F64 Log2(F64 值) {
+        回递 log2(值);
+    }
+    F64 Log10(F64 值) {
+        回递 log10(值);
+    }
+    F64 Log(F64 底数, F64 真数) {
+        回递 log2(真数) / log2(底数);
+    }
+}
