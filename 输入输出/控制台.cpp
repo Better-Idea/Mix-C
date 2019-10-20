e8cf531c3e0cd8539cca6815f9234b12b0e207b5
王晨 <x4495@outlook.com>
2019-10-20 12:27:28 +0800

    先提供几个基本的控制台功能

---
+++ 输入输出/控制台.cpp
@@ -0,0 +1,35 @@
+﻿#include<Windows.h>
+#ifdef _
+#undef _
+#endif
+#include"控制台.h"
+
+结构体 匿名字段{
+    HANDLE 输出;
+    HANDLE 输入;
+};
+
+#define 句柄 (*(::匿名字段 *)$D.匿名字段)
+
+命名空间 控制台空间{
+    模板<> 数据<>::数据() {
+        句柄.输入 = GetStdHandle(STD_INPUT_HANDLE);
+        句柄.输出 = GetStdHandle(STD_OUTPUT_HANDLE);
+        色彩.前景 = (U08)灰色;
+        色彩.背景 = (U08)黑色;
+        SetConsoleTextAttribute(句柄.输出, 色彩);
+    }
+    模板<> 空 接口<>::设置前景(控制台颜色 & 值) 常量 {
+        $D.色彩.前景 = (U08)值;
+        SetConsoleTextAttribute(句柄.输出, $D.色彩);
+    }
+    模板<> 空 接口<>::设置背景(控制台颜色 & 值) 常量 {
+        $D.色彩.背景 = (U08)值;
+        SetConsoleTextAttribute(句柄.输出, $D.色彩);
+    }
+    模板<> 空 接口<哑元结构>::打印(原始串 串) {
+        WriteConsoleW(句柄.输出, 串->指针, (DWORD)串->长度, NULL, NULL);
+    }
+}
+
+控制台 终端;
