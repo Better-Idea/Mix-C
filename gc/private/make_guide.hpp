d50790ee5edb9df618ee984081b00b0b80b3fbb2
PowerfulCat <x4495@outlook.com>
2020-03-15 20:37:11 +0800

    添加 make_guide 方法，用于在编译期间生成构成环形引用的路径

---
+++ gc/private/make_guide.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include"gc/private/routing.hpp"
+#include"gc/private/collect.hpp"
+
+namespace mixc::inner_gc{
+    template<class root>
+    inline auto make_guide() {
+        if constexpr(is_class<root>){
+            using routing_result = typename inner_gc::routing<root>::result_kvlist;
+            using guide = typename inner_gc::collect<root, routing_result>::result_list;
+            return guide();
+        }
+        else{
+            return tlist<>();
+        }
+    }
+}
