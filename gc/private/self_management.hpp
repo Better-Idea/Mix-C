eba4abc793d76733f4610fce1882926ac743d6b9
PowerfulCat <x4495@outlook.com>
2020-03-15 20:35:08 +0800

    添加 self_managment.hpp，给自动内存管理结构体打上标签，方便 collect 识别

---
+++ gc/private/self_management.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include"define/base_type.hpp"
+
+namespace mixc{
+    struct self_management{
+        template<class guide> uxx routing(void * root);
+        template<class guide> void clear_footmark();
+    };
+}
