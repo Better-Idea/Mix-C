40e02c1c8727b5ddde141c95dfd25b629de2c6a0
PowerfulCat <x4495@outlook.com>
2020-03-15 20:31:30 +0800

    给 atom_swap 添加注释

--- lock/atom_swap.hpp
+++ lock/atom_swap.hpp
@@ -3,6 +3,9 @@
 namespace mixc{
     template<class a>
     a atom_swap(a * left, a right){
+        // function equals to 
+        // r = left[0]
+        // left[0] = right
         a r;
         __atomic_exchange(left, & right, & r, 5);
         return r;
