65b4a95a53bd3c1733bf829646d07c55d7eabc60
PowerfulCat <x4495@outlook.com>
2020-03-18 17:03:57 +0800

    给 tuple 添加说明

--- gc/private/tuple.hpp
+++ gc/private/tuple.hpp
@@ -1,15 +1,42 @@
+/*
+在 GC 组件中，该 tuple 模板作为 GC 内部专用类型，用于组织 GC 的路由
+该模板反射对应结构体的成员变量，根据 member_list 模板参数将对应的结构体布局在该 tuple 中
+示意：如下
+
+struct foo {
+    type_first  first;
+    type_second second;
+    ...
+
+    using member_list = tlist<
+        type_first,
+        type_second,
+        ...
+    >;
+};
+
+tuple<foo::member_list> xx;
+
+我们本来打算使用 C++17 的结构化绑定来实现反射，但它不能绑定是子类的结构：
+    struct foo : bar{
+        ...
+    };
+
+    auto [xx] = foo(); // error
+*/
+
 #pragma once
 #include"define/base_type.hpp"
 #include"gc/private/self_management.hpp"
 #include"gc/private/routing_result.hpp"
-#include"memop/addressof.hpp"
+#include"memop/cast.hpp"
 #include"meta/is_class.hpp"
 #include"meta/is_based_on.hpp"
 #include"meta_seq/tlist.hpp"
 #include"meta_seq/tin.hpp"
 
 namespace mixc::inner_gc{
-    template<class list> struct tuple;
+    template<class member_list> struct tuple;
     template<class first, class ... args>
     struct tuple<tlist<first, args...>>{
     private:
@@ -21,28 +48,29 @@ namespace mixc::inner_gc{
             item(list...), next(list...){
         }
 
+        // 重点 ======================================================================================
+        // 因为在 GC 路由时的状态依赖于路径，所以需要保证 routing 和 clear_footmark 的路由路径是一致的
+        // 这里统一先经过 item 在经过 next
         template<class guide>
         routing_result routing(guide gui){
-            routing_result r = { 0, 0 };
+            routing_result r = { 0 };
             if constexpr (is_class<first>){
                 if constexpr (is_based_on<self_management, first>){
                     if constexpr (tin<guide, first>){
                         // printf("%s | routing\n", typeid(first).name());
                         if (r = item.routing(gui); r.can_arrive_root) {
-                            r.degree_dvalue -= 1;
+                            r.degree_dvalue -= 1; // 如果 item 可以到达根节点，那么该 tuple 就存在 1 条出度
                             // printf("%s | routing io:%lld\n", typeid(first).name(), r.degree_dvalue);
                         }
                     }
                 }
                 else{
-                    using tuplep = tuple<typename first::gc_candidate_list> *;
-                    r = tuplep(addressof(item))->routing(gui);
+                    using member_tuple = tuple<typename first::member_list>;
+                    r = mixc::cast<member_tuple>(item).routing(gui);
                 }
             }
             
-            routing_result n = next.routing(gui);
-            r.can_arrive_root |= n.can_arrive_root;
-            r.degree_dvalue += n.degree_dvalue;
+            r += next.routing(gui);
             return r;
         }
 
@@ -56,8 +84,8 @@ namespace mixc::inner_gc{
                     }
                 }
                 else{
-                    using tuplep = tuple<typename first::gc_candidate_list> *;
-                    tuplep(addressof(item))->clear_footmark(gui, root);
+                    using member_tuple = tuple<typename first::member_list>;
+                    mixc::cast<member_tuple>(item).clear_footmark(gui, root);
                 }
             }
             next.clear_footmark(gui, root);
@@ -68,7 +96,7 @@ namespace mixc::inner_gc{
     struct tuple<tlist<>>{
         template<class guide>
         constexpr routing_result routing(guide){
-            return { 0, 0 };
+            return { 0 };
         }
 
         template<class guide>
