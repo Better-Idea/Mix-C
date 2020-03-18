a53e657ff1fdece6fbf3215f018a36352c61218e
PowerfulCat <x4495@outlook.com>
2020-03-18 12:08:20 +0800

    给 self_management 添加说明

--- gc/private/self_management.hpp
+++ gc/private/self_management.hpp
@@ -1,6 +1,15 @@
+/*
+在 GC 组件中，self_management用于判定一个类是否带有 GC 成分
+判定方法 is_based_on：
+    我们定义：只要是继承了 self_management 的类都应该具有以下私有方法
+        - routing
+        - clear_footmark
+    目前版本的 GC 尚处于完善与发展阶段，以上两个方法的接口签名有待商榷，
+    暂时未显式申明在 self_management 中
+*/
+
 #pragma once
 #include"define/base_type.hpp"
-#include"gc/private/routing_result.hpp"
 
 namespace mixc{
     struct self_management{};
