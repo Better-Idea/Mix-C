9e1f55bcbab065f58c01e1aea5bc38b0169f0623
PowerfulCat <x4495@outlook.com>
2020-03-18 14:36:01 +0800

    让 xitf 支持模板接口，并添加说明

--- macro/xitf.hpp
+++ macro/xitf.hpp
@@ -1,13 +1,9 @@
-#pragma once
-#include"macro/xlink.hpp"
-#include"macro/xvaargs.hpp"
-#include"macro/xlist+.hpp"
-#include"memop/cast.hpp"
-#include"meta/is_same.hpp"
-#include"utility/type/place_holder.hpp"
-
 /*
+说明：xitf 组件旨在替换虚函数，简化 GC 结构
+用法：如下
+
 #include<stdio.h>
+#include"macro/xitf.hpp"
 
 xitf(can_fly)
     xitf_item(fly, void,
@@ -26,58 +22,70 @@ void fly(can_fly ufo){
 }
 
 int main(){
-    // correct
+    // 正确
     bird b;
     can_fly cf = b;
     cf.fly(2020);
 
-    // correct
+    // 正确
     fly(b);
 
-    // correct
+    // 正确
     fly(bird());
     
-    // NOTICE:
-    // can_fly cf = bird();
-    // error ========================================================
-    // xitf interface (can_fly) need a alive implement instance (bird)
+    // 错误 ========================================================
+    // xitf 接口（can_fly）需要存活的实例（bird）
+    // 而 bird 在赋值给 can_fly 对象 cf 后就析构了
+    // 虽然对于无成员变量的 bird 来说，这样并不会产生错误的输出，
+    // 但我们建议不要这么做
+    can_fly cf = bird();
+    cf.flay(2020);
     return 0;
 }
 */
 
-namespace mixc{
-    template<int i> struct interface{};
-}
+#pragma once
+#include"macro/xvaargs.hpp"
+#include"macro/xlist+.hpp"
+#include"memop/cast.hpp"
+#include"meta/is_same.hpp"
+#include"utility/type/place_holder.hpp"
 
-#define xitf(name)                                                                                  \
-using mixc::interface;                                                                              \
-using name = interface<__COUNTER__>;                                                                \
-template<> struct interface<__COUNTER__ - 1>{                                                       \
+#define xitf(name,...)                                                                              \
+struct name{                                                                                        \
 private:                                                                                            \
-    using __self__ = interface<__COUNTER__ - 2>;                                                    \
+    using __self__ = name __VA_ARGS__;                                                              \
+                                                                                                    \
+    template<class type, int i>                                                                     \
+    void __build__(type const & impl, mixc::place_holder<i>) {}                                     \
+                                                                                                    \
+    __self__ *  __object;                                                                           \
+    void    **  __func_list;                                                                        \
     enum { __start__ = __COUNTER__ + 1, };                                                          \
 public:                                                                                             \
+    template<class type>                                                                            \
+    name(type const & impl) {                                                                       \
+        __func_list = __func_list__<type>;                                                          \
+        __build__(impl, mixc::place_holder<0>());                                                   \
+    }                                                                                               \
 
 #define __xitf_item__(index,name,ret,...)                                                           \
     ret name(xlist_args(__VA_ARGS__)){                                                              \
         using action = ret(__self__::*)(xlist_type(__VA_ARGS__));                                   \
-        auto func = mixc::cast<action>(funcs[index]);                                               \
+        auto func = mixc::cast<action>(__func_list[index]);                                         \
         if constexpr(mixc::is_same<void, ret>){                                                     \
-            (object->*func)(xlist_name(__VA_ARGS__));                                               \
+            (__object->*func)(xlist_name(__VA_ARGS__));                                             \
         }                                                                                           \
         else{                                                                                       \
-            return (object->*func)(xlist_name(__VA_ARGS__));                                        \
+            return (__object->*func)(xlist_name(__VA_ARGS__));                                      \
         }                                                                                           \
     }                                                                                               \
 private:                                                                                            \
     template<class type>                                                                            \
-    interface(                                                                                      \
-        type const & impl,                                                                          \
-        void **,                                                                                    \
-        mixc::place_holder<index>                                                                   \
-    ):  interface(impl, nullptr, mixc::place_holder<index + 1>()){                                  \
+    void __build__(type const & impl, mixc::place_holder<index>){                                   \
+        __build__(impl, mixc::place_holder<index + 1>());                                           \
         using action = ret(type::*)(xlist_type(__VA_ARGS__));                                       \
-        funcs[index] = mixc::cast<void *>(action(& type::name));                                    \
+        __func_list[index] = mixc::cast<void *>(action(& type::name));                              \
     }                                                                                               \
 public:                                                                                             \
 
@@ -85,15 +93,8 @@ public:                                                                         
     __xitf_item__(xvaargs(__COUNTER__ - __start__),name,ret,__VA_ARGS__)
 
 #define xitf_end()                                                                                  \
-    template<class type>                                                                            \
-    interface(type const & impl) :                                                                  \
-        interface(impl, funcs = func_list<type>, mixc::place_holder<0>()){                          \
-    }                                                                                               \
 private:                                                                                            \
-    template<class type, int i>                                                                     \
-    interface(type const & impl, void ** current_func, place_holder<i>) {}                          \
-    interface * object;                                                                             \
-    void **     funcs;                                                                              \
     template<class type>                                                                            \
-    inline static void * func_list[__COUNTER__ - __start__];                                        \
-};
+    inline static void * __func_list__[__COUNTER__ - __start__];                                    \
+}
+
