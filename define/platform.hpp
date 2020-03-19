a1b505bfa219218e9740f9a3b25f135d728f1146
PowerfulCat <x4495@outlook.com>
2020-03-19 08:40:40 +0800

    调整 platform 的定义

--- define/platform.hpp
+++ define/platform.hpp
@@ -1,16 +1,31 @@
+/*
+可选硬件平台
+- xin_x86
+- xin_arm
+- xin_risc_v
+- xin_mips
+*/
 #pragma once
 
-constexpr bool          is_64bit = sizeof(void *) == 8;
-constexpr bool          is_32bit = sizeof(void *) == 4;
+constexpr static bool is_64bit          = sizeof(void *) == 8;
+constexpr static bool is_32bit          = sizeof(void *) == 4;
 
-enum{
-    in_intel,
-    in_arm,
-    in_risc_v,
-    in_mips,
-    in_power_pc,
-};
+#ifndef xis_x86
+    #define xis_86          0
+#endif
 
-#ifndef xplatform
-    #define xplatform   in_intel
+#ifndef xis_arm
+    #define xis_arm         0
+#endif
+
+#ifndef xis_risc_v
+    #define xis_risc_v      0
+#endif
+
+#ifndef xis_mips
+    #define xis_mips        0
+#endif
+
+#if not (xis_x86 || xis_arm || xis_risc_v || xis_mips)
+    #error "you need select one platform"
 #endif
