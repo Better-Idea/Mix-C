33956ecbeecb30b2964de19a5b94e38c1566a641
PowerfulCat <x4495@outlook.com>
2020-03-19 09:08:59 +0800

    添加更高级一点的 string_view

---
+++ macro/xsv.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include"gc/private/ref.hpp"
+#define __xsv__(ret,item,padding,str)                                           \
+struct sv{                                                                      \
+    sv() : buf(padding str) {                                                   \
+        new (this) plus_length(sizeof(str) / sizeof(item) - 1);                 \
+    }                                                                           \
+    item buf[sizeof(padding str) / sizeof(item)];                               \
+};                                                                              \
+static sv   __mem__;                                                            \
+static sv * __tmp__ = & __mem__;                                                \
+return *(ret *)(& __tmp__)
+
+#define xsv(ret,item,padding,str)                                               \
+([]() -> ret & {                                                                \
+    using namespace mixc::inner_gc;                                             \
+    if constexpr (is_64bit){                                                    \
+        __xsv__(ret, item, padding padding, str);                               \
+    }                                                                           \
+    if constexpr(is_32bit){                                                     \
+        __xsv__(ret, item, padding, str);                                       \
+    }                                                                           \
+    static_assert(sizeof(plus_length) == (is_64bit ? 16 : 8));                  \
+}())                                                                            \
