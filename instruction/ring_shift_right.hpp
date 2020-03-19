4b2ffb0f8247ed038f391dbdbba1138d15eb3cf8
PowerfulCat <x4495@outlook.com>
2020-03-19 08:38:12 +0800

    添加循环左移右移到 instruction 中

---
+++ instruction/ring_shift_right.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include"define/base_type.hpp"
+
+namespace mixc{
+    #define xgen(type,bits)                                 \
+    inline type ring_shift_right(type v, uxx s){            \
+        type a = v >> s;                                    \
+        type b = v << (64 - s);                             \
+        return a | b;                                       \
+    }
+
+    xgen(u08, 8)
+    xgen(u16, 16)
+    xgen(u32, 32)
+    xgen(u64, 64)
+    #undef xgen
+}
