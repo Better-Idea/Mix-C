68adb8785a38421b8e4ee5ec8c6981ca64b3d1c5
PowerfulCat <x4495@outlook.com>
2020-03-19 10:59:29 +0800

    取消 cxx.fill* 对 cxx 的直接依赖

--- lang/cxx.fill_left.hpp
+++ lang/cxx.fill_left.hpp
@@ -1,10 +1,11 @@
 #pragma once
 #include"define/base_type.hpp"
-#include"lang/cxx.hpp"
 #include"memop/fill.hpp"
 #include"memory/alloc_callback.hpp"
+// #include"cxx.hpp"
 
 namespace mixc{
+    template<class item> struct cxx;
     template<class item>
     inline cxx<item> fill_left(cxx<item> self, uxx count, item value, alloc_callback<item> alloc){
         uxx       length = self.length + count;
