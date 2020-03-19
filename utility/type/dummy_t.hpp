3dc7a7f498a001527fb1b3c3390c57495abaf188
PowerfulCat <x4495@outlook.com>
2020-03-19 08:35:56 +0800

    更新 dummy_t

--- utility/type/dummy_t.hpp
+++ utility/type/dummy_t.hpp
@@ -1,11 +1,10 @@
 #pragma once
-#include"define/base_type.hpp"
+#include"meta_seq/tlist.hpp"
 
 namespace mixc{
-    // 可用作继承，函数参数占位符等场景的哑元
-    template<ixx i = -1>
     struct dummy_t{
         template<class ... args>
         dummy_t(args const & ...){}
+        using member_list = mixc::tlist<>;
     };
 }
