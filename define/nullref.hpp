3758d47e5269b094deac8134cc4f693b1cd105ee
PowerfulCat <x4495@outlook.com>
2020-03-22 23:27:59 +0800

    更新 nullref

--- define/nullref.hpp
+++ define/nullref.hpp
@@ -4,6 +4,7 @@
         #undef  xuser
         #define xuser mixc::define_nullref_t
         #include"define/base_type.hpp"
+        #include"memop/addressof.hpp"
         #include"macro/xgc.hpp"
     #pragma pop_macro("xuser")
 
@@ -12,23 +13,31 @@
             xgc_fields();
 
             template<class a> friend auto operator == (a const & value, nullref_t) {
-                return voidp(& value) == nullref_t::value;
+                return inc::addressof(value) == nullptr;
             }
 
             template<class a> friend auto operator == (nullref_t, a const & value) {
-                return voidp(& value) == nullref_t::value;
+                return inc::addressof(value) == nullptr;
             }
 
             template<class a> friend auto operator != (a const & value, nullref_t) {
-                return voidp(& value) != nullref_t::value;
+                return inc::addressof(value) != nullptr;
             }
 
             template<class a> friend auto operator != (nullref_t, a const & value) {
-                return voidp(& value) != nullref_t::value;
+                return inc::addressof(value) != nullptr;
+            }
+
+            template<class a>
+            operator a & () const {
+                return *(a *)this;
             }
         xgc_end();
-        
+
+        #pragma warning(push)
+        #pragma GCC diagnostic ignored "-Wnull-dereference"
         inline const nullref_t & nullref = *(nullref_t *)nullptr;
+        #pragma warning(pop)
     }
 
 #endif
