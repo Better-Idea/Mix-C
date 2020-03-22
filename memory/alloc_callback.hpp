d616bc264a07cab1f70f1be418e1b8a7c17a4737
PowerfulCat <x4495@outlook.com>
2020-03-22 22:34:56 +0800

    让 memory/ 系列支持新式包含惯用法

--- memory/alloc_callback.hpp
+++ memory/alloc_callback.hpp
@@ -1,12 +1,22 @@
-#pragma once
-#include"define/base_type.hpp"
-#include"macro/xitf.hpp"
+#ifndef xpack_memory_alloc_callback
+#define xpack_memory_alloc_callback
+    #pragma push_macro("xuser")
+        #undef  xuser
+        #define xuser mixc::memory_alloc_callback
+        #include"define/base_type.hpp"
+        #include"macro/xitf.hpp"
+    #pragma pop_macro("xuser")
 
-namespace mixc{
-    template<class item>
-    xitf(alloc_callback, <item>)
-        xitf_item(operator(), item *,
-            xnt(count, uxx)
-        );
-    xitf_end();
+    namespace mixc::memory_alloc_callback{
+        template<class item>
+        xitf(alloc_callback, xtmpl(item))
+            xitf_item(operator(), item *,
+                xnt(count, uxx)
+            );
+        xitf_end();
+    }
+#endif
+
+namespace xuser::inc{
+    using namespace mixc::memory_alloc_callback;
 }
