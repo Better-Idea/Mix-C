359e1d898ce27fb9fba580189be1a164461abc38
PowerfulCat <x4495@outlook.com>
2020-03-22 22:34:06 +0800

    让 meta_ctr/ 系列支持新式包含惯用法

---
+++ meta_ctr/cassert.hpp
@@ -0,0 +1,26 @@
+#ifndef xpack_meta_ctr_cassert
+#define xpack_meta_ctr_cassert
+    #pragma push_macro("xuser")
+        #undef  xuser
+        #define xuser mixc::meta_ctr_cassert
+        #include"define/base_type.hpp"
+    #pragma pop_macro("xuser")
+
+    namespace mixc::meta_ctr_cassert{
+        namespace inner_cassert{
+            template<bool value>
+            struct meta{
+                static_assert(value);
+                static constexpr bool result = value;
+            };
+        }
+
+        template<bool value>
+        constexpr bool cassert = inner_cassert::meta<value>::result;
+    }
+
+#endif
+
+namespace xuser::inc{
+    using namespace mixc::meta_ctr_cassert;
+}
--- meta_ctr/cif.hpp
+++ meta_ctr/cif.hpp
@@ -1,13 +1,25 @@
-#pragma once
+#ifndef xpack_meta_ctr_cif
+#define xpack_meta_ctr_cif
+    #pragma push_macro("xuser")
+        #undef  xuser
+        #define xuser mixc::meta_ctr_cif
+        #include"define/base_type.hpp"
+    #pragma pop_macro("xuser")
 
-namespace mixc{
-    template<bool condition, class if_true, class if_false>
-    struct cif{
-        using result = if_true;
-    };
+    namespace mixc::meta_ctr_cif{
+        template<bool condition, class if_true, class if_false>
+        struct cif{
+            using result = if_true;
+        };
 
-    template<class if_true, class if_false>
-    struct cif<false, if_true, if_false>{
-        using result = if_false;
-    };
+        template<class if_true, class if_false>
+        struct cif<false, if_true, if_false>{
+            using result = if_false;
+        };
+    }
+
+#endif
+
+namespace xuser::inc{
+    using namespace mixc::meta_ctr_cif;
 }
