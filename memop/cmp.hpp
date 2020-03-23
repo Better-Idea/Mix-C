da3c776d6dc988862949e09a069c26ea6b1da0ae
PowerfulCat <x4495@outlook.com>
2020-03-23 21:43:12 +0800

    让 memop/cmp 通过 inc 引用 base_seqlize

--- memop/cmp.hpp
+++ memop/cmp.hpp
@@ -4,18 +4,20 @@
         #undef  xuser
         #define xuser mixc::memop_cmp
         #include"define/base_type.hpp"
+        #include"meta/is_class.hpp"
+        #include"meta/is_enum.hpp"
+        #include"meta/is_union.hpp"
         #include"memop/base_seqlize.hpp"
     #pragma pop_macro("xuser")
 
     namespace mixc::memop_cmp{
         template<class a>
         inline ixx cmp(a const & left, a const & right){
-            if constexpr(__is_class(a) or __is_union(a) or __is_enum(a)){
-                auto l = mixc::base_seqlize(left);
-                auto r = mixc::base_seqlize(right);
+            if constexpr(inc::is_class<a> or inc::is_enum<a> or inc::is_union<a>){
+                auto l = inc::base_seqlize(left);
+                auto r = inc::base_seqlize(right);
                 for (auto i = 0; i < l.lenght; i++){
-                    ixx s = cmp(l.ptr[i], r.ptr[i]);
-                    if (s){
+                    if (ixx s = cmp(l.ptr[i], r.ptr[i]); s != 0){
                         return s;
                     }
                 }
