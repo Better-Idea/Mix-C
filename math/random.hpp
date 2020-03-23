1048acc0bc9a0ea0932f33dabceabfea71c63049
PowerfulCat <x4495@outlook.com>
2020-03-23 09:58:51 +0800

    让 math/random 支持新式包含惯用法

--- math/random.hpp
+++ math/random.hpp
@@ -1,67 +1,78 @@
-#pragma once
-#include"define/base_type.hpp"
-#include"define/nan.hpp"
-#include"instruction/ring_shift_left.hpp"
-#include"instruction/time_stamp.hpp"
-#include"meta/is_same.hpp"
+#ifndef xpack_math_random
+#define xpack_math_random
+    #pragma push_macro("xuser")
+        #undef  xuser
+        #define xuser mixc::math_random
+        #include"define/base_type.hpp"
+        #include"define/nan.hpp"
+        #include"instruction/ring_shift_left.hpp"
+        #include"instruction/time_stamp.hpp"
+        #include"meta/is_same.hpp"
+    #pragma pop_macro("xuser")
 
-namespace mixc{
-    namespace inner_random{
-        inline u64 x;
-        inline u64 y;
+    namespace mixc::math_random{
+        namespace inner{
+            inline u64 x;
+            inline u64 y;
 
-        struct __ini__{
-            __ini__(){
-                x = mixc::time_stamp();
-                y = mixc::time_stamp() * magic_number;
+            struct __ini__{
+                __ini__(){
+                    x = inc::time_stamp();
+                    y = inc::time_stamp() * magic_number;
+                }
+            } __ini__;
+
+            inline u64 random() {
+                constexpr u64 change_period = 0x3f;
+
+                if ((y & change_period) == 0){
+                    y += inc::time_stamp();
+                }
+
+                x += inc::ring_shift_left(x, y);
+                y += inc::ring_shift_left(y, x);
+                return y;
             }
-        } __ini__;
+        }
 
-        inline u64 random() {
-            constexpr u64 change_period = 0x3f;
 
-            if ((y & change_period) == 0){
-                y += mixc::time_stamp();
+        template<class type>
+        inline type random(){
+            if constexpr (inc::is_same<type, f32>){
+                union {
+                    u64 u;
+                    f32 candidate[2];
+                } v ;
+
+                while(true){
+                    if (v.u = inner::random(); v.candidate[0] != inc::nan){
+                        return v.candidate[0];
+                    }
+                    else if (v.candidate[1] != inc::nan){
+                        return v.candidate[1];
+                    }
+                }
             }
+            else if constexpr (inc::is_same<type, f64>){
+                union {
+                    u64 u;
+                    f64 candidate;
+                } v ;
 
-            x += mixc::ring_shift_left(x, y);
-            y += mixc::ring_shift_left(y, x);
-            return y;
+                while(true){
+                    if (v.u = inner::random(); v.candidate != inc::nan){
+                        return v.candidate;
+                    }
+                }
+            }
+            else{
+                return type(inner::random());
+            }
         }
     }
 
+#endif
 
-    template<class type>
-    inline type random(){
-        if constexpr (is_same<type, f32>){
-            union {
-                u64 u;
-                f32 candidate[2];
-            } v ;
-
-            while(true){
-                if (v.u = inner_random::random(); v.candidate[0] != nan){
-                    return v.candidate[0];
-                }
-                else if (v.candidate[1] != nan){
-                    return v.candidate[1];
-                }
-            }
-        }
-        else if constexpr (is_same<type, f64>){
-            union {
-                u64 u;
-                f64 candidate;
-            } v ;
-
-            while(true){
-                if (v.u = inner_random::random(); v.candidate != nan){
-                    return v.candidate;
-                }
-            }
-        }
-        else{
-            return type(inner_random::random());
-        }
-    }
+namespace xuser::inc{
+    using namespace mixc::math_random;
 }
