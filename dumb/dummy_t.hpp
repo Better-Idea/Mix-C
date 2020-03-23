dd70942bf1ccc40eba157bd1765c16b5acc137a4
PowerfulCat <x4495@outlook.com>
2020-03-23 21:06:39 +0800

    让 dumb/dummy_t 使用 gc 结构

--- dumb/dummy_t.hpp
+++ dumb/dummy_t.hpp
@@ -1,29 +1,19 @@
-#pragma once
-#include"meta_seq/tlist.hpp"
-
-namespace mixc{
-    struct dummy_t{
-        template<class ... args>
-        dummy_t(args const & ...){}
-        using member_list = mixc::tlist<>;
-    };
-}
-
 #ifndef xpack_dumb_dummy_t
 #define xpack_dumb_dummy_t
     #pragma push_macro("xuser")
         #undef  xuser
         #define xuser mixc::dumb_dummy_t
         #include"define/base_type.hpp"
-        #include"meta_seq/tlist.hpp"
+        #include"macro/xgc.hpp"
     #pragma pop_macro("xuser")
 
     namespace mixc::dumb_dummy_t{
-        struct dummy_t{
+        xgc(dummy_t)
+            xgc_fields();
+
             template<class ... args>
             constexpr dummy_t(args const & ...){}
-            using member_list = mixc::tlist<>;
-        };
+        xgc_end();
     }
 
 #endif
