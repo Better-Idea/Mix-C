689ddcc3645664b61700c2200a46ddd08763f849
PowerfulCat <x4495@outlook.com>
2020-03-24 21:24:23 +0800

    让 macro/xsv 更通用化

--- macro/xsv.hpp
+++ macro/xsv.hpp
@@ -1,24 +1,43 @@
 #pragma once
-#include"gc/private/ref.hpp"
-#define __xsv__(ret,item,padding,str)                                           \
+#include"gc/private/token.hpp"
+#include"define/platform.hpp"
+#include"memory/new.hpp"
+
+namespace mixc{
+    template<class item>
+    struct static_string_holder {
+    private:
+        mixc::gc_token::token_plus * info;
+    public:
+        item * ptr() const {
+            return (item *)(info + 1);
+        }
+        uxx length() const {
+            return info->length;
+        }
+    };
+}
+
+#define __xsv__(item,padding,str)                                               \
 struct sv{                                                                      \
     sv() : buf(padding str) {                                                   \
-        new (this) plus_length(sizeof(str) / sizeof(item) - 1);                 \
+        new (this) mixc::gc_token::token_plus(sizeof(str) / sizeof(item) - 1);  \
     }                                                                           \
     item buf[sizeof(padding str) / sizeof(item)];                               \
 };                                                                              \
 static sv   __mem__;                                                            \
 static sv * __tmp__ = & __mem__;                                                \
-return *(ret *)(& __tmp__)
+return *(mixc::static_string_holder<item> *)(& __tmp__)
 
-#define xsv(ret,item,padding,str)                                               \
-([]() -> ret & {                                                                \
-    using namespace mixc::inner_gc;                                             \
+
+#define xsv(item,padding,str)                                                   \
+([]() {                                                                         \
+    using namespace mixc::gc_token;                                             \
     if constexpr (is_64bit){                                                    \
-        __xsv__(ret, item, padding padding, str);                               \
+        __xsv__(item, padding padding, str);                                    \
     }                                                                           \
     if constexpr(is_32bit){                                                     \
-        __xsv__(ret, item, padding, str);                                       \
+        __xsv__(item, padding, str);                                            \
     }                                                                           \
-    static_assert(sizeof(plus_length) == (is_64bit ? 16 : 8));                  \
+    static_assert(sizeof(token_plus) == (is_64bit ? 16 : 8));                   \
 }())
