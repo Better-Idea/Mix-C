ac0829a5e6d7841e7aaa5d9a516e9013824f94b7
PowerfulCat <x4495@outlook.com>
2020-03-25 21:41:13 +0800

    添加 interface/can_compare 接口

---
+++ interface/can_compare.hpp
@@ -0,0 +1,32 @@
+#ifndef xpack_interface_can_compare
+#define xpack_interface_can_compare
+    #pragma push_macro("xuser")
+        #undef  xuser
+        #define xuser mixc::interface_can_compare
+        #include"define/base_type.hpp"
+        #include"macro/xitf.hpp"
+        #include"memop/cmp.hpp"
+    #pragma pop_macro("xuser")
+
+    namespace mixc::interface_can_compare{
+        template<class type>
+        xitf(can_compare)
+            xitf_item(operator(), ixx,
+                xnt(left, type const &),
+                xnt(right, type const &)
+            );
+        xitf_end();
+
+        template<class type>
+        inline auto const & default_compare = 
+            [](type const & left, type const & right){
+                return inc::cmp<type>(left, right);
+            };
+    }
+
+#endif
+
+namespace xuser::inc{
+    using mixc::interface_can_compare::can_compare;
+    using mixc::interface_can_compare::default_compare;
+}
