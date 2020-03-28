50a5d08e2cca52ccae7b7b6969aed415584ebe58
PowerfulCat <x4495@outlook.com>
2020-03-28 21:57:32 +0800

    更新 docker/shared_ptr

--- docker/shared_ptr.hpp
+++ docker/shared_ptr.hpp
@@ -1,4 +1,3 @@
-
 #ifndef xpack_docker_shared_ptr
 #define xpack_docker_shared_ptr
     #pragma push_macro("xuser")
@@ -6,35 +5,37 @@
         #define xuser mixc::docker_shared_ptr
         #include"define/base_type.hpp"
         #include"gc/private/ref.hpp"
+        #include"macro/xgc.hpp"
     #pragma pop_macro("xuser")
 
     namespace mixc::docker_shared_ptr{
         template<class type> struct shared_ptr;
         template<class type>
-        struct shared_ptr : inc::ref_ptr<shared_ptr<type>, type> {
+        xgc(shared_ptr, xpub(inc::ref_ptr<shared_ptr<type>, type>))
             using meta = inc::ref_ptr<shared_ptr<type>, type>;
-            explicit shared_ptr(ini) : meta(length(0)) {}
+            using the_t = shared_ptr<type>;
+
+            explicit shared_ptr(inc::ini) : 
+                meta(inc::length(0)) {}
 
             template<class ... args>
-            explicit shared_ptr(ini, args const & ... list) : meta(length(0), list...) {}
+            explicit shared_ptr(inc::ini, args const & ... list) : 
+                meta(inc::length(0), list...) {}
 
             shared_ptr() = default;
 
             template<class ... args>
             shared_ptr<type> & operator()(ini, args const & ... list){
                 using metap = meta *;
-                xthe.~meta();
-                new (metap(this)) meta(length(0), list...);
-                return xthe;
+                the.~meta();
+                new (metap(this)) meta(inc::length(0), list...);
+                return the;
             }
-        private:
-            template<class __type__> friend union mixc::macro_xtypeid::__typeid__;
-            static constexpr const char * __self_name__ = "shared_ptr";
-        };
+        xgc_end();
     }
 
 #endif
 
 namespace xuser::inc{
-    using namespace mixc::docker_shared_ptr;
+    using mixc::docker_shared_ptr::shared_ptr;
 }
