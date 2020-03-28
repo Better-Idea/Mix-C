d9387d9194ec9abe90aec2e2b5e5d19f24683802
PowerfulCat <x4495@outlook.com>
2020-03-28 21:10:46 +0800

    添加 array 静态数组到 docker/

---
+++ docker/array.hpp
@@ -0,0 +1,19 @@
+#ifdef xuser
+    #ifndef xusing_docker_array
+        #include"docker/private/array.hpp"
+    #endif
+
+    namespace xuser::inc{
+        template<class item, uxx count, uxx ... rest>
+        struct array : xusing_docker_array::array<array<item, count, rest...>, item, count, rest...>{
+            using xusing_docker_array::array<array<item, count, rest...>, item, count, rest...>::array;
+        };
+
+        template<class type, class ... args>
+        struct array_view : array<type, 1 + sizeof...(args)>{
+            array_view(type const & first, args const & ... list) : 
+                array<type, 1 + sizeof...(args)>(first, list...) {
+            }
+        };
+    }
+#endif
---
+++ docker/private/array.hpp
@@ -0,0 +1,50 @@
+#ifndef xpack_docker_array
+#define xpack_docker_array
+    #pragma push_macro("xuser")
+        #undef  xuser
+        #define xuser mixc::docker_array
+        #include"define/base_type.hpp"
+        #include"macro/xgc.hpp"
+    #pragma pop_macro("xuser")
+
+    namespace mixc::docker_array{
+        template<class type, uxx count = 0, uxx ... rest>
+        xgc(core)
+            using self  = core<type, count, rest...>;
+            using item  = typename core<type, rest...>::self;
+            using itemp = item *;
+            using items = item[count];
+
+            xgc_fields(
+                xpro(data, items)
+            );
+
+            template<class ... args>
+            core(args const & ... list) : 
+                data { list... } {}
+        xgc_end();
+
+        template<class type>
+        struct core<type>{
+            using self = type;
+            using item = self;
+        };
+
+        template<class final, class type, uxx count, uxx ... rest>
+        struct array : core<type, count, rest...> {
+            using core<type, count, rest...>::core;
+            using the_t = core<type, count, rest...>;
+
+            auto & operator[] (uxx index) const {
+                return the_t::data[index];
+            }
+
+            constexpr static uxx length() {
+                return the_t::count;
+            }
+        };
+    }
+#endif
+
+#undef  xusing_docker_array
+#define xusing_docker_array     mixc::docker_array
