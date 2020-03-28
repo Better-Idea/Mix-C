fb80abe9590e36bf0ede45af565ed79c8ca09d8c
PowerfulCat <x4495@outlook.com>
2020-03-28 22:33:32 +0800

    移植 bitbind 到 define/

---
+++ define/bitbind.hpp
@@ -0,0 +1,43 @@
+#ifndef xpack_define_bitbind
+#define xpack_define_bitbind
+    #pragma push_macro("xuser")
+        #undef  xuser
+        #define xuser mixc::define_bitbind
+        #include"define/base_type.hpp"
+        #include"macro/xgc.hpp"
+    #pragma pop_macro("xuser")
+
+    namespace mixc::define_bitbind{
+        template<class type, class bit_type = bool>
+        xgc(bitbind)
+            xgc_fields(
+                xpro(ptr, type *),
+                xpro(mask, type)
+            );
+
+            bitbind() : bitbind(nullptr, 0) {}
+            bitbind(type * bits, uxx index){
+                this->bind(bits, index);
+            }
+
+            bit_type operator = (bit_type value){
+                ptr[0] = value ? ptr[0] | mask : ptr[0] & ~mask;
+                return value;
+            }
+
+            void bind(type * bits, uxx index){
+                ptr = bits;
+                mask = type(1) << index;
+            }
+
+            operator bit_type(){
+                return bit_type((ptr[0] & mask) != 0);
+            }
+        xgc_end();
+    }
+
+#endif
+
+namespace xuser::inc{
+    using mixc::define_bitbind::bitbind;
+}
