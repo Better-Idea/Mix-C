48a3c79f621a199e954e2209e8f990251cd7106e
PowerfulCat <x4495@outlook.com>
2020-03-31 22:21:14 +0800

    在 math/ 中添加 partial_sum 函数

---
+++ math/partial_sum.hpp
@@ -0,0 +1,39 @@
+#ifndef xpack_math_partial_sum
+#define xpack_math_partial_sum
+    #pragma push_macro("xuser")
+        #undef  xuser
+        #define xuser mixc::math_partial_sum
+        #include"define/base_type.hpp"
+        #include"interface/can_random_access.hpp"
+    #pragma pop_macro("xuser")
+
+    namespace mixc::math_partial_sum{
+        template<class item_t>
+        inline void partial_sum(
+            inc::can_random_access<item_t> des, 
+            inc::can_random_access<item_t> src){
+
+            if (src.length() == 0){
+                return;
+            }
+
+            auto tmp = des[0] = src[0];
+
+            for(uxx i = 1, length = src.length(); i < length; i++){
+                auto last = tmp;
+                tmp    = des[i];
+                des[i] = last + src[i];
+            }
+        }
+
+        template<class item_t>
+        inline void partial_sum(inc::can_random_access<item_t> des_src){
+            partial_sum<item_t>(des_src, des_src);
+        }
+    }
+
+#endif
+
+namespace xuser::inc{
+    using ::mixc::math_partial_sum::partial_sum;
+}
