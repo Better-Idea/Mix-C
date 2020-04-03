a3a42b25adf3de9a1d02557f0f11f85c0388dd57
PowerfulCat <x4495@outlook.com>
2020-04-03 10:55:48 +0800

    完善 macro/xtypeid

--- macro/xtypeid.hpp
+++ macro/xtypeid.hpp
@@ -21,27 +21,38 @@
 
             union{
                 operator asciis() const {
-                    #define xgen(type)      else if constexpr (is_same<the_type, type>){ return # type; }
+                    #define xgen(target)      else if constexpr (is_same<type, target>){ return # target; }
 
                     if constexpr (is_class<the_type>){
                         return the_type::__self_name;
                     }
+
                     xgen(char)
+                    xgen(char *)
                     xgen(u08)
+                    xgen(u08p)
                     xgen(u16)
+                    xgen(u16p)
                     xgen(u32)
+                    xgen(u32p)
                     xgen(u64)
+                    xgen(u64p)
                     xgen(i08)
+                    xgen(i08p)
                     xgen(i16)
+                    xgen(i16p)
                     xgen(i32)
+                    xgen(i32p)
                     xgen(i64)
+                    xgen(i64p)
                     xgen(f32)
+                    xgen(f32p)
                     xgen(f64)
+                    xgen(f64p)
                     xgen(asciis)
                     else{
                         return "wait implement";
                     }
-
                     #undef  xgen
                 }
 
@@ -58,18 +69,29 @@
                     if constexpr (is_class<the_type>){
                         return the_type::__class_id;
                     }
-                    
+
                     xgen(char)
+                    xgen(char *)
                     xgen(u08)
+                    xgen(u08p)
                     xgen(u16)
+                    xgen(u16p)
                     xgen(u32)
+                    xgen(u32p)
                     xgen(u64)
+                    xgen(u64p)
                     xgen(i08)
+                    xgen(i08p)
                     xgen(i16)
+                    xgen(i16p)
                     xgen(i32)
+                    xgen(i32p)
                     xgen(i64)
+                    xgen(i64p)
                     xgen(f32)
+                    xgen(f32p)
                     xgen(f64)
+                    xgen(f64p)
                     xgen(asciis)
                     else{
                         return not_exist;
