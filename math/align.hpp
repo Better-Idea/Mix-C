d670e37fbc348d83fdf83e559d2f80759fb737f3
PowerfulCat <x4495@outlook.com>
2020-04-04 22:35:14 +0800

    添加 align 到 math/

---
+++ math/align.hpp
@@ -0,0 +1,38 @@
+#ifndef xpack_math_align
+#define xpack_math_align
+    #pragma push_macro("xuser")
+        #undef  xuser
+        #define xuser mixc::math_align
+        #include"define/base_type.hpp"
+        #include"instruction/index_of_last_set.hpp"
+    #pragma pop_macro("xuser")
+
+    namespace mixc::math_align{
+        // 让 size 以 2 的指数次方 向上圆整
+        // 例如：
+        // align(0x14) = 0x20
+        // 注意：
+        // align(0) = 0
+        // align(v) = 1 if v > 2 exp (sizeof(uxx) * 8 - 1)
+        template<class type>
+        inline type align(type size){
+            return size & (size - 1) ?
+                type(1) << (inc::index_of_last_set(size) + 1) :
+                size;
+        }
+
+        // 让 size 的低 bits 位以 2 的指数次方 向上圆整
+        // 例如：
+        // align(0x123, 4) = 0x130
+        template<class type>
+        inline type align(type size, type bits){
+            type mask = (type(1) << bits) - 1;
+            return (size + mask) & ~mask;
+        }
+    }
+
+#endif
+
+namespace xuser::inc{
+    using ::mixc::math_align::align;
+}
