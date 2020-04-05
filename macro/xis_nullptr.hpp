350cfce46eca013c97ac94bec4dc78918cc8b9f0
PowerfulCat <x4495@outlook.com>
2020-04-05 18:37:41 +0800

    在 macro/ 新增 xis_nullptr 用于简化与 nullptr 比较的运算重载

---
+++ macro/xis_nullptr.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#define xis_nullptr(...)                                                        \
+bool operator==(decltype(nullptr)){                                             \
+    return __VA_ARGS__;                                                         \
+}                                                                               \
+friend bool operator==(decltype(nullptr), __self const & self){                 \
+    return self == nullptr;                                                     \
+}                                                                               \
+friend bool operator!=(__self const & self, decltype(nullptr)){                 \
+    return !(self == nullptr);                                                  \
+}                                                                               \
+friend bool operator!=(decltype(nullptr), __self const & self){                 \
+    return !(self == nullptr);                                                  \
+}
