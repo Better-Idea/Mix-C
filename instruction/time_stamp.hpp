57d9bde1c00f080bcb4d9808d619212103c744e4
PowerfulCat <x4495@outlook.com>
2020-04-06 18:47:29 +0800

    让 instruction/time_stamp 使用内建函数

--- instruction/time_stamp.hpp
+++ instruction/time_stamp.hpp
@@ -10,10 +10,7 @@
     namespace mixc::instruction_time_stamp{
         inline u64 time_stamp(){
             #if xis_x86
-                u32 low;
-                u32 high;
-                asm("rdtsc":"=a"(low),"=d"(high));
-                return u64(high) << 32 | low;
+                return __builtin_ia32_rdtsc();
             #endif
         }
     }
