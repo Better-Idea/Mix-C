81f56b8b440fae9eb769255d2a71bc2ba924b2e9
PowerfulCat <x4495@outlook.com>
2020-04-06 13:12:10 +0800

    简化 instruction/index_of_xxx

--- instruction/index_of_first_set.hpp
+++ instruction/index_of_first_set.hpp
@@ -8,30 +8,9 @@
     #pragma pop_macro("xuser")
 
     namespace mixc::instruction_index_of_first_set{
-        inline uxx index_of_first_set(u32 v){
-            uxx i = v;
-            uxx n = not_exist;
-            asm("bsf %0, %0":"=r"(i));
-            asm("cmovz %1, %2":"=a"(i):"r"(n),"a"(i));
-            return i;
-        }
-
-        inline uxx index_of_first_set(u64 v){
-            uxx i = uxx(v);
-            uxx n = not_exist;
-
-            if constexpr (is_64bit){
-                asm("bsf %0, %0":"=r"(i));
-                asm("cmovz %1, %2":"=a"(i):"r"(n),"a"(i));
-            }
-            else{
-                asm("bsf %0, %0":"=r"(i));
-                asm("jz pass");
-                asm("bsf %1, %0":"=r"(i):"r"(u32(v >> 32)));
-                asm("bts $5, %0":"=r"(i));
-                asm("pass:");
-            }
-            return i;
+        template<class type>
+        inline uxx index_of_first_set(type v){
+            return __builtin_ffs(v) - 1;
         }
     }
 
--- instruction/index_of_last_set.hpp
+++ instruction/index_of_last_set.hpp
@@ -8,33 +8,9 @@
     #pragma pop_macro("xuser")
 
     namespace mixc::instruction_index_of_last_set{
-        inline uxx index_of_last_set(u32 v){
-            uxx i = v;
-            uxx n = not_exist;
-            asm("bsr %0, %0":"=r"(i));
-            asm("cmovz %1, %2":"=a"(i):"r"(n),"a"(i));
-            return i;
-        }
-
-        inline uxx index_of_last_set(u64 v){
-            uxx n = not_exist;
-            uxx i;
-
-            if constexpr (is_64bit){
-                i = uxx(v);
-                asm("bsr %0, %0":"=r"(i));
-                asm("cmovz %1, %2":"=a"(i):"r"(n),"a"(i));
-            }
-            else{
-                i = uxx(v >> 32);
-                asm("bsf %0, %0":"=a"(i));
-                asm("jz pass");
-                asm("bts $5, %0":"=a"(i));
-                asm("ret");
-                asm("pass:");
-                asm("bsf %1, %0":"=r"(i):"r"(u32(v >> 32)));
-            }
-            return i;
+        template<class type>
+        inline uxx index_of_last_set(type v){
+            return v == 0 ? not_exist : uxx(sizeof(v) * 8 - 1 - __builtin_clz(v));
         }
     }
 
