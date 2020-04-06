7d9e4c6e140df72dd2deaf3a58bce8d98ccb0423
PowerfulCat <x4495@outlook.com>
2020-04-06 18:46:39 +0800

    简化 macro/private/callable

--- macro/private/callable.hpp
+++ macro/private/callable.hpp
@@ -1,32 +1,24 @@
 #ifndef xpack_macro_private_callable
 #define xpack_macro_private_callable
+    #pragma push_macro("xuser")
+        #undef  xuser
+        #define xuser mixc::macro_private_callable
+        #include"macro/xis_nullptr.hpp"
+    #pragma pop_macro("xuser")
     namespace mixc::macro_private_callable{
         struct callable_t{
         protected:
             void  *   __object;
             void  **  __func_list;
-            using base = callable_t;
+            using base   = callable_t;
+            using __self = callable_t;
         public:
             callable_t(){
                 __object    = nullptr;
                 __func_list = nullptr;
             }
 
-            bool operator==(decltype(nullptr)) const {
-                return __object == nullptr;
-            }
-
-            bool operator!=(decltype(nullptr)) const {
-                return __object != nullptr;
-            }
-
-            friend bool operator==(decltype(nullptr), callable_t const &  value){
-                return value == nullptr;
-            }
-
-            friend bool operator!=(decltype(nullptr), callable_t const &  value){
-                return !(value == nullptr);
-            }
+            xis_nullptr(__object == nullptr);
 
             void operator=(decltype(nullptr)){
                 __object = nullptr;
