13fd3a6ea7858bab63f894488ee9db41a67ade46
PowerfulCat <x4495@outlook.com>
2020-04-18 15:42:36 +0800

    纠正 lang/wxx/is_digital 命名

--- lang/wxx/is_digital.hpp
+++ lang/wxx/is_digital.hpp
@@ -2,47 +2,47 @@
     #include"lang/private/wxx.hpp"
 #endif
 
-#ifndef xpack_lang_wxx_is_digtal
-#define xpack_lang_wxx_is_digtal
+#ifndef xpack_lang_wxx_is_digital
+#define xpack_lang_wxx_is_digital
     #pragma push_macro("xuser")
     #pragma push_macro("xusing_lang_wxx")
         #undef  xusing_lang_wxx
         #undef  xuser
-        #define xuser mixc::lang_wxx_is_digtal
+        #define xuser mixc::lang_wxx_is_digital
         #include"define/base_type.hpp"
         #include"lang/wxx.hpp"
     #pragma pop_macro("xusing_lang_wxx")
     #pragma pop_macro("xuser")
 
-    namespace mixc::lang_wxx_is_digtal{
+    namespace mixc::lang_wxx_is_digital{
         template<class type>
         struct core : inc::wxx<type>{
             using inc::wxx<type>::wxx;
             using the_t = core<type>;
 
-            auto is_digtal() const {
+            auto is_digital() const {
                 return '0' <= the.data and the.data <= '9';
             }
         };
     }
 #endif
 
-namespace mixc::lang_wxx_is_digtal::xuser{
+namespace mixc::lang_wxx_is_digital::xuser{
     template<class final, class type>
     struct wxx : xusing_lang_wxx::wxx<final, type> {
         using xusing_lang_wxx::wxx<final, type>::wxx;
         using the_t = core<type>;
 
-        auto is_digtal() const {
-            return the.is_digtal();
+        auto is_digital() const {
+            return the.is_digital();
         }
 
-        final & is_digtal(bool * result) const {
-            result[0] = the.is_digtal();
+        final & is_digital(bool * result) const {
+            result[0] = the.is_digital();
             return thex;
         }
     };
 }
 
 #undef  xusing_lang_wxx
-#define xusing_lang_wxx ::mixc::lang_wxx_is_digtal::xuser
+#define xusing_lang_wxx ::mixc::lang_wxx_is_digital::xuser
