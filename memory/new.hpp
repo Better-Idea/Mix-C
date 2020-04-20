5e498eac7b7c50a65f61b9a146c993788131cc55
PowerfulCat <x4495@outlook.com>
2020-04-20 15:32:40 +0800

    简化 memory/new

--- memory/new.hpp
+++ memory/new.hpp
@@ -1,21 +1,6 @@
 #pragma once
 #ifndef _NEW
-    namespace mixc::dumb{
-        inline auto the_size_t(){
-            if constexpr (sizeof(void *) == 4){
-                return (unsigned int)(0);
-            }
-            else{
-                return (unsigned long int)(0);
-            }
-        }
-    }
-
-    inline void * operator new(
-        decltype(
-            mixc::dumb::the_size_t()
-        ) bytes, void * ptr
-    ) {
+    inline void * operator new(size_t bytes, void * ptr) {
         return ptr;
     }
 #endif
