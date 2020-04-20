2d6b3f3b0d3d624e7516b0b07a8be8bc80d664fc
PowerfulCat <x4495@outlook.com>
2020-04-20 15:56:52 +0800

    让 macro/xis_nullptr 适配 gc 框架

--- macro/xis_nullptr.hpp
+++ macro/xis_nullptr.hpp
@@ -4,12 +4,12 @@
 bool operator==(decltype(nullptr)) const {                                      \
     return __VA_ARGS__;                                                         \
 }                                                                               \
-friend bool operator==(decltype(nullptr), __self const & self){                 \
+friend bool operator==(decltype(nullptr), the_t const & self){                  \
     return self == nullptr;                                                     \
 }                                                                               \
-friend bool operator!=(__self const & self, decltype(nullptr)){                 \
+friend bool operator!=(the_t const & self, decltype(nullptr)){                  \
     return !(self == nullptr);                                                  \
 }                                                                               \
-friend bool operator!=(decltype(nullptr), __self const & self){                 \
+friend bool operator!=(decltype(nullptr), the_t const & self){                  \
     return !(self == nullptr);                                                  \
 }
