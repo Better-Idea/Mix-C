eba12cf3c8dc55025f4a95cc05c2a963479142ed
PowerfulCat <x4495@outlook.com>
2020-04-25 10:20:03 +0800

    将 gc/private/self_management 移动到 gc/self_management，表示对外部可见

--- gc/self_management.hpp
+++ gc/self_management.hpp
@@ -3,7 +3,6 @@
 判定方法 is_based_on：
     我们定义：只要是继承了 self_management 的类都应该具有以下私有方法
         - routing<guide>
-    暂时未显式申明在 self_management 中
 */
 
 #ifndef xpack_gc_self_management
@@ -12,18 +11,23 @@
         #undef  xuser
         #define xuser mixc::gc_self_management
         #include"define/base_type.hpp"
-        #include"meta_seq/vlist.hpp"
         #include"gc/private/routing_result.hpp"
+        #include"macro/xgc.hpp"
     #pragma pop_macro("xuser")
 
     namespace mixc::gc_self_management{
         struct self_management{
-            using member_list = inc::vlist<>;
+            xgc_fields(
+                xiam(self_management)
+            )
+        protected:
             template<class guide> inc::routing_result routing();
         };
     }
 
 #endif
+
+#include"gc/private/routing_result.hpp"
 
 namespace xuser::inc{
     using ::mixc::gc_self_management::self_management;
