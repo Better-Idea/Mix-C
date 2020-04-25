b6a16f6dafba69e548e183df41193870d7c36ec5
PowerfulCat <x4495@outlook.com>
2020-04-25 10:55:24 +0800

    将 dumb/disable_copy 中的 tab 制表符换成空格

--- dumb/disable_copy.hpp
+++ dumb/disable_copy.hpp
@@ -1,25 +1,25 @@
 #ifndef xpack_dumb_disable_copy
 #define xpack_dumb_disable_copy
-	#pragma push_macro("xuser")
-		#undef  xuser
-		#define xuser mixc::dumb_disable_copy
-		#include"define/base_type.hpp"
-		#include"macro/xgc.hpp"
-	#pragma pop_macro("xuser")
+    #pragma push_macro("xuser")
+        #undef  xuser
+        #define xuser mixc::dumb_disable_copy
+        #include"define/base_type.hpp"
+        #include"macro/xgc.hpp"
+    #pragma pop_macro("xuser")
 
-	namespace mixc::dumb_disable_copy {
-		struct disable_copy {
-			xgc_fields(
-				xiam(disable_copy)
-			)
-			
-			disable_copy() = default;
-			disable_copy(disable_copy const &) = delete;
-			void operator=(disable_copy const &) = delete;
-		};
-	}
+    namespace mixc::dumb_disable_copy {
+        struct disable_copy {
+            xgc_fields(
+                xiam(disable_copy)
+            )
+            
+            disable_copy() = default;
+            disable_copy(disable_copy const &) = delete;
+            void operator=(disable_copy const &) = delete;
+        };
+    }
 #endif
 
 namespace xuser::inc {
-	using ::mixc::dumb_disable_copy::disable_copy;
+    using ::mixc::dumb_disable_copy::disable_copy;
 }
