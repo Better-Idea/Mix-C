bcff3ebcf03a0001fa68499ff3997ae63f8aa01f
PowerfulCat <x4495@outlook.com>
2020-04-27 10:19:38 +0800

    纠正 memop/base_seqlize

--- memop/base_seqlize.hpp
+++ memop/base_seqlize.hpp
@@ -4,26 +4,27 @@
         #undef  xuser
         #define xuser mixc::memop_base_seqlize
         #include"define/base_type.hpp"
+        #include"memop/addressof.hpp"
     #pragma pop_macro("xuser")
 
     namespace mixc::memop_base_seqlize{
         // 把type转换成指定长度的无符号整数指针
         template<class type>
         inline auto base_seqlize(type const & value){
-            #define gen(type,scale,...)                                 \
-                if constexpr (sizeof(value) % scale == 0 __VA_ARGS__){  \
-                    struct seg{                                         \
-                        type * ptr;                                     \
-                        uxx    length;                                  \
-                    } r { (type *)& value, sizeof(value) / scale };     \
-                    return r;                                           \
-                }
+            #define xgen(type,scale,...)                                              \
+                if constexpr (sizeof(value) % scale == 0 __VA_ARGS__){                \
+                    struct seg{                                                       \
+                        type * ptr;                                                   \
+                        uxx    length;                                                \
+                    } r { (type *) inc::addressof(value), sizeof(value) / scale };    \
+                    return r;                                                         \
+                } else
             
-            gen(u64, 8);
-            gen(u32, 4);
-            gen(u16, 2);
-            gen(u08, 1, || true);
-            #undef gen
+            xgen(u64, 8)
+            xgen(u32, 4)
+            xgen(u16, 2)
+            xgen(u08, 1, || true);
+            #undef xgen
         }
     }
 
