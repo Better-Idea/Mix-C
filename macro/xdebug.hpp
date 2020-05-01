49100ce48ad0cd2010f2b27ace7feef3ed4f71c4
PowerfulCat <x4495@outlook.com>
2020-05-01 22:50:33 +0800

    规格化 macro/xdebug*

--- macro/xdebug.hpp
+++ macro/xdebug.hpp
@@ -1,52 +1,61 @@
-#pragma once
-#pragma warning(disable:4390)
-#ifdef xenable_xdebug
-    #include<stdio.h>
-    #include"macro/xtypeid.hpp"
-    #include"macro/private/log.hpp"
-    #define xdebug(token,...)                               \
-    if (token) {                                            \
-        ::mixc::macro_private_log::log(                     \
-        __debug_id += 1,                                    \
-        __FILE__,                                           \
-        __LINE__,                                           \
-        "func, " #__VA_ARGS__ ",", __func__, __VA_ARGS__);   \
-    }                                                       \
-    if (token)
+#ifndef xpack_macro_xdebug
+#define xpack_macro_xdebug
+    #pragma warning(disable:4390)
+    #pragma push_macro("xuser")
+        #undef  xuser
+        #define xuser mixc::macro_xdebug
+        #include"configure.hpp"
+        #include"macro/xtypeid.hpp"
+        #include"macro/private/log.hpp"
+    #pragma pop_macro("xuser")
 
-    #define xsw(token,...)            constexpr bool token = { __VA_ARGS__ + 0 };
+    #if xuse_xdebug
+        #define xdebug(token,...)                               \
+        if (token) {                                            \
+            ::mixc::macro_private_log::log(                     \
+            __debug_id += 1,                                    \
+            __FILE__,                                           \
+            __LINE__,                                           \
+            "func, " #__VA_ARGS__ ",", __func__, __VA_ARGS__);   \
+        }                                                       \
+        if (token)
 
-    inline int __debug_id;
+        #define xsw(token,...)            constexpr bool token = { __VA_ARGS__ + 0 };
 
-    // docker/private/hashmap.hpp
-    #define xon    0
-    xsw(im_docker_hashmap_node_set  , xon);
-    xsw(im_docker_hashmap_set       , xon);
-    xsw(im_docker_hashmap_get       , xon);
-    #undef  xon
+        inline int __debug_id;
 
-    // memop/memory.hpp
-    #define xon    1
-    xsw(im_alloc_with_initial       , xon);
-    xsw(im_alloc                    , xon);
-    xsw(im_free                     , xon);
-    xsw(im_free_with_destroy        , xon);
-    #undef  xon
+        // docker/private/hashmap.hpp
+        #define xon    0
+        xsw(im_docker_hashmap_node_set  , xon);
+        xsw(im_docker_hashmap_set       , xon);
+        xsw(im_docker_hashmap_get       , xon);
+        #undef  xon
 
-    // gc/ref
-    #define xon    0
-    xsw(im_gc_$token_mix            , xon);
-    xsw(im_gc_$meta                 , xon);
-    xsw(im_gc_meta_routing_entry    , xon);
-    xsw(im_gc_meta_routing          , xon);
-    #undef  xon
+        // memop/memory.hpp
+        #define xon    1
+        xsw(im_alloc_with_initial       , xon);
+        xsw(im_alloc                    , xon);
+        xsw(im_free                     , xon);
+        xsw(im_free_with_destroy        , xon);
+        #undef  xon
 
-    // gc/private/tuple
-    #define xon    0
-    xsw(im_gc_tuple_routing         , xon);
-    #undef  xon
+        // gc/ref
+        #define xon    0
+        xsw(im_gc_$token_mix            , xon);
+        xsw(im_gc_$meta                 , xon);
+        xsw(im_gc_meta_routing_entry    , xon);
+        xsw(im_gc_meta_routing          , xon);
+        #undef  xon
 
-    #undef  xsw
-#else
-    #define xdebug(...)
+        // gc/private/tuple
+        #define xon    0
+        xsw(im_gc_tuple_routing         , xon);
+        #undef  xon
+
+        #undef  xsw
+    #else
+        #define xdebug(...)
+    #endif
 #endif
+
+
--- macro/xdebug_fail.hpp
+++ macro/xdebug_fail.hpp
@@ -1,18 +1,26 @@
-#pragma once
-#ifdef xenable_xdebug_fail
-    #include<stdio.h>
+#ifndef xpack_macro_xdebug_fail
+#define xpack_macro_xdebug_fail
+    #pragma warning(disable:4390)
+    #pragma push_macro("xuser")
+        #undef  xuser
+        #define xuser mixc::macro_xdebug_fail
+        #include"configure.hpp"
+        #include<stdio.h>
+    #pragma pop_macro("xuser")
 
-    #define xdebug_fail(...)                                                    \
-        if (__builtin_expect(([](const char * name, bool error){                \
-            if (not error){                                                     \
-                return false;                                                   \
-            }                                                                   \
-            else{                                                               \
-                printf(" FAIL | " __FILE__ ":%d\n", __LINE__);                  \
-                printf("      | func:%s expr:" # __VA_ARGS__ "\n", name);       \
-                return error;                                                   \
-            }                                                                   \
-        })(__func__, __VA_ARGS__), 0))
-#else
-    #define xdebug_fail(...)
+    #if xuse_xdebug_fail
+        #define xdebug_fail(...)                                                    \
+            if (__builtin_expect(([](const char * name, bool error){                \
+                if (not error){                                                     \
+                    return false;                                                   \
+                }                                                                   \
+                else{                                                               \
+                    printf(" FAIL | " __FILE__ ":%d\n", __LINE__);                  \
+                    printf("      | func:%s expr:" # __VA_ARGS__ "\n", name);       \
+                    return error;                                                   \
+                }                                                                   \
+            })(__func__, __VA_ARGS__), 0))
+    #else
+        #define xdebug_fail(...)
+    #endif
 #endif
