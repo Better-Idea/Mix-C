bc4fb44afd827479f8b62c9c2c6e728b623ea0de
PowerfulCat <x4495@outlook.com>
2020-05-06 22:50:06 +0800

    在 interface/ranger 中应用 matth/index_system

--- interface/ranger.hpp
+++ interface/ranger.hpp
@@ -6,6 +6,7 @@
         #include"define/base_type.hpp"
         #include"interface/initializer_list.hpp"
         #include"macro/xindex_rollback.hpp"
+        #include"math/index_system.hpp"
         #include"memop/signature.hpp"
         #include"memop/addressof.hpp"
         #include"meta/is_based_on.hpp"
@@ -28,7 +29,7 @@
         struct name : data {                                                \
             using base = data;                                              \
                                                                             \
-            name(owner const * ptr, item const &, uxx ofs, uxx len) :       \
+            name(owner const * ptr, item const & dummy, uxx ofs, uxx len) : \
                 base(ptr, ofs, len){}                                       \
             item & operator[](uxx index) {                                  \
                 return (*(owner *)base::ptr)__VA_ARGS__[base::ofs op index];\
@@ -92,20 +93,31 @@
         };
     }
 
-    #define xrange(...)                                                                 \
-    ::mixc::interface_ranger::ranger<__VA_ARGS__> range(ixx start, ixx end = -1) const {\
-        using namespace ::mixc::interface_ranger;                                       \
-        xindex_rollback(length(), start);                                               \
-        xindex_rollback(length(), end);                                                 \
-        if (start <= end){                                                              \
-            return positive{ this, this[0][0], uxx(start), uxx(end - start) + 1 };      \
-        }                                                                               \
-        else{                                                                           \
-            return negative{ this, this[0][0], uxx(start), uxx(start - end) + 1 };      \
-        }                                                                               \
+    #define xranger(...)                                                                    \
+    inc::ranger<__VA_ARGS__> range(inc::iinterval i){                                       \
+        using namespace ::mixc::interface_ranger;                                           \
+        if (i.normalize(the.length());                                                      \
+            i.left() <= i.right()){                                                         \
+            return positive{                                                                \
+                this,                                                                       \
+                this[0][0],                                                                 \
+                uxx(i.left()),                                                              \
+                uxx(i.left() - i.right()) + 1                                               \
+            };                                                                              \
+        }                                                                                   \
+        else{                                                                               \
+            return negative{                                                                \
+                this,                                                                       \
+                this[0][0],                                                                 \
+                uxx(i.left()),                                                              \
+                uxx(i.left() - i.right()) + 1                                               \
+            };                                                                              \
+        }                                                                                   \
     }
 #endif
 
 namespace xuser::inc{
     using ::mixc::interface_ranger::ranger;
 }
+
+#include"math/index_system.hpp"
