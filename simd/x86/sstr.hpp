2b6296125b23a3b569b299ebf711b44f377ebdba
PowerfulCat <x4495@outlook.com>
2020-05-08 12:59:09 +0800

    在 simd/x86/sstr 中新增 sstr_stou

--- simd/x86/sstr.hpp
+++ simd/x86/sstr.hpp
@@ -11,7 +11,12 @@
     #include"instruction/index_of_first_set.hpp"
     #include"instruction/index_of_last_set.hpp"
     #include"immintrin.h"
+    #include"math/smaller.hpp"
 #pragma pop_macro("xuser")
+
+namespace mixc::simd_x86_sstr{
+    inline static u32 base[] = { 1000000000, 100000000, 10000000, 1000000, 100000, 10000, 1000, 100, 10, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+}
 
 #include"beginc"
 
@@ -82,10 +87,36 @@ inline uxx sstr_index_of_last(sstr seq, char value){
     }
 }
 
+inline u32 sstr_stou(sstr seq){
+    if (seq.ptr[0] == '+'){
+        seq.ptr += 1;
+        seq.len -= 1;
+    }
+
+    auto max_length = 10;
+    auto offset     = max_length - seq.len;
+    auto zero       = _mm256_set1_epi8('0');
+    auto ymm0       = _mm256_loadu_si256((__m256i *)seq.ptr);
+    auto ymm1       = _mm256_sub_epi8(ymm0, zero);
+    auto full       = _mm256_cvtepi8_epi32(_mm256_extractf128_si256(ymm1, 0));
+    auto muli       = _mm256_loadu_si256((__m256i *)(mixc::simd_x86_sstr::base + offset));
+    auto part       = _mm256_mullo_epi32(full, muli);
+    auto sum0       = _mm_add_epi32(_mm256_extractf128_si256(part, 0), _mm256_extractf128_si256(part, 1));
+    auto sum1       = _mm_hadd_epi32(sum0, sum0);
+    auto sum2       = (sum1[0] >> 32) + (sum1[0] & 0xffffffff);
+    
+    if (offset == 0){
+        sum2 += _mm256_extract_epi8(ymm1, 8) * 10 + _mm256_extract_epi8(ymm1, 9);
+    }
+    else if (offset == 1){
+        sum2 += _mm256_extract_epi8(ymm1, 8);
+    }
+    return sum2;
+}
+
 inline ixx sstr_compare(sstr left, sstr right){
     using namespace simd::inc;
-    uxx r   = not_exist;
-    uxx len = left.len <= right.len ? left.len : right.len;
+    uxx len = smaller(left.len, right.len);
 
     for(uxx i = 0; i < len; i += sizeof(__m256i)){
         auto ymm0 = _mm256_loadu_si256((__m256i *)(left.ptr + i));
