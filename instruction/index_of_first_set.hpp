e996ebfb68edf6a872a5848b07458aff37d9f870
PowerfulCat <x4495@outlook.com>
2020-05-25 22:45:45 +0800

    糾正 instruction/index_of_xxx_set

--- instruction/index_of_first_set.hpp
+++ instruction/index_of_first_set.hpp
@@ -3,13 +3,26 @@
     #pragma push_macro("xuser")
         #undef  xuser
         #define xuser mixc::instruction_index_of_first_set
+        #include"configure.hpp"
         #include"define/base_type.hpp"
     #pragma pop_macro("xuser")
 
     namespace mixc::instruction_index_of_first_set{
         template<class type>
         inline uxx index_of_first_set(type v){
-            return __builtin_ffs(v) - 1;
+            uxx i;
+            if constexpr (xis_x86){
+                if constexpr (xis_os64){
+                    i = __builtin_ctzll(v);
+                }
+                else if constexpr (sizeof(v) == 8){
+                    i = u32(v) ? __builtin_ctz(u32(v)) : __builtin_ctz(v >> 32) + 32;
+                }
+                else {
+                    i = __builtin_ctz(v);
+                }
+            }
+            return v == 0 ? not_exist : i;
         }
     }
 
--- instruction/index_of_last_set.hpp
+++ instruction/index_of_last_set.hpp
@@ -3,13 +3,28 @@
     #pragma push_macro("xuser")
         #undef  xuser
         #define xuser mixc::instruction_index_of_last_set
+        #include"configure.hpp"
         #include"define/base_type.hpp"
     #pragma pop_macro("xuser")
 
     namespace mixc::instruction_index_of_last_set{
         template<class type>
         inline uxx index_of_last_set(type v){
-            return v == 0 ? not_exist : uxx(sizeof(v) * 8 - 1 - __builtin_clz(v));
+            uxx i;
+            if constexpr (xis_x86){
+                if constexpr (xis_os64){
+                    i = 63 - __builtin_clzll(v);
+                }
+                else if constexpr (sizeof(v) == 8){
+                    i = u64(v) >> 32 ? 
+                        31 - __builtin_clz(u64(v) >> 32) + 32:
+                        31 - __builtin_clz(u32(v));
+                }
+                else {
+                    i = 31 - __builtin_clz(v);
+                }
+            }
+            return v == 0 ? not_exist : i;
         }
     }
 
