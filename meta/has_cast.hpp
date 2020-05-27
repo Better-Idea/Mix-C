043bce19c50fa60b92ca1e91ae497e1054f01cc2
PowerfulCat <x4495@outlook.com>
2020-05-27 22:30:24 +0800

    支持 meta/has_cast concept

--- meta/has_cast.hpp
+++ meta/has_cast.hpp
@@ -3,27 +3,35 @@
     #pragma push_macro("xuser")
         #undef  xuser
         #define xuser mixc::meta_has_cast
+        #include"configure.hpp"
         #include"define/base_type.hpp"
     #pragma pop_macro("xuser")
 
     namespace mixc::meta_has_cast{
-        template<class target, class source>
-        struct meta{
-        private:
-            static u08 test(target){
-                return true;
-            }
-            static u16 test(...){
-                return false;
-            }
-        public:
-            static constexpr bool result = sizeof(
-                decltype(test(*(source *)nullptr))
-            ) == 1;
-        };
+        #if xfor_msvc_hint
+            template<class target, class source>
+            struct meta{
+            private:
+                static u08 test(target){
+                    return true;
+                }
+                static u16 test(...){
+                    return false;
+                }
+            public:
+                static constexpr bool result = sizeof(
+                    decltype(test(*(source *)nullptr))
+                ) == 1;
+            };
 
-        template<class target, class source>
-        constexpr bool has_cast = meta<target, source>::result;
+            template<class target, class source>
+            constexpr bool has_cast = meta<target, source>::result;
+        #else
+            template<class target, class source>
+            concept has_cast = requires(target tar, source src){
+                tar = src;
+            };
+        #endif
     }
 
 #endif
