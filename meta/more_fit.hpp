3a278cab41fdc23bbc28387069a036688ee85de6
PowerfulCat <x4495@outlook.com>
2020-05-27 22:31:15 +0800

    重写 meta/more_fit 以适应复杂情况

--- meta/more_fit.hpp
+++ meta/more_fit.hpp
@@ -5,32 +5,58 @@
         #define xuser mixc::meta_more_fit
         #include"define/base_type.hpp"
         #include"dumb/mirror.hpp"
+        #include"meta/has_cast.hpp"
+        #include"meta/is_same.hpp"
     #pragma pop_macro("xuser")
 
     namespace mixc::meta_more_fit{
-        template<uxx count, class source, class ... target> struct meta;
-        template<uxx count, class source, class first, class ... target>
-        struct meta<count, source, first, target...> : meta<count, source, target...>{
-            static auto test(first){
-                struct inner { u08 mem[count - sizeof...(target)]; };
-                return inner();
-            }
-            using meta<count, source, target...>::test;
+        template<
+            class the_target = void,
+            uxx  index_value = not_exist, 
+            uxx  size_different = uxx(-1), 
+            bool has_full_match = false>
+        struct record{
+            using target = the_target;
+            static constexpr uxx  index      = index_value;
+            static constexpr uxx  different  = size_different;
+            static constexpr bool full_match = has_full_match;
         };
 
+        template<uxx count, class source, class ... target> struct meta;
         template<uxx count, class source>
         struct meta<count, source>{
-            static constexpr u08 test(...){
-                return u08();
+            static constexpr auto test(){
+                return record<>();
             }
         };
 
-        template<class source, class first, class ... target>
-        constexpr uxx more_fit = sizeof(
-            decltype(
-                meta<1 + sizeof...(target), source, first, target...>::test(*(source *)nullptr)
-            )
-        ) - 1;
+        template<uxx count, class source, class target, class ... rest>
+        struct meta<count, source, target, rest...> : meta<count, source, rest...>{
+            static constexpr auto test(){
+                constexpr auto next = meta<count, source, rest...>::test();
+                constexpr auto curr_diff = 
+                    sizeof(target) > sizeof(source) ? 
+                    sizeof(target) - sizeof(source) : 
+                    sizeof(source) - sizeof(target);
+                using next_target = typename decltype(next)::target;
+
+                if  constexpr (inc::is_same<target, source>){
+                    return record<target, count - sizeof...(rest) - 1, curr_diff, true>();
+                }
+                else if constexpr (
+                    next.full_match or 
+                    next.different < curr_diff or 
+                    not inc::has_cast<target, source>){
+                    return next;
+                }
+                else{
+                    return record<target, count - sizeof...(rest) - 1, curr_diff>();
+                }
+            }
+        };
+
+        template<class source, class target, class ... rest>
+        constexpr uxx more_fit = meta<sizeof...(rest) + 1, source, target, rest...>::test().index;
     }
 
 #endif
