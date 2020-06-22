1843633f1901d0481aec00ebfe3f9cc30f891771
PowerfulCat <x4495@outlook.com>
2020-06-22 20:22:30 +0800

    在 macro/xgc 更改 xgc_fields 内部 routing 函数访问修饰符

--- macro/xgc.hpp
+++ macro/xgc.hpp
@@ -76,7 +76,7 @@
                     __xlist_core__(first_member_list_,member_list_,a0,a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15,a16,a17,a18,a19,a20,a21,a22,a23,a24,a25,a26,a27,a28,a29,a30,a31,a32,a33,a34,a35,a36,a37,a38,a39,a40,a41,a42,a43,a44,a45,a46,a47,a48,a49,a50,a51,a52,a53,a54,a55,a56,a57,a58,a59,a60,a61,a62,a63)                  \
                 >                                                                           \
             >::new_list;                                                                    \
-        private: template<class guide> ::mixc::gc_routing_result::routing_result routing()
+        protected: template<class guide> ::mixc::gc_routing_result::routing_result routing()
         
     #else
         #define xgc_fields(meta,...)    __xgc_fields__(meta,__VA_ARGS__,) 
@@ -96,7 +96,7 @@
                     __xlist__(first_member_list_,member_list_,__VA_ARGS__)                      \
                 >                                                                               \
             >::new_list;                                                                        \
-        private: template<class guide> ::mixc::gc_routing_result::routing_result routing()
+        protected: template<class guide> ::mixc::gc_routing_result::routing_result routing()
     #endif
 
     #define xiam(...)        iam__(__VA_ARGS__)
