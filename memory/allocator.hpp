50449b20efdea2dec6b802f6337bbd6a0d43af8c
PowerfulCat <x4495@outlook.com>
2020-06-25 12:23:11 +0800

    在 memory/allocator 中对接 memory/private/tiny_allocator

--- memory/allocator.hpp
+++ memory/allocator.hpp
@@ -1,18 +1,20 @@
 #ifndef xpack_memory_alloctor
 #define xpack_memory_alloctor
     #pragma push_macro("xuser")
-        #undef  xuser
-        #define xuser mixc::memory_alloctor
-        #include"define/base_type.hpp"
-        #include"macro/xdebug.hpp"
-        #include"macro/xtypeid.hpp"
-        #include"memory/new.hpp"
-        #include"lock/atom_add.hpp"
-        #include"lock/atom_sub.hpp"
-        #include<malloc.h>
-    #pragma pop_macro("xuser")
+    #undef  xuser
+    #define xuser mixc::memory_alloctor
+    #include"define/base_type.hpp"
+    #include"macro/xdebug.hpp"
+    #include"macro/xtypeid.hpp"
+    #include"memory/new.hpp"
+    #include"memory/private/tiny_allocator.hpp"
 
     namespace mixc::memory_alloctor{
+        // 单线程
+        inline static inc::classifier mem;
+    }
+
+    namespace mixc::memory_alloctor::origin{
         struct memory_size{
             explicit memory_size(uxx size) : 
                 size(size){
@@ -26,9 +28,11 @@
 
         inline uxx __used_mem;
 
-        inline void * malloc(uxx bytes){
-            inc::atom_add(& __used_mem, uxx(bytes));
-            return ::malloc(bytes);
+        template<class type, class ... args>
+        inline type * alloc_with_initial(memory_size bytes, args const & ... list){
+            auto ptr = (type *)mem.alloc(bytes);
+            xdebug(im_alloc_with_initial, xtypeid(type).name, ptr, bytes);
+            return new(ptr) type(list...);
         }
 
         template<class type, class ... args>
@@ -36,18 +40,11 @@
             return alloc_with_initial<type>(memory_size(sizeof(type)), list...);
         }
 
-        template<class type, class ... args>
-        inline type * alloc_with_initial(memory_size bytes, args const & ... list){
-            auto mem = (type *)malloc(bytes);
-            xdebug(im_alloc_with_initial, xtypeid(type).name, mem, bytes);
-            return new(mem) type(list...);
-        }
-
         template<class type>
         inline type * alloc(memory_size bytes){
-            auto mem = (type *)malloc(bytes);
-            xdebug(im_alloc, xtypeid(type).name, mem, bytes);
-            return mem;
+            auto ptr = (type *)mem.alloc(bytes);
+            xdebug(im_alloc, xtypeid(type).name, ptr, bytes);
+            return ptr;
         }
 
         template<class type>
@@ -56,10 +53,9 @@
         }
 
         template<class type>
-        inline void free(type * mem, memory_size bytes){
-            xdebug(im_free, xtypeid(type).name, mem, bytes);
-            ::free(mem);
-            inc::atom_sub(& __used_mem, uxx(bytes));
+        inline void free(type * ptr, memory_size bytes){
+            xdebug(im_free, xtypeid(type).name, ptr, bytes);
+            mem.free(ptr, bytes);
         }
 
         template<class type>
@@ -79,16 +75,17 @@
         }
 
         inline uxx used_bytes(){
-            return __used_mem;
+            return mem.used_bytes();
+        }
+
+        inline uxx need_free_count(){
+            return mem.need_free_count();
         }
     }
+    #pragma pop_macro("xuser")
+
 #endif
 
 namespace xuser::inc{
-    using ::mixc::memory_alloctor::alloc;
-    using ::mixc::memory_alloctor::alloc_with_initial;
-    using ::mixc::memory_alloctor::free;
-    using ::mixc::memory_alloctor::free_with_destroy;
-    using ::mixc::memory_alloctor::used_bytes;
-    using ::mixc::memory_alloctor::memory_size;
+    using namespace ::mixc::memory_alloctor::origin;
 }
