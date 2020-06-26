1ea89c36be3e17e9d02703ab70ddb9d10373ddae
PowerfulCat <x4495@outlook.com>
2020-06-26 18:57:17 +0800

    简化 gc/self_management 接口要求

--- gc/self_management.hpp
+++ gc/self_management.hpp
@@ -2,31 +2,27 @@
 在 GC 组件中，self_management用于判定一个类是否带有 GC 成分
 判定方法 is_based_on：
     我们定义：只要是继承了 self_management 的类都应该具有以下私有方法
-        - routing<guide>
+        - template<class guide> bool routing<guide>();
 */
 
 #ifndef xpack_gc_self_management
 #define xpack_gc_self_management
     #pragma push_macro("xuser")
-        #undef  xuser
-        #define xuser mixc::gc_self_management
-        #include"define/base_type.hpp"
-        #include"gc/private/routing_result.hpp"
-        #include"macro/xgc.hpp"
-    #pragma pop_macro("xuser")
+    #undef  xuser
+    #define xuser mixc::gc_self_management
+    #include"define/base_type.hpp"
+    #include"macro/xgc.hpp"
 
-    namespace mixc::gc_self_management{
+    namespace xuser::origin{
         struct self_management{
             xgc_fields(
                 xiam(self_management)
             );
         };
     }
-
+    #pragma pop_macro("xuser")
 #endif
 
-#include"gc/private/routing_result.hpp"
-
 namespace xuser::inc{
-    using ::mixc::gc_self_management::self_management;
+    using namespace ::mixc::gc_self_management::origin;
 }
