ba8f50b698a403c58cd5fcd563778b4d7527a458
PowerfulCat <x4495@outlook.com>
2020-06-26 22:21:18 +0800

    提供 debug/release 版 talk_multi_barrier 用于讨论 multi_barrier 的可用性

---
+++ talk_multi_barrier.cpp
@@ -0,0 +1,48 @@
+#ifndef xpack_talk_multi_barrier
+#define xpack_talk_multi_barrier
+    #pragma push_macro("xuser")
+    #undef  xuser
+    #define xuser mixc::talk_multi_barrier
+    #include<thread>
+    #include"define/base_type.hpp"
+    #include"io/tty.hpp"
+    #include"lock/multi_barrier.hpp"
+
+    namespace xuser{
+        volatile uxx cnt = 0;
+        inc::multi_barrier mutex;
+
+        void test(){
+            using namespace inc;
+            auto add = [](voidp key){
+                for(uxx i = 1; i <= 10000000; i++){
+                    while(mutex.try_lock(uxx(key)) == lock_state_t::blocked){
+                        std::this_thread::yield();
+                    }
+                    if (i % 10000 == 0){
+                        tty.write_line(key, ":", i);
+                    }
+                    cnt = cnt + 1;
+                    mutex.unlock(uxx(key));
+                }
+            };
+
+            std::thread a(add, voidp(59369));
+            std::thread b(add, voidp(59377));
+            std::thread c(add, voidp(59387));
+            std::thread d(add, voidp(59393));
+
+            a.join();
+            b.join();
+            c.join();
+            d.join();
+            tty.write_line(cnt);
+        }
+    }
+
+    int main(){
+        xuser::test();
+        return 0;
+    }
+    #pragma pop_macro("xuser")
+#endif
