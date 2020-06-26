e17172daf4bb1df497ab34ecbed7a549a2ed42c6
PowerfulCat <x4495@outlook.com>
2020-06-26 19:17:16 +0800

    在 gc/private/tuple 使用简单的结构

--- gc/private/tuple.hpp
+++ gc/private/tuple.hpp
@@ -28,23 +28,23 @@ tuple<foo::member_list> xx;
 #ifndef xpack_gc_tuple
 #define xpack_gc_tuple
     #pragma push_macro("xuser")
-        #undef  xuser
-        #define xuser mixc::gc_tuple
-        #include"define/base_type.hpp"
-        #include"gc/self_management.hpp"
-        #include"macro/xdebug.hpp"
-        #include"memop/cast.hpp"
-        #include"meta/is_class.hpp"
-        #include"meta/is_based_on.hpp"
-        #include"meta/remove_membership.hpp"
-        #include"meta_seq/tlist.hpp"
-        #include"meta_seq/vlist.hpp"
-        #include"meta_seq/tin.hpp"
-    #pragma pop_macro("xuser")
+    #undef  xuser
+    #define xuser mixc::gc_tuple
+    #include"define/base_type.hpp"
+    #include"gc/self_management.hpp"
+    #include"macro/xdebug.hpp"
+    #include"memop/cast.hpp"
+    #include"meta/is_class.hpp"
+    #include"meta/is_based_on.hpp"
+    #include"meta/remove_membership.hpp"
+    #include"meta_seq/tlist.hpp"
+    #include"meta_seq/vlist.hpp"
+    #include"meta_seq/tin.hpp"
 
+    // 成员变量指针的魔法
     #define xroot   ((*(root_t *)this).*first)
 
-    namespace mixc::gc_tuple{
+    namespace xuser::origin{
         using namespace inc;
 
         template<class root_t, class member_list> union tuple;
@@ -54,41 +54,39 @@ tuple<foo::member_list> xx;
             tuple<root_t, vlist<values...>> next;
             using origin = typename remove_membership<decltype(first)>::result;
         public:
-            template<class guide> routing_result routing(){
-                routing_result r = { 0 };
+            template<class guide> bool routing(){
+                bool can_arrive_root = false;
+                xdebug(im_gc_tuple_routing, xtypeid(origin).name);
 
                 if constexpr (is_class<origin>){
                     if constexpr (is_based_on<self_management, origin>){
                         if constexpr (tin<guide, origin>){
-                            xdebug(im_gc_tuple_routing, xtypeid(origin).name);
-
-                            if (r = xroot.template routing<guide>(); r.can_arrive_root) {
-                                r.degree_dvalue -= 1; // 如果 item 可以到达根节点，那么该 tuple 就存在 1 条出度
-                                xdebug(im_gc_tuple_routing, xtypeid(origin).name, r.degree_dvalue);
-                            }
+                            can_arrive_root = xroot.template routing<guide>();
                         }
                     }
                     else{
                         using member_tuple = tuple<origin, typename origin::member_list>;
-                        r = cast<member_tuple>(xroot).template routing<guide>();
+                        can_arrive_root = cast<member_tuple>(xroot).template routing<guide>();
                     }
                 }
-                
-                r += next.template routing<guide>();
-                return r;
+
+                xdebug(im_gc_tuple_routing, xtypeid(origin).name, can_arrive_root);
+                can_arrive_root |= next.template routing<guide>();
+                return can_arrive_root;
             }
         };
 
         template<class root_t>
         union tuple<root_t, vlist<>>{
-            template<class guide> routing_result routing(){
-                return { 0 };
+            template<class guide> bool routing(){
+                return false;
             }
         };
     }
-
+    #undef  xroot
+    #pragma pop_macro("xuser")
 #endif
 
 namespace xuser::inc{
-    using ::mixc::gc_tuple::tuple;
+    using namespace ::mixc::gc_tuple::origin;
 }
