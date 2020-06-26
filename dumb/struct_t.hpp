61cf094d5a35c4859cf8e1ad815747e757f27a1f
PowerfulCat <x4495@outlook.com>
2020-06-26 18:49:46 +0800

    在 dumb/struct_t 中支持 void 模板特化

--- dumb/struct_t.hpp
+++ dumb/struct_t.hpp
@@ -1,14 +1,13 @@
 #ifndef xpack_dumb_struct_t
 #define xpack_dumb_struct_t
     #pragma push_macro("xuser")
-        #undef  xuser
-        #define xuser mixc::dumb_struct_t
-        #include"define/base_type.hpp"
-        #include"macro/xgc.hpp"
-        #include"meta/is_class.hpp"
-    #pragma pop_macro("xuser")
+    #undef  xuser
+    #define xuser mixc::dumb_struct_t
+    #include"define/base_type.hpp"
+    #include"macro/xgc.hpp"
+    #include"meta/is_class.hpp"
 
-    namespace mixc::dumb_struct_t{
+    namespace xuser::origin{
         template<class type, bool is_class> struct struct_t;
         template<class type>
         struct struct_t<type, true> : type {
@@ -44,12 +43,19 @@
                 return data;
             }
         };
-    }
 
+        template<>
+        struct struct_t<void, false>{
+            xgc_fields(
+                xiam(struct_t<void, false>)
+            );
+        };
+    }
+    #pragma pop_macro("xuser")
 #endif
 
 namespace xuser::inc{
     template<class type>
-    using struct_t = ::mixc::dumb_struct_t::
+    using struct_t = ::mixc::dumb_struct_t::origin::
         struct_t<type, mixc::dumb_struct_t::inc::is_class<type>>;
 }
