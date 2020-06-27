81549072145f50cc129ddc68fabb1ebaee9c6d94
PowerfulCat <x4495@outlook.com>
2020-06-27 17:17:41 +0800

    macro/xdebug 进化

--- macro/xdebug.hpp
+++ macro/xdebug.hpp
@@ -2,64 +2,77 @@
 #define xpack_macro_xdebug
     #pragma warning(disable:4390)
     #pragma push_macro("xuser")
-        #undef  xuser
-        #define xuser mixc::macro_xdebug
-        #include"configure.hpp"
-        #include"macro/xtypeid.hpp"
-        #include"macro/private/log.hpp"
+    #undef  xuser
+    #define xuser mixc::macro_xdebug
+    #include"configure.hpp"
+    #include"macro/xlink.hpp"
+    #include"macro/xtypeid.hpp"
+    #include"macro/private/log.hpp"
     #pragma pop_macro("xuser")
 
     #if xuse_xdebug
         #define xdebug(token,...)                               \
-        if (token) {                                            \
+        if (token > 0 and ([&](){                               \
             ::mixc::macro_private_log::log(                     \
             __debug_id += 1,                                    \
             __FILE__,                                           \
             __LINE__,                                           \
             "func, " #__VA_ARGS__ ",", __func__, __VA_ARGS__);  \
-        }                                                       \
-        if (token)
-
-        #define xsw(token,...)            constexpr bool token = { __VA_ARGS__ + 0 };
+            return true;                                        \
+        })())
 
         inline int __debug_id;
 
-        // docker/private/hashmap.hpp
-        #define xon    0
-        xsw(im_docker_hashmap_node_set                      , xon);
-        xsw(im_docker_hashmap_set                           , xon);
-        xsw(im_docker_hashmap_get                           , xon);
-        xsw(im_docker_hashmap_addressing                    , xon);
-        #undef  xon
+        // 在 xon 后面的枚举是启用 xdebug 打印的
+        #define xon         xlink2(__enable, __COUNTER__)       = 0
 
-        #define xon     1
-        xsw(im_memory_classifier_take_out                   , xon);
-        xsw(im_memory_classifier_free                       , xon);
-        xsw(im_memory_classifier_split                      , xon);
-        xsw(im_memory_classifier_append                     , xon);
-        #undef  xon
+        // 在 xoff 后面的枚举是禁用 xdebug 打印的
+        #define xoff        xlink2(__disable, __COUNTER__)      = -100000000
 
-        // memop/memory.hpp
-        #define xon    1
-        xsw(im_alloc_with_initial                           , xon);
-        xsw(im_alloc                                        , xon);
-        xsw(im_free                                         , xon);
-        xsw(im_free_with_destroy                            , xon);
-        #undef  xon
+        enum{
+            // docker/private/hashmap.hpp
+            xon,
+            xoff,
+                im_docker_hashmap_node_set,
+                im_docker_hashmap_set,
+                im_docker_hashmap_get,
+                im_docker_hashmap_addressing,
 
-        // gc/ref
-        #define xon    1
-        xsw(im_gc_$token_mix                                , xon);
-        xsw(im_gc_$meta                                     , xon);
-        xsw(im_gc_meta_routing                              , xon);
-        #undef  xon
+            // memory/tiny_allocator
+            xon,
+                im_memory_tiny_allocator_take_out,
+                im_memory_tiny_allocator_free,
+            xoff,
+                im_memory_tiny_allocator_split,
+                im_memory_tiny_allocator_append,
 
-        // gc/private/tuple
-        #define xon    1
-        xsw(im_gc_tuple_routing                             , xon);
-        #undef  xon
+            // memop/memory.hpp
+            xon,
+                im_alloc_with_initial,
+                im_alloc,
+                im_free,
+                im_free_with_destroy,
+            xoff,
 
-        #undef  xsw
+            // gc/ref
+            xon,
+                im_gc_$token_mix,
+                im_gc_$meta,
+                im_gc_meta_routing,
+            xoff,
+
+            // gc/private/tuple
+            xon,
+                im_gc_tuple_routing,
+            xoff,
+
+            // 模板
+            xon,
+            xoff,
+        };
+
+        #undef  xon
+        #undef  xoff
     #else
         #define xdebug(...)
     #endif
