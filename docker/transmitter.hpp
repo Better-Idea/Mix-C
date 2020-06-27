0cc02b0da002b24df8c52a9f5e26b0e8575eff61
PowerfulCat <x4495@outlook.com>
2020-06-27 23:30:42 +0800

    简化 docker/transmitter

--- docker/transmitter.hpp
+++ docker/transmitter.hpp
@@ -1,62 +1,45 @@
 #ifndef xpack_docker_transmitter
 #define xpack_docker_transmitter
     #pragma push_macro("xuser")
-        #undef  xuser
-        #define xuser mixc::docker_transmitter
-        #include"define/base_type.hpp"
-        #include"dumb/mirror.hpp"
-        #include"macro/xgc.hpp"
-        #include"macro/xis_nullptr.hpp"
-        #include"memop/addressof.hpp"
-        #include"memop/copy.hpp"
-        #include"memory/new.hpp"
-    #pragma pop_macro("xuser")
+    #undef  xuser
+    #define xuser mixc::docker_transmitter
+    #include"define/base_type.hpp"
+    #include"dumb/struct_t.hpp"
 
-    namespace mixc::docker_transmitter{
+    namespace xuser::origin{
         template<class type>
-        struct transmitter{
-            xgc_fields(
-                xiam(transmitter<type>),
-                xpri(data,    inc::mirror<type>),
-                xpri(is_none, mutable bool)
-            );
+        struct transmitter : inc::struct_t<type>{
+        private:
+            bool phas_hold_value = true;
         public:
-
-            transmitter() : 
-                is_none(true) {}
-
-            transmitter(type const & value){
-                inc::copy(xref data, value);
-                is_none = false;
+            transmitter(){}
+            transmitter(type const & value) : 
+                inc::struct_t<type>(value){
+                phas_hold_value = false;
             }
 
-            transmitter(transmitter<type> const & value) {
-                inc::copy(xref data, value.data);
-                value.is_none = true;
+            auto & operator=(type const & value){
+                auto & ori = *(type *)this;
+                ori = value;
+                phas_hold_value = true;
+                return this[0];
             }
 
-            ~transmitter(){
-                if (not is_none){
-                    ((type &)data).~type();
-                }
+            auto & operator=(transmitter<type> const & value){
+                this[0] = (inc::struct_t<type> &) value;
+                phas_hold_value = value.phas_hold_value;
+                return this[0];
             }
 
-            void operator=(type const & value) {
-                this->~transmitter();
-                new (this) transmitter(value);
+            bool has_hold_value() const {
+                return phas_hold_value;
             }
-
-            operator type & (){
-                is_none = true;
-                return data;
-            }
-
-            xis_nullptr(is_none == true);
         };
     }
 
+    #pragma pop_macro("xuser")
 #endif
 
 namespace xuser::inc{
-    using mixc::docker_transmitter::transmitter;
+    using namespace ::mixc::docker_transmitter::origin;
 }
