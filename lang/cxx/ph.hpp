e3acfcf990a4ff442ae303a2e49fb93ccaf904dd
PowerfulCat <x4495@outlook.com>
2020-06-29 13:05:46 +0800

    增强 lang/cxx/ph，占位符 v 现在支持把多个元素看作一个整体来对齐

--- lang/cxx/ph.hpp
+++ lang/cxx/ph.hpp
@@ -9,86 +9,89 @@
     #undef  xusing_lang_cxx
     #undef  xuser
     #define xuser mixc::lang_cxx_ph
-    #include"define/base_type.hpp"
     #include"interface/can_alloc.hpp"
     #include"lang/cxx/strlize.hpp"
     #include"lang/cxx.hpp"
     #include"macro/xdebug_fail.hpp"
-    #include"memop/addressof.hpp"
+    #include"memop/cast.hpp"
     #include"memop/copy.hpp"
     #include"memop/fill.hpp"
     #include"meta/is_based_on.hpp"
     #include"meta/is_ptr.hpp"
     #include"meta/unsigned_type.hpp"
+    #include"mixc.hpp"
 
     namespace xuser{
         /*
-        * h{}      hex
-        * H{}      upper hex 
-        * zh{}     hex with leading zero
-        * zH{}     upper hex with leading zero
-        * x{}      hex with 0x prefix
-        * X{}      upper hex with 0x prefix
-        * zx{}     hex with 0x prefix and leading zero
-        * zX{}     upper hex with 0x prefix and leading zero
-        * 
-        * v{}      normal value
-        * d{}      dec(pending)
-        * 
-        * b{}      bin
-        * zb{}     bin with leading zero
-        * 
-        * .l(5)    align left
-        * .r(5)    align right
-        * .c(5)    align center
-        * 
-        * more... to be continue
-        */
+         * h{}  hex
+         * H{}  upper hex 
+         * zh{} hex with leading zero
+         * zH{} upper hex with leading zero
+         * x{}  hex with 0x prefix
+         * X{}  upper hex with 0x prefix
+         * zx{} hex with 0x prefix and leading zero
+         * zX{} upper hex with 0x prefix and leading zero
+         * 
+         * v{}  normal multi-value
+         * f{}  decimal(pending)
+         * 
+         * b{}  bin
+         * zb{} bin with leading zero
+         * 
+         * .l() align left
+         * .r() align right
+         * .c() align center
+         * 
+         * more... to be continue
+         */
 
         struct place_holder_group{};
 
-        template<class final, class type>
-        struct base_ph : place_holder_group{
+        template<class final, class type = void> struct base_ph;
+        template<class final>
+        struct base_ph<final, void> : place_holder_group{
         protected:
-            type        value;
-            uxx         align_mode          : 2  = 0;
-            uxx         numeration          : 6  = 10;
-            uxx         align_width              = 0;
+            u08         align_mode          : 2  = 0;
+            u08         numeration          : 6  = 10;
+            char        left_padding_char        = ' ';
+            char        right_padding_char       = ' ';
+            u32         align_width              = 0;
 
             enum{
                 align_center,
                 align_left,
                 align_right,
             };
+
+            xgc_fields(
+                xiam(base_ph<final, void>)
+            );
         public:
-            base_ph(){}
-            base_ph(type const & value) : 
-                value(value){}
-
-            final & c(uxx align_center_width){
-                align_mode  = align_center;
-                align_width = align_center_width;
+            final & c(uxx align_center_width, char left_padding_char = ' ', char right_padding_char = ' '){
+                the.align_mode              = align_center;
+                the.align_width             = align_center_width;
+                the.left_padding_char       = left_padding_char;
+                the.right_padding_char      = right_padding_char;
                 return thex;
             }
 
-            final & l(uxx align_left_width){
-                align_mode  = align_left;
-                align_width = align_left_width;
+            final & c(uxx align_center_width, char padding_char = ' '){
+                return c(align_center_width, padding_char, padding_char);
+            }
+
+            final & l(uxx align_left_width, char padding_char = ' '){
+                the.align_mode              = align_left;
+                the.align_width             = align_left_width;
+                the.right_padding_char      = padding_char;
                 return thex;
             }
 
-            final & r(uxx align_right_width){
-                align_mode  = align_right;
-                align_width = align_right_width;
+            final & r(uxx align_right_width, char padding_char){
+                the.align_mode              = align_right;
+                the.align_width             = align_right_width;
+                the.left_padding_char       = padding_char;
                 return thex;
             }
-        };
-
-        template<class final, class type>
-        struct basex_ph : base_ph<final, type>{
-            using the_t = base_ph<final, type>;
-            using the_t::the_t;
-            basex_ph(){}
 
             template<class item_t>
             item_t * align(uxx length, inc::can_alloc<item_t> alloc){
@@ -102,20 +105,31 @@
 
                 switch(the_t::align_mode){
                 case the_t::align_center:
-                    inc::fill_with_operator(mem, ' ', half);
-                    inc::fill_with_operator(length + (mem += half), ' ', pad_width - half);
+                    inc::fill_with_operator(mem, left_padding_char, half);
+                    inc::fill_with_operator(length + (mem += half), right_padding_char, pad_width - half);
                     return mem;
                 case the_t::align_left:
-                    inc::fill_with_operator(mem + length, ' ', pad_width);
+                    inc::fill_with_operator(mem + length, right_padding_char, pad_width);
                     return mem;
                 default: // align_right
-                    inc::fill_with_operator(mem, ' ', pad_width);
+                    inc::fill_with_operator(mem, left_padding_char, pad_width);
                     return mem + pad_width;
                 }
             }
         };
 
-        #define xbase   (*(basex_ph<decltype(*this), type> *)this)
+        template<class final, class type>
+        struct base_ph : base_ph<final, void>{
+            xgc_fields(
+                xiam(base_ph),
+                xpro(value, type)
+            );
+        public:
+            base_ph(){}
+            base_ph(type const & value) : 
+                value(value){}
+        };
+
         #define xopt                                                                            \
             inc::c08 operator >> (inc::can_alloc<char> alloc) { return output(alloc); }         \
             inc::c16 operator >> (inc::can_alloc<char16_t>  alloc) { return output(alloc); }    \
@@ -143,7 +157,7 @@
                         return the_t::value;
                     }
                 };
-                return inc::cxx<item_t>(deformation(), n,  lut, [this, alloc](uxx length){
+                return inc::cxx<item_t>(deformation(), n, lut, [this, alloc](uxx length){
                     auto klz_length = sizeof(type) * 8; // keep leading zero length
 
                     if constexpr (n == inc::numeration::hex){
@@ -153,12 +167,12 @@
                         klz_length = klz_length / 3 + (klz_length % 3 != 0);
                     }
                     else if constexpr (n == inc::numeration::bin){
-                        klz_length = klz_length;
+                        ; // klz_length = klz_length;
                     }
 
                     auto new_length = (keep_leading_zero ? klz_length : length);
                     auto zero_count = new_length - length;
-                    auto mem        = xbase.template align<item_t>(
+                    auto mem        = the.template align<item_t>(
                         new_length += (with_prefix ? 2 : 0), 
                         alloc
                     );
@@ -174,6 +188,49 @@
                     return mem;
                 });
             }
+        };
+
+        
+        template<class final, class ... args> struct v;
+        template<class final, class a0, class ... args>
+        struct v<final, a0, args...> : v<final, args...> {
+            using base_t = v<final, args...>;
+
+            v(){}
+            v(a0 const & first, args const & ... rest) : 
+                item(first), base_t(rest...){
+            }
+
+            template<class item_t>
+            inc::cxx<item_t> output(uxx length){
+                inc::cxx<item_t> buf;
+                inc::cxx<item_t>(item, [&](uxx this_length){
+                    buf = base_t::template output<item_t>(length + this_length);
+                    buf = buf.forward(this_length);
+                    return (item_t *)buf;
+                });
+                return buf;
+            }
+        private:
+            a0 item;
+        };
+
+        template<class final>
+        struct v<final> : base_ph<final> {
+            using the_t = base_ph<final>;
+
+            template<class item_t>
+            inc::cxx<item_t> output(uxx length){
+                return { 
+                    the.template align<item_t>(
+                        length, 
+                        inc::cast<inc::can_alloc<item_t>>(alloc)
+                    ) + length, 
+                    0
+                };
+            }
+        protected:
+            inc::can_alloc<void> alloc;
         };
 
         constexpr bool with_prefix       = true;
@@ -201,7 +258,6 @@
 
     namespace xuser::ph{
         using xuser::place_holder_group;
-        template<class> struct v;
         template<class> struct zx;
 
         xhex(h , not with_prefix, not keep_leading_zero, inc::lower);
@@ -220,17 +276,16 @@
         xbin(b , not with_prefix, not keep_leading_zero, inc::lower);
         xbin(zb, not with_prefix,     keep_leading_zero, inc::lower);
 
-        template<class type>
-        struct v : base_ph<v<type>, type>{
-            using the_t = base_ph<v<type>, type>;
+        template<class a0, class ... args>
+        struct v : xuser::v<v<a0, args...>, a0, args...>{
+            using base_t = xuser::v<v<a0, args...>, a0, args...>;
             v(){}
-            v(type const & value)
-                : the_t(value){}
+            v(a0 const & first, args const & ... rest) : 
+                base_t(first, rest...){}
 
             xopt {
-                return inc::cxx<item_t>(the_t::value, [this, alloc](uxx length){
-                    return xbase.template align<item_t>(length, alloc);
-                });
+                inc::copy(xref base_t::alloc, alloc);
+                return base_t::template output<item_t>(0);
             }
         };
 
@@ -284,7 +339,7 @@
         };
     }
 
-    namespace mixc::lang_cxx_ph::ph{ // place_holder
+    namespace xuser::ph{ // place_holder
         template<class a0, class ... args>
         struct phg{ // place_holder group
             phg(a0 const & first, args const & ... list)
@@ -314,4 +369,5 @@
 namespace xuser::inc::ph{
     using namespace ::mixc::lang_cxx_ph::ph;
 }
+
 
