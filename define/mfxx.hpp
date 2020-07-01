ad68c0c4e5dc0f3fe005e315e4565132d6151a34
PowerfulCat <x4495@outlook.com>
2020-07-01 21:17:40 +0800

    规格化 define/mfxx

--- define/mfxx.hpp
+++ define/mfxx.hpp
@@ -1,54 +1,46 @@
 #ifndef xpack_define_mfxx
 #define xpack_define_mfxx
     #pragma push_macro("xuser")
-        #undef  xuser
-        #define xuser mixc::define_mfxx
-        #include"define/base_type.hpp"
-        #include"macro/xgc.hpp"
-    #pragma pop_macro("xuser")
-
-    namespace mixc::define_mfxx{
+    #undef  xuser
+    #define xuser mixc::define_mfxx
+    #include"define/base_type.hpp"
+    #include"macro/xgc.hpp"
+    
+    namespace xuser::origin{
         template<
             class   float_type, 
             class   equivalent_type, 
             uxx     decimal_bits, 
             uxx     exp_bits, 
             uxx     exp_offset>
-        struct mfxx{
-            xgc_fields(
-                xiam(mfxx<
-                    float_type,
-                    equivalent_type,
-                    decimal_bits,
-                    exp_bits,
-                    exp_offset
-                >)
-            );
-        public:
-            union {
-                struct {
-                    equivalent_type decimal : decimal_bits;
-                    equivalent_type exp     : exp_bits;
-                    equivalent_type sign    : 1;
-                };
-                float_type value;
+        union mfxx{
+            using the_t = mfxx<
+                float_type,
+                equivalent_type,
+                decimal_bits,
+                exp_bits,
+                exp_offset
+            >;
 
-                struct {
-                    operator ixx () const {
-                        return ixx(the.exp) - exp_offset;
-                    }
-
-                    void operator = (ixx value) const {
-                        the.exp = value + exp_offset;
-                    }
-                } real_exp;
-
-                struct {
-                    operator u64 () const {
-                        return u64(1) << decimal_bits | the.decimal;
-                    }
-                } real_decimal;
+            struct {
+                equivalent_type decimal : decimal_bits;
+                equivalent_type exp     : exp_bits;
+                equivalent_type sign    : 1;
             };
+
+            float_type value;
+
+            the_t & real_exp(ixx value){
+                the.exp = value + exp_offset;
+            }
+
+            ixx real_exp() const {
+                return ixx(the.exp) - exp_offset;
+            }
+
+            ixx real_dec() const {
+                return u64(1) << decimal_bits | the.decimal;
+            }
 
             mfxx() : value(0) { }
             mfxx(float_type value) : value(value) { }
@@ -69,9 +61,9 @@
         using mf64 = mfxx<f64, u64, 52, 11, 1023>;
     }
 
+    #pragma pop_macro("xuser")
 #endif
 
 namespace xuser::inc{
-    using ::mixc::define_mfxx::mf32;
-    using ::mixc::define_mfxx::mf64;
+    using namespace ::mixc::define_mfxx::origin;
 }
