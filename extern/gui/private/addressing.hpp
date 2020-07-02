b4f0b48e3912a3db305fde18930c39c86ee3fe02
PowerfulCat <x4495@outlook.com>
2020-07-02 19:21:22 +0800

    创建 extern/gui/ 用于图形化编程创作，并在 private/ 目录添加 addressing，用于将二维索引一维化

---
+++ extern/gui/private/addressing.hpp
@@ -0,0 +1,35 @@
+#ifndef xpack_extern_gui_private_addressing
+#define xpack_extern_gui_private_addressing
+    #pragma push_macro("xuser")
+    #undef  xuser
+    #define xuser mixc::extern_gui_private_addressing
+    #include"mixc.hpp"
+
+    namespace xuser::origin {
+        template<uxx arg_width, uxx arg_height>
+        struct addressing{
+        protected:
+            uxx addressing(uxx x, uxx y){
+                return y * arg_width + x;
+            }
+        public:
+            constexpr uxx width(){
+                return arg_width;
+            }
+
+            constexpr uxx height(){
+                return arg_height;
+            }
+
+            constexpr uxx pixels(){
+                return arg_width * arg_height;
+            }
+        };
+    }
+
+    #pragma pop_macro("xuser")
+#endif
+
+namespace xuser::inc {
+    using namespace ::mixc::extern_gui_private_addressing::origin;
+}
