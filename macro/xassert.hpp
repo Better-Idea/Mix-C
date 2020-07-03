b316ee4a6bb328a962d1432c2ec836e6570e005a
PowerfulCat <x4495@outlook.com>
2020-07-03 22:52:06 +0800

    在 macro/xassert 中使用 tty 代替 printf

--- macro/xassert.hpp
+++ macro/xassert.hpp
@@ -1,15 +1,15 @@
 #ifndef xpack_macro_xassert
 #define xpack_macro_xassert
     #pragma push_macro("xuser")
-        #undef  xuser
-        #define xuser mixc::macro_xassert
-        #include"define/base_type.hpp"
-        #include"io/tty.hpp"
-        #include"io/private/tty.hpp"
-        #include"macro/private/log.hpp"
-        #include"macro/xlink.hpp"
+    #undef  xuser
+    #define xuser mixc::macro_xassert
+    #include"define/base_type.hpp"
+    #include"io/tty.hpp"
+    #include"io/private/tty.hpp"
+    #include"macro/private/log.hpp"
+    #include"macro/xlink.hpp"
     #pragma pop_macro("xuser")
-
+    
     namespace mixc::macro_xassert{
         struct result{
             u32 error_threshold;
@@ -18,36 +18,40 @@
 
             result(asciis name, uxx error_threshold = 128) : 
                 error_threshold(error_threshold){
-                inc::print_core(" TEST | %-24s\n", name);
+                inc::tty.write_line("TEST | ", name);
             }
 
             ~result(){
                 asciis msg[] = { "fail", "pass" };
-                inc::print_core("      | case:%-9u %s\n", case_count, msg[error_count == 0]);
+                inc::tty.write_line("     | case:", case_count, " ", msg[error_count == 0]);
             }
         };
     }
 
     #define xassert(condition,...)                                                                      \
-    if (([&](){                                                                                         \
+    if (([&](asciis func){                                                                              \
         if (__test.case_count += 1; condition){                                                         \
             return false;                                                                               \
         }                                                                                               \
         else{                                                                                           \
-            using namespace ::mixc::macro_xassert::inc;                                                 \
+            using namespace mixc::macro_private_log::origin;                                            \
+            using namespace mixc::io_tty::origin;                                                       \
+            __test.error_count += 1;                                                                    \
             log(                                                                                        \
-                __test.error_count += 1,                                                                \
+                for_test,                                                                               \
                 __FILE__,                                                                               \
                 __LINE__,                                                                               \
-                #__VA_ARGS__ ",", ## __VA_ARGS__);                                                      \
+                func,                                                                                   \
+                "error, " #__VA_ARGS__ ",", __test.error_count, ## __VA_ARGS__);                        \
             if (__test.error_count % __test.error_threshold == 0) {                                     \
                 tty.write("pause key to continue...");                                                  \
+                tty.flush();                                                                            \
                 tty.read_key();                                                                         \
                 tty.write("\n");                                                                        \
             }                                                                                           \
             return true;                                                                                \
         }                                                                                               \
-    })())
+    })(__func__))
 
     #define xassert_eq(...)                                                                             \
     if (([&](auto const & wanted, auto const & actually){                                               \
@@ -63,4 +67,5 @@
             item(r);                                                                                    \
         }                                                                                               \
     } xlink2(__, __COUNTER__) = [](::mixc::macro_xassert::result & __test)
+
 #endif
