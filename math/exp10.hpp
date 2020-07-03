24d829ed229e2d1fe9df9053cec7041e0d98ab50
PowerfulCat <x4495@outlook.com>
2020-07-03 23:03:55 +0800

    作者粗心的漏掉了 math/exp10

--- math/exp10.hpp
+++ math/exp10.hpp
@@ -7,7 +7,7 @@
     #include"mixc.hpp"
     #pragma pop_macro("xuser")
 
-    namespace xuser{
+    namespace mixc::math_exp10{
         constexpr f64 lut_1e0_1e15[]     = { 1e0, 1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7, 1e8, 1e9, 1e10, 1e11, 1e12, 1e13, 1e14, 1e15, };
         constexpr f64 lut_1e16_1e240[]   = { 1e0, 1e16, 1e32, 1e48, 1e64, 1e80, 1e96, 1e112, 1e128, 1e144, 1e160, 1e176, 1e192, 1e208, 1e224, 1e240};
         constexpr f64 lut_1e256[]        = { 1e0, 1e256 };
