b0849ca23255dc6cb1d327939341a203ca01d707
PowerfulCat <x4495@outlook.com>
2020-07-04 19:23:55 +0800

    在 macro/ 中新增 xhint 支持无条件信息打印功能

---
+++ macro/xhint.hpp
@@ -0,0 +1,15 @@
+#ifndef xpack_macro_xhint
+#define xpack_macro_xhint
+    #pragma warning(disable:4390)
+    #pragma push_macro("xuser")
+    #undef  xuser
+    #define xuser mixc::macro_xhint
+    #include"macro/private/log.hpp"
+    #pragma pop_macro("xuser")
+
+    #define xhint(...) {                                    \
+        using namespace ::mixc::macro_private_log::origin;  \
+        log(for_hint, __FILE__, __LINE__,                   \
+            __func__, #__VA_ARGS__ ",", __VA_ARGS__);       \
+    }
+#endif
