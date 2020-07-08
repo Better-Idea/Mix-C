90def73b6558423f6f7643daa78d1c861af31d8b
PowerfulCat <x4495@outlook.com>
2020-07-08 19:18:17 +0800

    消除 max min 宏对 math/max min 的影响

--- math/max.hpp
+++ math/max.hpp
@@ -1,11 +1,16 @@
 #ifndef xpack_math_max
 #define xpack_math_max
+    #ifdef max
+        #undef max
+    #endif
+
     namespace mixc::math_max{
         template<class type>
         inline type max(type left, type right){
             return left > right ? left : right;
         }
     }
+
 #endif
 
 namespace xuser::inc{
--- math/min.hpp
+++ math/min.hpp
@@ -1,11 +1,16 @@
 #ifndef xpack_math_min
 #define xpack_math_min
+    #ifdef min
+        #undef min
+    #endif
+
     namespace mixc::math_min{
         template<class type>
         inline type min(type left, type right){
             return left < right ? left : right;
         }
     }
+
 #endif
 
 namespace xuser::inc{
