0219385d2a68854bde93070fc37e4f0f6751bd18
PowerfulCat <x4495@outlook.com>
2020-07-17 11:47:41 +0800

    把 gc/ref 遍历数组的职责交给 gc/private/tuple

--- gc/private/tuple.hpp
+++ gc/private/tuple.hpp
@@ -1,76 +1,102 @@
 #ifndef xpack_gc_private_tuple
 #define xpack_gc_private_tuple
-    #pragma push_macro("xuser")
-    #undef  xuser
-    #define xuser mixc::gc_private_tuple
-    #include"define/base_type.hpp"
-    #include"gc/self_management.hpp"
-    #include"macro/xdebug.hpp"
-    #include"meta/is_based_on.hpp"
-    #include"meta/remove_membership.hpp"
-    #include"meta_seq/tlist.hpp"
-    #include"meta_seq/vlist.hpp"
-    #include"meta_seq/tin.hpp"
-    #pragma pop_macro("xuser")
+#pragma push_macro("xuser")
+#undef  xuser
+#define xuser mixc::gc_private_tuple
+#include"define/base_type.hpp"
+#include"gc/self_management.hpp"
+#include"macro/xdebug.hpp"
+#include"meta/is_based_on.hpp"
+#include"meta/remove_const.hpp"
+#include"meta/remove_ref.hpp"
+#include"meta/remove_membership.hpp"
+#include"meta_seq/tlist.hpp"
+#include"meta_seq/vlist.hpp"
+#include"meta_seq/tin.hpp"
+#pragma pop_macro("xuser")
 
-    namespace mixc::gc_private_tuple{
-        using namespace inc;
+namespace mixc::gc_private_tuple{
+    using namespace inc;
 
-        template<class root_t>
-        struct tuple{
-            using base_list             = typename root_t::base_list;
-            using member_list_partial   = typename root_t::member_list_partial;
-            
-            template<class guide>
-            bool routing(){
-                bool can_arrive_root = false;
-                can_arrive_root |= routing_base<guide>(base_list());
-                can_arrive_root |= routing_self<guide>(member_list_partial());
-                return can_arrive_root;
-            }
+    template<class root_t>
+    struct tuple{
+        using base_list             = typename root_t::base_list;
+        using member_list_partial   = typename root_t::member_list_partial;
+        
+        template<class guide>
+        bool routing(){
+            bool can_arrive_root = false;
+            can_arrive_root |= routing_base<guide>(base_list());
+            can_arrive_root |= routing_self<guide>(member_list_partial());
+            return can_arrive_root;
+        }
 
-        private:
-            template<class guide, auto a0, auto ... args>
-            bool routing_self(vlist<a0, args...>){
-                using origin = remove_membership<decltype(a0)>;
-                bool can_arrive_root = false;
+    private:
+        template<class guide, auto a0, auto ... args>
+        bool routing_self(vlist<a0, args...>){
+            using origin = remove_membership<decltype(a0)>;
+            bool can_arrive_root = false;
 
-                if constexpr (tin<guide, origin>){
-                    auto field = xref ((root_t *)this)->*a0;
+            if constexpr (tin<guide, origin>){
+                auto field = xref ((root_t *)this)->*a0;
 
-                    if constexpr (is_based_on<self_management, origin>){
-                        can_arrive_root |= field->template routing<guide>();
-                    }
-                    else{
-                        auto entry = (tuple<origin> *)field;
-                        can_arrive_root |= entry->template routing<guide>();
-                    }
+                if constexpr (is_based_on<self_management, origin>){
+                    can_arrive_root |= field->template routing<guide>();
                 }
-                return can_arrive_root | routing_self<guide>(vlist<args...>());
+                else{
+                    auto entry = (tuple<origin> *)field;
+                    can_arrive_root |= entry->template routing<guide>();
+                }
             }
+            return can_arrive_root | routing_self<guide>(vlist<args...>());
+        }
 
-            template<class guide>
-            bool routing_self(vlist<>){
-                return false;
-            }
+        template<class guide>
+        bool routing_self(vlist<>){
+            return false;
+        }
 
-            template<class guide, class base0, class ... basex>
-            bool routing_base(tlist<base0, basex...>){
-                auto child           = (root_t *)this;
-                auto base            = (base0 *)child; // 李氏转换
-                auto new_tuple       = (tuple<base0> *)base;
-                auto can_arrive_root = 
-                    new_tuple->template routing<guide>() | 
-                    routing_base<guide>(tlist<basex...>());
-                return can_arrive_root;
-            }
+        template<class guide, class base0, class ... basex>
+        bool routing_base(tlist<base0, basex...>){
+            auto child           = (root_t *)this;
+            auto base            = (base0 *)child; // 李氏转换
+            auto new_tuple       = (tuple<base0> *)base;
+            auto can_arrive_root = 
+                new_tuple->template routing<guide>() | 
+                routing_base<guide>(tlist<basex...>());
+            return can_arrive_root;
+        }
 
-            template<class guide>
-            bool routing_base(tlist<>){
-                return false;
+        template<class guide>
+        bool routing_base(tlist<>){
+            return false;
+        }
+    };
+
+    template<>
+    struct tuple<void>{
+        template<class guide, class array>
+        static bool routing(array & list, uxx length){
+            using item_t = remove_const<
+                remove_ref<decltype(list[0])>
+            >;
+            using tuplep = tuple<item_t> *;
+            bool can_arrive_root = false;
+
+            if constexpr (is_based_on<self_management, item_t>){
+                for(uxx i = 0; i < list.length(); i++){
+                    can_arrive_root |= list[i].template routing<guide>();
+                }
             }
-        };
-    }
+            else{
+                for(uxx i = 0; i < list.length(); i++){
+                    can_arrive_root |= tuplep(xref list[i])->template routing<guide>();
+                }
+            }
+            return can_arrive_root;
+        }
+    };
+}
 
 #endif
 
--- gc/ref.hpp
+++ gc/ref.hpp
@@ -1,93 +1,86 @@
 #ifndef xpack_gc_ref
 #define xpack_gc_ref
-    #pragma push_macro("xuser")
-    #undef  xuser
-    #define xuser mixc::gc_ref
-    #include"define/base_type.hpp"
-    #include"define/nullref.hpp"
-    #include"docker/hashmap.hpp"
-    #include"dumb/dummy_t.hpp"
-    #include"dumb/struct_t.hpp"
-    #include"gc/self_management.hpp"
-    #include"gc/private/make_guide.hpp"
-    #include"gc/private/token.hpp"
-    #include"gc/private/tuple.hpp"
-    #include"lock/atom_swap.hpp"
-    #include"macro/xdebug.hpp"
-    #include"macro/xis_nullptr.hpp"
-    #include"macro/xstruct.hpp"
-    #include"memory/allocator.hpp"
-    #include"memop/addressof.hpp"
-    #include"memop/cast.hpp"
-    #include"meta/is_based_on.hpp"
-    #include"meta/is_class.hpp"
-    #include"meta_ctr/cif.hpp"
-    #include"meta_seq/tlist.hpp"
-    #include"meta_seq/tin.hpp"
-    #include"meta_seq/vlist.hpp"
-    #pragma pop_macro("xuser")
+#pragma push_macro("xuser")
+#undef  xuser
+#define xuser mixc::gc_ref
+#include"define/base_type.hpp"
+#include"define/nullref.hpp"
+#include"docker/hashmap.hpp"
+#include"dumb/dummy_t.hpp"
+#include"dumb/struct_t.hpp"
+#include"gc/self_management.hpp"
+#include"gc/private/make_guide.hpp"
+#include"gc/private/token.hpp"
+#include"gc/private/tuple.hpp"
+#include"lock/atom_swap.hpp"
+#include"macro/xdebug.hpp"
+#include"macro/xis_nullptr.hpp"
+#include"macro/xstruct.hpp"
+#include"memory/allocator.hpp"
+#include"memop/addressof.hpp"
+#include"memop/cast.hpp"
+#include"meta/is_based_on.hpp"
+#include"meta/is_class.hpp"
+#include"meta_ctr/cif.hpp"
+#include"meta_seq/tlist.hpp"
+#include"meta_seq/tin.hpp"
+#include"meta_seq/vlist.hpp"
+#pragma pop_macro("xuser")
 
-    namespace mixc::gc_ref{
-        using namespace inc;
-        using visited_ptr_t = voidp;
+namespace mixc::gc_ref{
+    using namespace inc;
+    using visited_ptr_t = voidp;
 
-        xstruct(
-            xname(info_t)
-        )
-            uxx can_arrive_root : 1;
-            uxx visited         : sizeof(uxx) * 8 - 1;
+    xstruct(
+        xname(info_t)
+    )
+        uxx can_arrive_root : 1;
+        uxx visited         : sizeof(uxx) * 8 - 1;
 
-            info_t() : 
-                can_arrive_root(0), visited(0){}
+        info_t() : 
+            can_arrive_root(0), visited(0){}
 
-            operator uxx (){
-                return uxxp(this)[0];
+        operator uxx (){
+            return uxxp(this)[0];
+        }
+    $
+
+    extern hashmap<visited_ptr_t, info_t>    gc_map;
+    extern uxx                               degree_dvalue;
+    extern visited_ptr_t                     root;
+    extern bool                              can_free_whole_ring;
+
+    template<class final, class item_t, class attribute_t, bool is_array>
+    xstruct(
+        xtmpl(meta, final, item_t, attribute_t, is_array),
+        xpubb(self_management),
+        xasso(attribute_t),
+        xasso(item_t)
+    )
+    private:
+        using the_length  = typename cif<is_array, token_plus, token>::result;
+        using token_mix_t = token_mix<item_t, attribute_t, the_length>;
+
+        template<class guide>
+        bool routing() {
+            using tuplep = tuple<attribute_t> *;
+
+            bool can_arrive_root = false;
+
+            // 无法通向 root 的死路
+            if (mem == nullptr){
+                return false;
             }
-        $
 
-        inline static hashmap<visited_ptr_t, info_t>    gc_map;
-        inline static uxx                               degree_dvalue;
-        inline static visited_ptr_t                     root;
-        inline static bool                              need_free_whole_ring;
+            xdebug(im_gc_meta_routing, mem, xtypeid(attribute_t).name, mem->owners(), the.length());
 
-        template<class impl, class item, class attribute, bool is_array>
-        xstruct(
-            xtmpl(meta, impl, item, attribute, is_array),
-            xpubb(self_management),
-            xasso(attribute),
-            xasso(item)
-        )
-            using the_length  = typename cif<is_array, token_plus, token>::result;
-            using token_mix_t = token_mix<item, attribute, the_length>;
+            // 如果数组元素 item_t 属于 guide 类型集合，那么就遍历数组元素
+            if constexpr (tin<guide, item_t>){
+                can_arrive_root |= tuple<void>::template routing<guide>(the, the.length());
+            }
 
-            template<class guide>
-            bool routing() {
-                using tuplep = tuple<attribute> *;
-
-                bool can_arrive_root = false;
-
-                // 无法通向 root 的死路
-                if (mem == nullptr){
-                    return false;
-                }
-
-                xdebug(im_gc_meta_routing, mem, xtypeid(attribute).name, mem->owners(), the.length());
-
-                if constexpr (tin<guide, item>){
-                    using tuplep = tuple<item> *;
-
-                    if constexpr (is_based_on<self_management, item>){
-                        for(uxx i = 0; i < the.length(); i++){
-                            can_arrive_root |= the[i].template routing<guide>();
-                        }
-                    }
-                    else{
-                        for(uxx i = 0; i < the.length(); i++){
-                            can_arrive_root |= tuplep(xref the[i])->template routing<guide>();
-                        }
-                    }
-                }
-
+            // 如果 attribute_t 属于 guide 类型集合，就遍历该对象的字段
+            if constexpr (tin<guide, attribute_t>){
                 // 我们把经过的节点存下来
                 // 如果是首次经过就让它遍历成员节点
                 // 如果该节点可以通往 root，那么曾经拜访过该节点的附和节点也可以通往根节点
@@ -97,9 +90,9 @@
                 //             +-----> 其他节点 -> root
                 // 在此时附和节点不能确定此节点是否还存在可以通往 root 的路径，所以指示暂时的让此节点的 visited 访问计数器加一
                 // 而计数汇总的工作则是交给此节点来完成
-                if (auto & info = gc_map.get(mem); info == nullref){
-                    info_t      this_node;
-                    attribute * ptr             = mem; // 李氏转换
+                if (info_t & info = gc_map.get(mem); info == nullref){
+                    info_t        this_node;
+                    attribute_t * ptr           = mem; // 李氏转换
                     this_node.can_arrive_root   = mem == root;
                     this_node.visited           = mem != root; // 除了根节点，其他节点都有直接的入边（表示从前一个节点到此节点）
                     gc_map.set(mem, this_node);
@@ -107,7 +100,7 @@
                     can_arrive_root |= tuplep(ptr)->template routing<guide>();
 
                     if (can_arrive_root){
-                        auto & this_node            = gc_map.get(mem);
+                        info_t & this_node          = gc_map.get(mem);
                         this_node.can_arrive_root   = true;
                         degree_dvalue              += mem->owners() - this_node.visited;
                     }
@@ -121,184 +114,196 @@
                     info.visited   += 1;
                     xdebug(im_gc_meta_routing, info.visited);
                 }
-                return can_arrive_root;
+            }
+            return can_arrive_root;
+        }
+
+        template<class guide>
+        bool can_release(){
+            bool    state;
+            root            = mem;
+
+            // not can_arrive_root
+            if (not the.template routing<guide>()){
+                return false;
             }
 
-            template<class guide>
-            bool can_release(){
-                bool    state;
-                root                    = mem;
+            state           = degree_dvalue == 0;
+            degree_dvalue   = 0;
+            return state;
+        }
 
-                // not can_arrive_root
-                if (not the.template routing<guide>()){
-                    return false;
-                }
+    public:
+        meta() : mem(nullptr) { }
 
-                state                   = degree_dvalue == 0;
-                degree_dvalue           = 0;
-                return state;
+        template<class ... args>
+        meta(::length length, args const & ... list) {
+            mem = alloc(length);
+
+            for(uxx i = 0; i < length; i++) {
+                new (mem->ptr(i)) item_t(list...);
             }
-        public:
-            meta() : mem(nullptr) { }
+        }
 
-            template<class ... args>
-            meta(::length length, args const & ... list) {
-                mem = alloc(length);
+        template<class ... args>
+        meta(::ini, args const & ... list) {
+            mem = alloc(::length(0), list...);
+        }
 
-                for (uxx i = 0; i < length; i++) {
-                    new (mem->ptr(i)) item(list...);
-                }
+        meta(the_t const & value){
+            if (mem = value.mem; mem != nullptr) {
+                mem->owners_inc();
+            }
+        }
+    protected:
+        ~meta(){
+            the = nullptr;
+        }
+
+        auto operator -> () const{
+            return mem;
+        }
+    public:
+        final & operator = (the_t const & value){
+            if (value.mem != nullptr){ 
+                value->owners_inc();
             }
 
-            template<class ... args>
-            meta(ini, args const & ... list) {
-                mem = alloc(::length(0), list...);
+            // 当有多个线程对该对象赋值时，该原子操作可以保证正确性
+            cast<the_t>(atom_swap(& mem, value.mem)).~meta();
+
+            // 虽然作者想这么写，奈何优化器喜欢暴力强拆
+            // the_t & old = cast<the_t>(atom_swap(& mem, value.mem));
+            // old         = nullptr;
+            return thex;
+        }
+
+        xis_nullptr(mem == nullptr);
+
+        final & operator=(decltype(nullptr)){
+            using guide = decltype(make_guide<final>());
+            constexpr bool need_gc = guide::length != 0;
+            token_mix_t *  ptr = nullptr;
+            uxx            cnt;
+
+            if (ptr = atom_swap(& mem, ptr); ptr == nullptr) { // enter only once
+                return thex;
             }
 
-            meta(the_t const & value){
-                if (mem = value.mem; mem != nullptr) {
-                    mem->owners_inc();
-                }
-            }
-        protected:
-            ~meta(){
-                using guide = decltype(make_guide<impl>());
-                constexpr bool need_gc = guide::length != 0;
-                token_mix_t *  ptr = nullptr;
-                uxx            cnt;
+            // 后面的代码可以推送给后台 gc 线程
+            the_t & old = cast<the_t>(ptr);
+            cnt         = ptr->owners_dec();
+            xdebug(im_gc__meta, xtypeid(attribute_t).name, cnt, ptr);
 
-                if (ptr = atom_swap(& mem, ptr); ptr == nullptr) { // enter only once
-                    return;
-                }
-
-                auto & old = cast<the_t>(ptr);
-                cnt        = ptr->owners_dec();
-                xdebug(im_gc__meta, xtypeid(attribute).name, cnt, ptr);
-
-                if constexpr (not need_gc){
-                    if (cnt == 0){
-                        old.free();
-                    }
-                }
-                else if (need_free_whole_ring){
-                    if (auto && i = gc_map.take_out(ptr); i.has_hold_value() and i.can_arrive_root){
-                        old.free();
-                    }
-                }
-                else if (cnt == 0){
+            if constexpr (not need_gc){
+                if (cnt == 0){
                     old.free();
                 }
-                else if (old.template can_release<guide>()){
-                    need_free_whole_ring = true;
-                    gc_map.take_out(ptr);
+            }
+            else if (can_free_whole_ring){
+                if (auto && i = gc_map.take_out(ptr); i.has_hold_value() and i.can_arrive_root){
                     old.free();
-                    gc_map.clear();
-                    need_free_whole_ring = false;
-                }
-                else{
-                    gc_map.clear();
                 }
             }
-
-            auto operator -> () const{
-                return mem;
+            else if (cnt == 0){
+                old.free();
             }
-        public:
-            impl & operator = (the_t const & value){
-                if (value.mem){ 
-                    value->owners_inc();
-                }
-                cast<the_t>(
-                    atom_swap(& mem, value.mem)
-                ).~meta();
-                return (impl &)this[0];
+            else if (old.template can_release<guide>()){
+                can_free_whole_ring = true;
+                gc_map.take_out(ptr);
+                old.free();
+                gc_map.clear();
+                can_free_whole_ring = false;
             }
-
-            xis_nullptr(mem == nullptr);
-
-            bool operator == (the_t const & value) const {
-                return mem == value.mem;
+            else{
+                gc_map.clear();
             }
+            return thex;
+        }
 
-            bool operator != (the_t const & value) const {
-                return mem != value.mem;
-            }
+        bool operator == (the_t const & value) const {
+            return mem == value.mem;
+        }
 
-            void swap(the_t * values) {
-                values->mem = atom_swap(& mem, values->mem);
-            }
+        bool operator != (the_t const & value) const {
+            return mem != value.mem;
+        }
 
-        protected:
-            operator item * () {
-                return xref mem[0][0];
-            }
+        void swap(the_t * values) {
+            values->mem = atom_swap(& mem, values->mem);
+        }
 
-            operator item const * () const {
-                return xref mem[0][0];
-            }
+    protected:
+        operator item_t * () {
+            return xref mem[0][0];
+        }
 
-            item & operator [] (uxx index){
-                return mem[0][index];
-            }
+        operator item_t const * () const {
+            return xref mem[0][0];
+        }
 
-            const item & operator [] (uxx index) const {
-                return mem[0][index];
-            }
+        item_t & operator [] (uxx index){
+            return mem[0][index];
+        }
 
-            uxx length() const {
-                return mem->this_length();
-            }
+        const item_t & operator [] (uxx index) const {
+            return mem[0][index];
+        }
 
-            constexpr static uxx header_size(){
-                return sizeof(token_mix_t);
-            }
-        private:
-            token_mix_t * mem;
+        uxx length() const {
+            return mem->this_length();
+        }
 
-            template<class ... args> auto alloc(uxx length, args const & ... list) {
-                return alloc_with_initial<token_mix_t>(
-                    memory_size(
-                        sizeof(token_mix_t) + length * sizeof(item)
-                    ),
-                    length, list...
-                );
-            }
+        constexpr static uxx header_size(){
+            return sizeof(token_mix_t);
+        }
+    private:
+        token_mix_t * mem;
 
-            void free(){
-                free_with_destroy(mem,
-                    memory_size(
-                        sizeof(token_mix_t) + mem->this_length() * sizeof(item)
-                    )
-                );
-            }
-        $
-    }
+        template<class ... args> auto alloc(uxx length, args const & ... list) {
+            return alloc_with_initial<token_mix_t>(
+                memory_size(
+                    sizeof(token_mix_t) + length * sizeof(item_t)
+                ),
+                length, list...
+            );
+        }
 
-    namespace mixc::gc_ref::origin{
-        template<class impl, class type>
-        using ref_ptr = meta<
-            impl, 
-            dummy_t, 
-            typename cif<
-                is_class<type>,
-                type,
-                struct_t<type>
-            >::result,
-            false
-        >;
+        void free(){
+            free_with_destroy(mem,
+                memory_size(
+                    sizeof(token_mix_t) + mem->this_length() * sizeof(item_t)
+                )
+            );
+        }
+    $
+}
 
-        template<class impl, class item, class attribute = void>
-        using ref_array = meta<
-            impl, 
-            item, 
-            typename cif<
-                is_class<attribute>,
-                attribute,
-                struct_t<attribute>
-            >::result,
-            true
-        >;
-    }
+namespace mixc::gc_ref::origin{
+    template<class final, class type>
+    using ref_ptr = meta<
+        final, 
+        dummy_t, 
+        typename cif<
+            is_class<type>,
+            type,
+            struct_t<type>
+        >::result,
+        false
+    >;
+
+    template<class final, class item_t, class attribute_t = void>
+    using ref_array = meta<
+        final, 
+        item_t, 
+        typename cif<
+            is_class<attribute_t>,
+            attribute_t,
+            struct_t<attribute_t>
+        >::result,
+        true
+    >;
+}
 
 #endif
 
