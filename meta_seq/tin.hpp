15e255f9350420ba4ac5f05e89726fdb946786d7
PowerfulCat <x4495@outlook.com>
2020-07-17 17:16:44 +0800

    格式化 meta_seq/tin

--- meta_seq/tin.hpp
+++ meta_seq/tin.hpp
@@ -1,37 +1,37 @@
 #ifndef xpack_meta_seq_tin
 #define xpack_meta_seq_tin
-    #pragma push_macro("xuser")
-        #undef  xuser
-        #define xuser mixc::meta_seq_tin
-        #include"define/base_type.hpp"
-        #include"meta/is_same.hpp"
-        #include"meta_seq/tlist.hpp"
-    #pragma pop_macro("xuser")
+#pragma push_macro("xuser")
+#undef  xuser
+#define xuser mixc::meta_seq_tin
+#include"define/base_type.hpp"
+#include"meta/is_same.hpp"
+#include"meta_seq/tlist.hpp"
+#pragma pop_macro("xuser")
 
-    namespace mixc::meta_seq_tin{
-        using namespace inc;
+namespace mixc::meta_seq_tin{
+    using namespace inc;
 
-        namespace inner{
-            template<class item>
-            inline constexpr bool meta(tlist<>){
-                return false;
-            }
+    template<class item>
+    inline constexpr bool meta(tlist<>){
+        return false;
+    }
 
-            template<class item, class first, class ... args>
-            inline constexpr bool meta(tlist<first, args...>){
-                if constexpr (is_same<first, item>){
-                    return true;
-                }
-                else{
-                    return meta<item>(tlist<args...>());
-                }
-            }
+    template<class item, class first, class ... args>
+    inline constexpr bool meta(tlist<first, args...>){
+        if constexpr (is_same<first, item>){
+            return true;
+        }
+        else{
+            return meta<item>(tlist<args...>());
         }
     }
+
+    template<class tlist, class item>
+    constexpr bool tin = meta<item>(tlist());
+}
 
 #endif
 
 namespace xuser::inc{
-    template<class tlist, class item>
-    constexpr bool tin = ::mixc::meta_seq_tin::inner::meta<item>(tlist());
+    using ::mixc::meta_seq_tin::tin;
 }
