7707f1f16b8d53156c7cfea1fe6a78e59a033ca2
PowerfulCat <x4495@outlook.com>
2020-07-17 17:18:25 +0800

    在 meta/ 中添加 fit_bits 用于根据所需位宽选择合适的无符号基础类型

---
+++ meta/fit_bits.hpp
@@ -0,0 +1,38 @@
+#ifndef xpack_meta_fit_bits
+#define xpack_meta_fit_bits
+#pragma push_macro("xuser")
+#undef  xuser
+#define xuser mixc::meta_fit_bits
+#include"define/base_type.hpp"
+#pragma pop_macro("xuser")
+
+namespace mixc::meta_fit_bits{
+    template<uxx bits>
+    struct meta{
+        static auto invoke(){
+            static_assert(bits <= 64);
+
+            if constexpr(bits <= 8){
+                return u08();
+            }
+            else if constexpr(bits <= 16){
+                return u16();
+            }
+            else if constexpr(bits <= 32){
+                return u32();
+            }
+            else{
+                return u64();
+            }
+        }
+    };
+
+    template<uxx bits>
+    using fit_bits = decltype(meta<bits>::invoke());
+}
+
+#endif
+
+namespace xuser::inc{
+    using ::mixc::meta_fit_bits::fit_bits;
+}
