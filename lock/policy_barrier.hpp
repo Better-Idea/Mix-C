a57bacbdb34d0a132ddc2c2c0a89507ce158c3c6
PowerfulCat <x4495@outlook.com>
2020-07-18 18:30:44 +0800

    完善 lock/policy_barrier

--- lock/policy_barrier.hpp
+++ lock/policy_barrier.hpp
@@ -1,17 +1,13 @@
 // 注意：
 // 在使用 when<>::can<> 模板时，参数可以不按顺序，但必须保证参数是从 0 到 n 每次步进为 1
-#ifndef xpack_policy_barrier
-#define xpack_policy_barrier
+#ifndef xpack_lock_policy_barrier
+#define xpack_lock_policy_barrier
 #pragma push_macro("xuser")
 #undef  xuser
 #define xuser mixc::lock_policy_barrier
 #include"lock/atom_and.hpp"
-#include"lock/atom_fetch_add.hpp"
-#include"lock/atom_fetch_and.hpp"
 #include"lock/atom_fetch_or.hpp"
-#include"lock/atom_sub.hpp"
-#include"lock/atom_swap.hpp"
-#include"lock/private/lock_state_t.hpp"
+#include"lock/private/thread_yield.hpp"
 #include"meta/fit_bits.hpp"
 #include"meta_seq/tget.hpp"
 #include"meta_seq/tlist.hpp"
@@ -21,6 +17,11 @@
 namespace mixc::lock_policy_barrier{
     using namespace inc;
 
+    enum{
+        max_concurrency      = 4,
+        max_concurrency_mask = (1 << max_concurrency) - 1,
+    };
+
     template<class previous, class ... result>
     inline auto make_order(previous, tlist<result...>, tlist<>){
         return tlist<result...>();
@@ -28,7 +29,7 @@ namespace mixc::lock_policy_barrier{
 
     template<class previous, class first, class ... rest, class ... result>
     inline auto make_order(previous, tlist<result...>, tlist<first, rest...>){
-        if constexpr(previous::master + 1 == first::master){
+        if constexpr(previous::index + 1 == first::index){
             return make_order(first(), tlist<result..., first>(), tlist<rest...>());
         }
         else{
@@ -36,81 +37,118 @@ namespace mixc::lock_policy_barrier{
         }
     }
 
-    template<auto do_something>
-    struct when{
-        template<auto ... do_something_else>
-        struct can{
-            static constexpr auto also             = (... + (uxx(1) << uxx(do_something_else)));
-            static constexpr auto master           = (do_something);
+    template<auto do_something, auto ... do_something_else>
+    struct can_also{
+        enum : uxx{
+            also    = (... + (uxx(max_concurrency_mask) << (uxx(do_something_else) * max_concurrency))),
+            master  = (uxx(max_concurrency_mask) << (uxx(do_something) * max_concurrency)),
+            index   = (do_something),
         };
     };
 
-    template<uxx number_of_counter, class count_t>
-    struct meta{
-        using bits_t = fit_bits<number_of_counter>;
-        static constexpr bits_t no_clean = bits_t(-1);
-        bits_t  bits    = 0;
-        bits_t  clean   = no_clean;
-        count_t count[number_of_counter] = {0};
+    template<auto do_something>
+    struct can_also<do_something>{
+        enum : uxx{
+            also    = (0),
+            master  = (uxx(max_concurrency_mask) << (uxx(do_something) * max_concurrency)),
+            index   = (do_something),
+        };
+    };
+
+    template<auto do_something>
+    struct when{
+        template<auto ... do_something_else>
+        using can = can_also<do_something, do_something_else...>;
     };
 
     struct dummy_previous{
-        static constexpr uxx master = uxx(-1);
+        enum : uxx{
+            index   = uxx(-1)
+        };
     };
 
-    template<class count_t, class ... rules>
-    struct policy_barrier : meta<sizeof...(rules), count_t>{
+    template<class ... rules>
+    xstruct(
+        xtmpl(policy_barrier, rules...),
+        xprif(state, fit_bits<sizeof...(rules)>)
+    )
         using rule_list = decltype(
             make_order(dummy_previous(), tlist<>(), tlist<rules...>())
         );
-        using base_t    = meta<sizeof...(rules), count_t>;
+
+        using bits_t = fit_bits<sizeof...(rules)>;
 
         template<auto operation>
-        lock_state_t try_lock_for(){
+        [[nodiscard]] uxx try_lock(){
             using rule  = typename tget<rule_list, operation>::item;
-            uxx mask    = uxx(1) << operation;
+            uxx mutex   = ~rule::also;
+            uxx mask    = uxx(1) << (uxx(operation) * max_concurrency);
 
-            // 设置状态
-            uxx old_bit;
-            uxx old_cnt = atom_fetch_add(xref base_t::count[operation], 1);
-            uxx clean   = atom_swap(xref base_t::clean, base_t::no_clean);
+            // 可以执行相同的操作
+            if constexpr (rule::master & rule::also){
+                for(uxx i = 0; i < max_concurrency; i++, mask <<= 1){
+                    uxx old = atom_fetch_or<bits_t>(xref state, mask);
 
-            // 谁先得到 clean，谁就更有机会不被 blocked
-            // 这里为了对齐美观，做了一次无效的赋值
-            old_bit     = atom_fetch_and(xref base_t::bits, clean);
-            old_bit     = atom_fetch_or(xref base_t::bits, mask);
-
-            // 假如不包含与当前操作互斥的操作，则可以进行该操作
-            if ((old_bit & ~rule::also) == 0){
-                return lock_state_t::accept;
+                    // 存在互斥操作
+                    if ((old & mutex) != 0){
+                        if ((old & mask) == 0){
+                            atom_and<bits_t>(xref state, ~mask);
+                        }
+                        return not_exist;
+                    }
+                    // 当前位未被占用，则使用通道 0
+                    if ((old & mask) == 0){
+                        return i;
+                    }
+                }
+                return not_exist;
             }
+            // 相同的操作不能同时进行
             else{
-                unlock_for<operation>();
-                return lock_state_t::blocked;
+                // 当前位被占用，无法操作
+                uxx old = atom_fetch_or<bits_t>(xref state, mask);
+                if (old & mask){
+                    return not_exist;
+                }
+
+                // 后判断互斥操作，因为此处的 operation 可以与自己互斥
+                if (old & mutex){
+                    atom_and<bits_t>(xref state, ~mask);
+                    return not_exist;
+                }
+                else{
+                    return 0;
+                }
             }
         }
 
         template<auto operation>
-        void unlock_for(){
-            using rule = typename tget<rule_list, operation>::item;
-            uxx mask   = uxx(1) << operation;
-
-            if (atom_sub(xref base_t::count[operation], 1) == 0){
-                atom_and(xref base_t::clean, ~mask);
+        [[nodiscard]] uxx lock(){
+            while(true){
+                if (uxx channel = try_lock<operation>(); channel != not_exist){
+                    return channel;
+                }
+                thread_yield();
             }
-
-            // 错误的方式：
-            // if (atom_sub(xref base_t::count[operation], 1) == 0){
-            //     // 此夹缝导致即使其他线程给 bits 置位，也会被下面语句给复位
-            //     // 所以我们干脆把复位的责任交给 try_lock_for
-            //     atom_and(xref base_t::bits, ~mask);
-            // }
         }
-    };
+
+        template<auto operation, class callback>
+        void lock(callback const & call){
+            uxx channel = lock<operation>();
+            call();
+            unlock<operation>(channel);
+        }
+
+        template<auto operation>
+        void unlock(uxx channel){
+            using rule  = typename tget<rule_list, operation>::item;
+            uxx mask    = uxx(1) << (uxx(operation) * max_concurrency + channel);
+            atom_and<bits_t>(xref state, ~mask);
+        }
+    $
 }
 
 namespace mixc::lock_policy_barrier::origin{
-    using inc::lock_state_t;
     using mixc::lock_policy_barrier::policy_barrier;
     using mixc::lock_policy_barrier::when;
 }
