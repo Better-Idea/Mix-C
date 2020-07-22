ddfcd5f5a2eb8b5f671c380414b3a44462c555f0
PowerfulCat <x4495@outlook.com>
2020-07-22 20:44:55 +0800

    再发布 docker/stack，支持策略锁

--- docker/private/stack.hpp
+++ docker/private/stack.hpp
@@ -1,6 +1,3 @@
-// 注意：
-// 虽然 stack::push、stack::pop 等函数用了原子操作
-// 但只保证了同一操作的原子性，如果想要同时使用 push/pop 需要上锁
 #ifndef xpack_docker_stack
 #define xpack_docker_stack
 #pragma push_macro("xuser")
@@ -31,37 +28,34 @@ namespace mixc::docker_stack{
         push,
         pop,
         foreach,
-
-        // 下面属性不受约束
-        // top
-        // is_empty 
+        top_xr,
+        top_xw,
     };
 
-    // push 时可以 foreach，但反过来时不行的，因为 push 只是局部锁住
-    using barrier = policy_barrier<thread_counter_t,
-                        // routing, clear, push, pop, foreach
-        when<routing>::can<                push,      foreach>,
-        when<clear  >::can<         clear, push              >,
-        when<push   >::can<routing, clear, push              >,
+    enum{
+        co_max = (sizeof(uxx) * 8 - 4) / 4,
+    };
+
+    using with_stack_lock = policy_barrier<force_machine_word,
+                        // routing, clear, push, pop, foreach, top_xr, top_xw
+        when<routing>::can<                           foreach, top_xr        >,
+        when<clear  >::can<>,
+        when<push   >::can<                push              , top_xr, top_xw>::concurrency<co_max>,
         when<pop    >::can<>,
-        when<foreach>::can<routing,        push,      foreach>
+        when<foreach>::can<routing,                   foreach, top_xr, top_xw>::concurrency<co_max>,
+        when<top_xr >::can<routing,        push,      foreach, top_xr, top_xw>::concurrency<co_max>,
+        when<top_xw >::can<                push,      foreach, top_xr, top_xw>::concurrency<co_max>
     >;
 
-    template<class final>
+    template<class final, class item_t, class lock_t>
     xstruct(
-        xtmpl(barrier_component, final),
-        xprob(barrier)
-    ) $
-
-    template<class final, class item_t>
-    xstruct(
-        xtmpl(stack_t, final, item_t),
-        xprob(barrier_component<final>),
+        xtmpl(stack_t, final, item_t, lock_t),
+        xprob(node_field<item_t, lock_t>),
         xpubb(self_management),
-        xpubb(disable_copy),
-        xprof(ptop, mutable single_linked_node<item_t> *)
+        xpubb(disable_copy)
     )
     private:
+        using base_t    = node_field<item_t, lock_t>;
         using node      = single_linked_node<item_t>;
         using nodep     = node *;
         using iteratorx = iteratorx<item_t &> const &;
@@ -69,67 +63,53 @@ namespace mixc::docker_stack{
 
         template<class guide>
         bool routing(){
-            return false;    
+            // TODO:==================================================
+            return false;
         }
 
         // 构造析构区
     public:
-        stack_t() : 
-            ptop(nullptr){
+        stack_t(){
+            base_t::ptop = nullptr;
         }
     protected:
         ~stack_t() {
             clear();
         }
 
-        // 私有静态字段
-    private:
-        static inline nodep wait_free = nullptr;
-
         // 公有函数区
     public:
         void clear() {
-            nodep cur = atom_swap(& the.ptop, nodep(nullptr));
+            nodep cur = nullptr;
             nodep tmp;
 
-            barrier::lock_for<opr::clear>([&](){
-                while(cur != nullptr){
-                    tmp = cur;
-                    cur = cur->next;
-                    free_with_destroy<node>(tmp);
-                }
+            lock<opr::clear>([&](){
+                cur = base_t::swap_top(nullptr);
             });
+
+            while(cur != nullptr){
+                tmp = cur;
+                cur = cur->next;
+                free_with_destroy<node>(tmp);
+            }
         }
 
         void push(item_t const & value) {
-            nodep new_top  = alloc_with_initial<node>(value);
-            new_top->next = nullptr;
-            xmflush(); // 确保不和 clear 冲突
-
-            barrier::lock_for<opr::push>([&](){
-                new_top->next = atom_swap(& ptop, new_top);
+            nodep new_top = alloc_with_initial<node>(value);
+            lock<opr::push>([&](){
+                new_top->next = base_t::swap_top(new_top);
             });
         }
-
-        bool enter = false;
 
         transmitter<item_t> pop() {
             transmitter<item_t> r;
             nodep               free_item = nullptr;
-            barrier::lock_for<opr::pop>([&](){
-                if (enter){
-                    xhint("error");
+            lock<opr::pop>([&](){
+                if (nodep cur = base_t::top(); cur != nullptr){
+                    free_item = cur;
+                    r         = cur[0];
+                    base_t::swap_top(cur->next);
                 }
-                if (ptop == nullptr){
-                    return;
-                }
-                enter = true;
-
-                free_item   = ptop;
-                r           = ptop[0];
-                ptop        = ptop->next;
-
-                enter = false;
             });
 
             if (free_item){
@@ -141,8 +121,8 @@ namespace mixc::docker_stack{
         // 迭代器区
     public:
         final & foreach(iteratorx itr){
-            barrier::lock_for<opr::foreach>([&](){
-                nodep cur = ptop;
+            lock<opr::foreach>([&](){
+                nodep cur = base_t::top();
                 for(uxx i = 0; cur != nullptr; cur = cur->next, i++){
                     if (itr(i, cur[0]) == loop_t::finish){
                         return;
@@ -153,8 +133,8 @@ namespace mixc::docker_stack{
         }
 
         final & foreach(iterator itr){
-            barrier::lock_for<opr::foreach>([&](){
-                nodep cur = ptop;
+            lock<opr::foreach>([&](){
+                nodep cur = base_t::top();
                 for(uxx i = 0; cur != nullptr; cur = cur->next, i++){
                     itr(i, cur[0]);
                 }
@@ -163,17 +143,33 @@ namespace mixc::docker_stack{
         }
         // 属性区
     public:
-        xpubget_pubsetx(top, item_t &)
-            xr{ return ptop[0]; }
-            xw{ ptop[0] = value; }
+        xpubget_pubsetx(top, transmitter<item_t>)
+            xr{
+                transmitter<item_t> r;
+                lock<opr::top_xr>([&](){
+                    if (nodep cur = base_t::top(); cur != nullptr){
+                        r = cur[0];
+                    }
+                });
+                return r;
+            }
+            xw{
+                lock<opr::top_xw>([&](){
+                    if (nodep cur = base_t::top(); cur != nullptr){
+                        cur[0] = value;
+                    }
+                });
+            }
 
+        // 注意：
+        // 多线程环境即使读取到的 stack::is_empty() 为 false 也不能确定该 stack 非空
         xpubgetx(is_empty, bool){
-            return ptop == nullptr;
+            return base_t::top() == nullptr;
         }
     };
 
-    template<class final, class item_t>
-    using stack = adapter_pushpop<final, stack_t<final, item_t>, item_t>;
+    template<class final, class item_t, class lock>
+    using stack = adapter_pushpop<final, stack_t<final, item_t, lock>, item_t>;
 }
 
 #endif
--- docker/stack.hpp
+++ docker/stack.hpp
@@ -5,8 +5,10 @@
 #include"macro/xfinal.hpp"
 
 namespace xuser::inc{
-    template<class item_t>
-    xfinal_tmpl(xusing_docker_stack, stack);
+    using xusing_docker_stack::with_stack_lock;
+
+    template<class item_t, class lock_t = void>
+    xfinal_tmpl(xusing_docker_stack, stack, item_t, lock_t);
 }
 
 #undef  xusing_docker_stack
