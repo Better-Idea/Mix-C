0a4b33263c22bbc866ed9e3095cf2b524d99e403
PowerfulCat <x4495@outlook.com>
2020-07-22 18:35:31 +0800

    在 docker/private/single_linked_node 中支持两种线程同步方式

--- docker/private/single_linked_node.hpp
+++ docker/private/single_linked_node.hpp
@@ -5,6 +5,8 @@
 #define xuser mixc::docker_single_linked_node
 #include"dumb/struct_type.hpp"
 #include"interface/iterator.hpp"
+#include"lock/mutex.hpp"
+#include"meta_ctr/cif.hpp"
 #include"mixc.hpp"
 #pragma pop_macro("xuser")
 
@@ -20,6 +22,45 @@ namespace mixc::docker_single_linked_node::origin {
             inc::struct_type<item_t>(list...), next(nullptr){
         }
     $
+}
+
+namespace mixc::docker_single_linked_node{
+    template<class item_t, class barrier_t>
+    xstruct(
+        xtmpl(meta   , item_t, barrier_t),
+        xpubf(ptop   , mutable origin::single_linked_node<item_t> *),
+        xprif(barrier, barrier_t)
+    )
+        template<auto opr, class callback>
+        void lock(callback const & call){
+            barrier.template lock<opr>(call);
+        }
+
+        enum : uxx{
+            mask = uxx(-1), // 不屏蔽任何位
+        };
+    $
+
+    // 默认使用 base_t::ptop 的第 0 位作为互斥位
+    template<class item_t>
+    xstruct(
+        xspec(meta, item_t, void),
+        xpubf(ptop, mutable origin::single_linked_node<item_t> *)
+    )
+        template<auto opr, class callback>
+        void lock(callback const & call){
+            inc::mutex::lock(xref ptop, 0, call);
+        }
+
+        enum : uxx{
+            mask = uxx(-2), // 屏蔽第 0 位
+        };
+    $
+
+    namespace origin{
+        template<class item_t, class barrier_t, bool with_lock>
+        using node_field = meta<item_t, inc::cif<with_lock, barrier_t, void>>;
+    }
 }
 
 #endif
