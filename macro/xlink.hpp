72f4d7d38b30b912e6d63390c6e2686f94d1ab18
PowerfulCat <x4495@outlook.com>
2020-07-25 23:42:17 +0800

    让 macro/xlink xlist 支持多级宏展开

--- macro/xlink.hpp
+++ macro/xlink.hpp
@@ -1,3 +1,12 @@
 #pragma once
 #define xlink(a,b)      a ## b
 #define xlink2(a,b)     xlink(a, b)
+
+#define xlinka(a,b)     a ## b
+#define xlinka2(a,b)    xlinka(a, b)
+
+#define xlinkb(a,b)     a ## b
+#define xlinkb2(a,b)    xlinkb(a, b)
+
+#define xlinkc(a,b)     a ## b
+#define xlinkc2(a,b)    xlinkc(a, b)
