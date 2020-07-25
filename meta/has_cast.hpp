56dfe018e87c09af512c8da4e8b9871cb35b6d81
PowerfulCat <x4495@outlook.com>
2020-07-25 15:06:41 +0800

    完善 meta/more_fit 周边

--- meta/has_cast.hpp
+++ meta/has_cast.hpp
@@ -8,30 +8,10 @@
 #pragma pop_macro("xuser")
 
 namespace mixc::meta_has_cast{
-    #if xis_msvc
-        template<class target, class source>
-        struct meta{
-        private:
-            static u08 test(target){
-                return true;
-            }
-            static u16 test(...){
-                return false;
-            }
-        public:
-            static constexpr bool result = sizeof(
-                decltype(test(*(source *)nullptr))
-            ) == 1;
-        };
-
-        template<class target, class source>
-        constexpr bool has_cast = meta<target, source>::result;
-    #else
-        template<class target, class source>
-        concept has_cast = requires(target tar, source src){
-            tar = src;
-        };
-    #endif
+    template<class target, class source>
+    concept has_cast = requires(target tar, source src){
+        tar = src;
+    };
 }
 
 #endif
--- meta/more_fit.hpp
+++ meta/more_fit.hpp
@@ -4,6 +4,7 @@
 #undef  xuser
 #define xuser mixc::meta_more_fit
 #include"define/base_type.hpp"
+#include"math/const.hpp"
 #include"meta/has_cast.hpp"
 #include"meta/has_operator_cast.hpp"
 #include"meta/is_based_on.hpp"
@@ -12,10 +13,11 @@
 #pragma pop_macro("xuser")
 
 namespace mixc::meta_more_fit{
+    using namespace inc;
     template<
         class the_target            = void,
         uxx   index_value           = not_exist,
-        uxx   size_different        = uxx(-1),
+        ixx   size_different        = min_value_of<ixx>,
         bool  full_match_v          = false,
         bool  has_operator_cast_v   = false,
         bool  has_implicit_cast_v   = false
@@ -23,54 +25,51 @@ namespace mixc::meta_more_fit{
     struct record{
         using type                              = the_target;
         static constexpr uxx  index             = index_value;
-        static constexpr uxx  different         = size_different;
+        static constexpr ixx  different         = size_different;
         static constexpr bool full_match        = full_match_v;
         static constexpr bool has_operator_cast = has_operator_cast_v;
         static constexpr bool has_implicit_cast = has_implicit_cast_v;
     };
 
-    template<uxx count, class source, class ... target> struct meta;
-    template<uxx count, class source>
-    struct meta<count, source>{
+    template<uxx index, class source, class ... target> struct meta;
+    template<uxx index, class source>
+    struct meta<index, source>{
         static auto invoke(){
             return record<>();
         }
     };
 
-    template<uxx count, class source, class first, class ... target>
-    struct meta<count, source, first, target...> {
+    template<uxx index, class source, class first, class ... target>
+    struct meta<index, source, first, target...> {
         static auto invoke(){
-            constexpr auto diff = sizeof(first) - sizeof(source);
+            constexpr ixx diff = ixx(sizeof(first) - sizeof(source));
 
             using next = decltype(
-                meta<count, source, target...>::invoke()
+                meta<index + 1, source, target...>::invoke()
             );
 
-            if constexpr (next::full_match){
-                return next();
+            if constexpr (is_same<first, source>){
+                return record<first, index, 0, true>();
             }
-            else if constexpr (inc::is_same<first, source>){
-                return record<first, count - sizeof...(target) - 1, 0, true>();
-            }
-            else if constexpr (next::has_operator_cast){
+            else if constexpr (next::full_match or next::has_operator_cast){
                 return next();
             }
             else if constexpr (
-                inc::has_operator_cast<first, source> or 
-                inc::has_operator_cast<inc::remove_ref<first> &, source> or
-                inc::has_operator_cast<inc::remove_ref<first> const &, source> or
-                inc::is_based_on<first, source>){
-                return record<first, count - sizeof...(target) - 1, 0, false, true>();
+                has_operator_cast<first, source> or 
+                has_operator_cast<remove_ref<first> &, source> or
+                has_operator_cast<remove_ref<first> const &, source> or
+                is_based_on<first, source>){
+                return record<first, index, 0, false, true>();
             }
-            else if constexpr (inc::has_cast<first, source>){
-                if constexpr
+            else if constexpr (has_cast<first, source>){
+                if constexpr 
                     (next::has_implicit_cast and (
                     (next::different >= 0 and (diff <= 0 or diff >= next::different)) or
                     (next::different < 0 and diff <= next::different))) {
                     return next();
                 }
                 else{
-                    return record<first, count - sizeof...(target) - 1, diff, false, false, true>();
+                    return record<first, index, diff, false, false, true>();
                 }
             }
             else{
@@ -81,7 +80,7 @@ namespace mixc::meta_more_fit{
 
     template<class source, class target, class ... rest>
     using more_fit = decltype(
-        meta<sizeof...(rest) + 1, source, target, rest...>::invoke()
+        meta<0, source, target, rest...>::invoke()
     );
 }
 
