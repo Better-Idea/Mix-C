5528a2021ab56fc4920cad7815bb7f51bbf3a4aa
PowerfulCat <x4495@outlook.com>
2020-07-28 22:21:32 +0800

    调整 memory/private/extern.malloc 格式

--- memory/private/extern.malloc.hpp
+++ memory/private/extern.malloc.hpp
@@ -1,41 +1,38 @@
-#ifndef xpack_memory_malloc
-#define xpack_memory_malloc 
-    #pragma push_macro("xuser")  
-    #undef  xuser
-    #define xuser mixc::memory_private_tiny_allocator
-    #include"configure.hpp"
-    #include"define/base_type.hpp"
-    #include<malloc.h>
-    #pragma pop_macro("xuser")
+#ifdef xuser
+    #undef xuser
+#endif
 
-    namespace mixc::memory_private_tiny_allocator{
-        voidp malloc(size_t bytes){
-            return ::malloc(bytes);
-        }
+#define xuser mixc::memory_private_tiny_allocator
+#include"configure.hpp"
+#include"define/base_type.hpp"
+#include<malloc.h>
 
-        voidp malloc_aligned(size_t bytes, size_t align_bytes){
-            #if xis_windows
-                return ::_mm_malloc(bytes, align_bytes);
-            #endif
-
-            #if xis_linux
-                return ::memalign(align_bytes, bytes);
-            #endif
-        }
-
-        void free(voidp ptr){
-            ::free(ptr);
-        }
-
-        void free_aligned(voidp ptr){
-            #if xis_windows
-                ::_mm_free(ptr);
-            #endif
-
-            #if xis_linux
-                return ::free(ptr);
-            #endif
-        }
+namespace mixc::memory_private_tiny_allocator{
+    voidp malloc(size_t bytes){
+        return ::malloc(bytes);
     }
 
-#endif
+    voidp malloc_aligned(size_t bytes, size_t align_bytes){
+        #if xis_windows
+            return ::_mm_malloc(bytes, align_bytes);
+        #endif
+
+        #if xis_linux
+            return ::memalign(align_bytes, bytes);
+        #endif
+    }
+
+    void free(voidp ptr){
+        ::free(ptr);
+    }
+
+    void free_aligned(voidp ptr){
+        #if xis_windows
+            ::_mm_free(ptr);
+        #endif
+
+        #if xis_linux
+            return ::free(ptr);
+        #endif
+    }
+}
