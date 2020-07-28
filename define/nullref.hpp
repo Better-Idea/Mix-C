b183b31bffc4670e581d9d78d1ef5f294b15213f
PowerfulCat <x4495@outlook.com>
2020-07-28 16:29:18 +0800

    优化 define/nullref

--- define/nullref.hpp
+++ define/nullref.hpp
@@ -10,26 +10,21 @@ namespace mixc::define_nullref_t{
     xstruct(
         xname(nullref_t)
     )
-        template<class a> friend auto operator == (a const & value, nullref_t) {
-            // 编译器假定绑定是不会绑定到空地址的
-            // 所以使用 volatile 避免被优化(不加 volatile 优化器直接暴力移除该比较)
-            volatile auto ptr = inc::addressof(value);
-            return voidp(ptr) == nullptr;
+        template<class a> auto operator == (a const & value) const {
+            auto ptr = inc::addressof(value);
+            return voidp(ptr) == this;
         }
 
-        template<class a> friend auto operator == (nullref_t, a const & value) {
-            volatile auto ptr = inc::addressof(value);
-            return voidp(ptr) == nullptr;
+        template<class a> friend auto operator == (a const & value, nullref_t const & nullref) {
+            return nullref == value;
         }
 
-        template<class a> friend auto operator != (a const & value, nullref_t) {
-            volatile auto ptr = inc::addressof(value);
-            return voidp(ptr) != nullptr;
+        template<class a> friend auto operator != (a const & value, nullref_t const & nullref) {
+            return not (nullref == value);
         }
 
-        template<class a> friend auto operator != (nullref_t, a const & value) {
-            volatile auto ptr = inc::addressof(value);
-            return voidp(ptr) != nullptr;
+        template<class a> friend auto operator != (nullref_t const & nullref, a const & value) {
+            return not (nullref == value);
         }
 
         template<class a>
