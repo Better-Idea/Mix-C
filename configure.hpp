c849641bc5b26a4541147fc1d655668b77e9b354
PowerfulCat <x4495@outlook.com>
2020-07-28 16:34:00 +0800

    简化 configure 配置项

--- configure.hpp
+++ configure.hpp
@@ -1,45 +1,39 @@
 #pragma once
 #define xmixc                       "Mix-C"
 
-// debug
-#define xuse_xdebug                 1
+// debug options
+#define xuse_xdebug                 0
 #define xuse_xdebug_fail            0
 #define xuse_xdebug_short_path      1
 
-// memory
-#define xuse_libc_malloc            1
-
-// select os
-#define xis_windows                 1
-#define xis_linux                   0
-#define xis_osx                     0
-
-#if xis_windows + xis_linux + xis_osx != 1
-    #error "you can select only one operation system"
-#endif
-
-// select architecture
-#define xis_x86                     1
-#define xis_arm                     0
-#define xis_risc_v                  0
-#define xis_mips                    0
-
-#if xis_x86 + xis_arm + xis_risc_v + xis_mips != 1
-    #error "you can select only one cpu platform"
-#endif
+// select memory allocator
+#define xuse_libc_malloc            0
 
 // select os-bits
 #define xis_os64                    1
 #define xis_os32                    0
 
+// auto select os
+#if     __WIN32__ or __WIN64__
+    #define xis_windows             1
+#elif   __linux__
+    #define xis_linux               1
+#endif
+
+// auto select architecture
+#if     __x86_64__
+    #define xis_x86                 1
+#elif   __arm__
+    #define xis_arm                 1
+#endif
+
+#if     _MSVC_LANG
+    #define xis_msvc                1
+#endif
+
+// check
 #if xis_os64 + xis_os32 != 1
     static_assert(xis_os64 and (sizeof(voidp) == 8));
     static_assert(xis_os32 and (sizeof(voidp) == 4));
     #error "you can select only one between xis_os64 and xis_os32"
-#endif
-
-#if __clang__ || __GNUC__
-    #define xis_msvc          0
-#elif _MSVC_LANG
-    #define xis_msvc          1
 #endif
