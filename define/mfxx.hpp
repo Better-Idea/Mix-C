7da447dbe3b964dfa13e087f7079cc2c2d85c3ee
PowerfulCat <x4495@outlook.com>
2020-07-31 21:55:00 +0800

    在 define/mfxx 中支持 f80，并添加最大有效位数

--- define/mfxx.hpp
+++ define/mfxx.hpp
@@ -12,14 +12,16 @@ namespace mixc::define_mfxx{
         class   equivalent_type, 
         uxx     decimal_bits, 
         uxx     exp_bits, 
-        uxx     exp_offset>
+        uxx     exp_offset,
+        uxx     precious_bits>
     union mfxx{
         using the_t = mfxx<
             float_type,
             equivalent_type,
             decimal_bits,
             exp_bits,
-            exp_offset
+            exp_offset,
+            precious_bits
         >;
 
         struct {
@@ -32,14 +34,15 @@ namespace mixc::define_mfxx{
 
         the_t & real_exp(ixx value){
             the.exp = value + exp_offset;
+            return the;
         }
 
         ixx real_exp() const {
             return ixx(the.exp) - exp_offset;
         }
 
-        i64 real_dec() const {
-            return i64(u64(1) << decimal_bits | the.decimal);
+        u64 real_dec() const {
+            return u64(u64(1) << decimal_bits | the.decimal);
         }
 
         mfxx() : value(0) { }
@@ -47,6 +50,10 @@ namespace mixc::define_mfxx{
 
         constexpr mfxx(equivalent_type sign, equivalent_type exp, equivalent_type decimal) : 
             decimal(decimal), exp(exp), sign(sign) {}
+
+        constexpr uxx precious(){
+            return precious_bits;
+        }
 
         operator float_type & () {
             return value;
@@ -59,8 +66,9 @@ namespace mixc::define_mfxx{
 }
 
 namespace mixc::define_mfxx::origin{
-    using mf32 = mfxx<f32, u32, 23, 8, 127 >;
-    using mf64 = mfxx<f64, u64, 52, 11, 1023>;
+    using mf32 = mfxx<f32, u32, 23, 8 , (1 <<  8) - 1, 7>;
+    using mf64 = mfxx<f64, u64, 52, 11, (1 << 10) - 1, 16>;
+    using mf80 = mfxx<f80, u64, 64, 15, (1 << 14) - 1, 18>;
 }
 
 #endif
