1561a8d29706b36b44e532e5acec3d523319801f
PowerfulCat <x4495@outlook.com>
2020-07-31 22:00:44 +0800

    添加 math/private/extern.trigo+ 到 extern.api 中

--- extern.api.hpp
+++ extern.api.hpp
@@ -5,5 +5,6 @@
 #include"lock/private/extern.atom.hpp"
 #include"lock/private/extern.thread_yield.hpp"
 #include"macro/private/extern.log.hpp"
+#include"math/private/extern.trigo+.hpp"
 #include"memory/private/extern.allocator.hpp"
 #include"memory/private/extern.malloc.hpp"
