46e609556ff7865f5c91b3690000fdd399174e07
PowerfulCat <x4495@outlook.com>
2020-07-31 10:08:27 +0800

    调整一下 draft/octant_circle.hpp

--- draft/octant_circle.hpp
+++ draft/octant_circle.hpp
@@ -4,6 +4,7 @@
 #undef  xuser
 #define xuser mixc::draft_octant_circle
 #include"define/base_type.hpp"
+#include<math.h>
 #pragma pop_macro("xuser")
 
 namespace mixc::draft_octant_circle{
@@ -16,16 +17,16 @@ namespace mixc::draft_octant_circle{
      * 在 45°角处水平速度等于垂直数组
      * vx - ax·t = bx·t
      * 
-     * 下列算法作为用浮点数示意的八分画圆算法有以下特点
+     * 下列以浮点数示意的八分画圆算法有以下特点
      * - 可并行化
      * - 可随机访问圆上任意角度对应的坐标
      * - 时间作为精度的刻度，被分得越细精度越高
      */ 
     template<class painter>
     inline void octant_circle(u32 r, painter const & draw){
-        u32 w      = (u32)(f32(r) * 0.5 * sqrt(2.0));
-        u32 t      = (r);
-        u32 tt     = (t * t);
+        f32 t      = f32(r) * 2;
+        f32 tt     = (t * t);
+        f32 w      = round(f32(r) * 0.5 * sqrt(2.0));
         f32 ay     = (f32(r - w) * 2.0 / tt);
         f32 vx     = (f32(w) - 0.5 * ay * tt) * 2.0 / t;
         f32 ax     = (vx * t - f32(w)) * 2.0 / tt;
@@ -33,10 +34,9 @@ namespace mixc::draft_octant_circle{
         f32 sx     = (0);
         f32 sy     = (r);
         u32 last_x = u32(0);
-        u32 last_y = u32(sy);
         u32 i      = 1;
 
-        draw(0, last_x, last_y);
+        draw(0, last_x, sy);
 
         while(vx > vy){
             vx    -= ax;
@@ -47,11 +47,10 @@ namespace mixc::draft_octant_circle{
             auto y = (u32)round(sy);
 
             if (last_x == x){
-                last_y = last_y;
                 continue;
             }
 
-            draw(i, last_x = x, last_y = y);
+            draw(i, last_x = x, y);
             i     += 1;
         }
     }
