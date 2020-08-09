95b312dd066718b7eefad5ead3b73de9625a26f6
PowerfulCat <x4495@outlook.com>
2020-08-09 11:36:54 +0800

    在 lang/ 中显式特化 cxx<char> cxx<char16_t> wxx<char> wxx<char16_t> 并添加说明该模板仅用于字符类型处理，以防误用

--- lang/cxx.hpp
+++ lang/cxx.hpp
@@ -1,12 +1,21 @@
+// 注意：cxx<item_t> 主要用作字符串处理，目前仅支持 char/char16_t 两种元素类型
 #ifdef xuser
 #ifndef xusing_lang_cxx
 #include"lang/private/cxx.hpp"
 #endif
-#include"macro/xfinal.hpp"
 
 namespace xuser::inc{
-    template<class item>
-    xfinal_tmpl(xusing_lang_cxx, cxx, item);
+    template<class item_t> struct cxx;
+
+    template<>
+    struct cxx<char> : xusing_lang_cxx::cxx<cxx<char>, char>{
+        using xusing_lang_cxx::cxx<cxx<char>, char>::cxx;
+    };
+
+    template<>
+    struct cxx<char16_t> : xusing_lang_cxx::cxx<cxx<char16_t>, char16_t>{
+        using xusing_lang_cxx::cxx<cxx<char16_t>, char16_t>::cxx;
+    };
 
     using c08 = cxx<char>;
     using c16 = cxx<char16_t>;
--- lang/wxx.hpp
+++ lang/wxx.hpp
@@ -1,12 +1,21 @@
+// 注意：wxx<item_t> 主要用作字符处理，目前仅支持 char/char16_t 两种元素类型
 #ifdef xuser
 #ifndef xusing_lang_wxx
 #include"lang/private/wxx.hpp"
 #endif
-#include"macro/xfinal.hpp"
 
 namespace xuser::inc{
-    template<class item>
-    xfinal_tmpl(xusing_lang_wxx, wxx, item);
+    template<class item_t> struct wxx;
+
+    template<>
+    struct wxx<char> : xusing_lang_wxx::wxx<wxx<char>, char>{
+        using xusing_lang_wxx::wxx<wxx<char>, char>::wxx;
+    };
+
+    template<>
+    struct wxx<char16_t> : xusing_lang_wxx::wxx<wxx<char16_t>, char16_t>{
+        using xusing_lang_wxx::wxx<wxx<char16_t>, char16_t>::wxx;
+    };
 
     using w08 = wxx<char>;
     using w16 = wxx<char16_t>;
