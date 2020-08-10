df9fc14b384810c130170ad4805bc2a6f3b1d068
PowerfulCat <x4495@outlook.com>
2020-08-10 15:15:00 +0800

    在 math/ 中新增 sqrt 操作

--- math/sqrt.hpp
+++ math/sqrt.hpp
@@ -4,14 +4,26 @@
 #undef  xuser
 #define xuser mixc::math_sqrt
 #include"define/base_type.hpp"
+#include"define/nan.hpp"
 #pragma pop_macro("xuser")
 
 namespace mixc::math_sqrt{
-    inline f64 sqrt_unsafe(f64 x);
+    extern f64 sqrt_unsafe(f64 x);
+
+    inline f64 sqrt(f64 x){
+        if (x < 0){
+            return inc::nan;
+        }
+        return sqrt_unsafe(x);
+    }
 }
 
 #endif
 
 namespace xuser::adv{
+    using ::mixc::math_sqrt::sqrt_unsafe;
+}
+
+namespace xuser::inc{
     using ::mixc::math_sqrt::sqrt_unsafe;
 }
