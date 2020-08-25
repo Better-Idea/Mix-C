4f126c4b07b0b163f57c7ddb4c185a0dbb570bf7
PowerfulCat <x4495@outlook.com>
2020-08-25 18:13:48 +0800

    更新 algo/heap_root

--- algo/heap_root.hpp
+++ algo/heap_root.hpp
@@ -7,82 +7,114 @@
 #include"docker/transmitter.hpp"
 #include"dumb/mirror.hpp"
 #include"interface/can_compare.hpp"
-#include"interface/ranger.hpp"
+#include"interface/unified_seq.hpp"
 #include"memop/addressof.hpp"
 #include"memop/copy.hpp"
 #include"memory/new.hpp"
+#include"meta/item_origin_of.hpp"
 #pragma pop_macro("xuser")
 
 namespace mixc::algo_heap_root{
-    template<class item_t>
-    struct heap_root{
-        static void push(
-            inc::ranger<item_t> const & range,
-            uxx                         length,
-            item_t              const & value, 
-            inc::can_compare<item_t>    compare = inc::default_compare<item_t>){
+    template<class item_t, inc::unified_seq_t seq_t>
+    static void push_core(
+        seq_t                       seq,
+        item_t const &              value, 
+        inc::can_compare<item_t>    compare = inc::default_compare<item_t>){
+        
+        // 注意：
+        // [value] 可能存在 [seq] 中, 
+        // 所以需要一个 [value] 的副本
+        inc::mirror<item_t> similarities{value, inc::construction_t::ignore};
+        uxx                 i = seq.length();
+        uxx                 ii;
 
-            // 注意：
-            // [value] 可能存在 [range] 中, 
-            // 所以需要一个 [value] 的副本
-            inc::mirror<item_t> similarities = value;
-            uxx                 i            = length;
-            uxx                 ii;
-            
-            for (; i > 0; i = ii){
-                auto & parent = range[
-                    ii = (i - 1) >> 1
-                ];
+        for (; i > 0; i = ii){
+            auto & parent = seq[
+                ii = (i - 1) >> 1
+            ];
 
-                if (compare(parent, similarities) <= 0){
-                    break;
-                }
-
-                // 避免拷贝赋值
-                inc::copy(xref range[i], parent);
+            if (compare(parent, similarities) <= 0){
+                break;
             }
 
-            // 这里需要拷贝构造
-            new (xref range[i]) item_t(similarities);
+            // 避免拷贝赋值
+            inc::copy(xref seq[i], parent);
         }
 
-        static auto pop(
-            inc::ranger<item_t> const & range,
-            uxx                         length,
-            item_t              const & insert_value, 
-            inc::can_compare<item_t>    compare = inc::default_compare<item_t>){
-            
-            // 避免返回值的复制构造
-            auto && wanted     = inc::transmitter{ range[0] };
-            uxx     i          = 0;
-            uxx     left_index = 1;
+        // 这里需要拷贝构造
+        new (xref seq[i]) item_t(similarities);
+    }
 
-            while(left_index + 1 < length) {
-                item_t & left  = range[left_index];
-                item_t & right = range[left_index + 1];
-                item_t * select;
+    template<class item_t, inc::unified_seq_t seq_t>
+    static auto pop_core(
+        seq_t                       seq, 
+        item_t const &              insert_value, 
+        inc::can_compare<item_t>    compare = inc::default_compare<item_t>){
 
-                // 小根堆 父节点要小于左右节点 所以要找子节点中较小者
-                if (compare(left, right) > 0){
-                    select = xref right;
-                    left_index++;
-                }
-                else{
-                    select = xref left;
-                }
+        // 避免返回值的复制构造
+        auto && wanted     = inc::transmitter{ seq[0] };
+        uxx     i          = 0;
+        uxx     left_index = 1;
 
-                // 较小的子节点大于等于插入 就不用再下移了
-                if (compare(select[0], insert_value) >= 0){
-                    break;
-                }
+        while(left_index + 1 < seq.length()) {
+            item_t & left  = seq[left_index];
+            item_t & right = seq[left_index + 1];
+            item_t * select;
 
-                inc::copy(xref range[i], select[0]);
-                i          = (left_index);
-                left_index = (left_index << 1) + 1;
+            // 小根堆 父节点要小于左右节点 所以要找子节点中较小者
+            if (compare(left, right) > 0){
+                select = xref right;
+                left_index++;
+            }
+            else{
+                select = xref left;
             }
 
-            new (xref range[i]) item_t(insert_value);
-            return wanted;
+            // 较小的子节点大于等于插入 就不用再下移了
+            if (compare(select[0], insert_value) >= 0){
+                break;
+            }
+
+            inc::copy(xref seq[i], select[0]);
+            i          = (left_index);
+            left_index = (left_index << 1) + 1;
+        }
+
+        new (xref seq[i]) item_t(insert_value);
+        return wanted;
+    }
+
+    struct heap_root{
+        template<inc::unified_seq_t seq_t>
+        static void push(
+            seq_t                       const & seq,
+            uxx                                 length,
+            inc::item_origin_of<seq_t>  const & value, 
+            inc::can_compare<
+                inc::item_origin_of<seq_t>
+            >                                   compare = 
+            inc::default_compare<
+                inc::item_origin_of<seq_t>
+            >){
+            push_core<
+                inc::item_origin_of<seq_t>
+            >(inc::unified_seq<seq_t>(seq).subseq(co{0, length}), value, compare);
+        }
+
+        template<inc::unified_seq_t seq_t>
+        static auto pop(
+            seq_t                               seq, 
+            uxx                                 length, 
+            inc::item_origin_of<seq_t> const &  insert_value, 
+            inc::can_compare<
+                inc::item_origin_of<seq_t>
+            >                                   compare = 
+            inc::default_compare<
+                inc::item_origin_of<seq_t>
+            >){
+            return pop_core<
+                inc::item_origin_of<seq_t>
+            >(inc::unified_seq<seq_t>(seq).subseq(co{0, length}), insert_value, compare);
         }
     };
 }
@@ -92,4 +124,3 @@ namespace mixc::algo_heap_root{
 namespace xuser::inc{
     using ::mixc::algo_heap_root::heap_root;
 }
-
