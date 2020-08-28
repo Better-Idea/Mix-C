61724387052881fb9476c6ad394c7112ca1938ef
PowerfulCat <x4495@outlook.com>
2020-08-28 22:48:12 +0800

    让 instruction/count_of_set 支持更多平台

--- instruction/count_of_set.hpp
+++ instruction/count_of_set.hpp
@@ -10,17 +10,15 @@
 namespace mixc::instruction_count_of_set{
     template<class type>
     inline uxx count_of_set(type v){
-        #if xis_x86
-            #if xis_os64
-                return __builtin_popcountll(v);
-            #else
-                if constexpr (sizeof(v) == 8){
-                    return __builtin_popcount(v & u32(-1)) + __builtin_popcount(u64(v) >> 32);
-                }
-                else {
-                    return __builtin_popcount(v);
-                }
-            #endif
+        #if xis_os64
+            return __builtin_popcountll(v);
+        #elif xis_os32
+        if constexpr (sizeof(v) == 8){
+            return __builtin_popcount(v & u32(-1)) + __builtin_popcount(u64(v) >> 32);
+        }
+        else {
+            return __builtin_popcount(v);
+        }
         #endif
     }
 }
