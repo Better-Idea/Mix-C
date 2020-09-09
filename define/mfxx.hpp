616d535709751af3086b644602112c768952cf64
PowerfulCat <x4495@outlook.com>
2020-09-09 17:58:51 +0800

    给 define/mfxx 添加帮助注释

--- define/mfxx.hpp
+++ define/mfxx.hpp
@@ -1,3 +1,36 @@
+/* 模块：mfxx
+ * 别名：mf32/mf64/mf80
+ * 类型：工具类
+ * 功能：用于获取和设置浮点数的字段布局
+ * 用法：
+ * // 初始化，可以当普通浮点数用
+ * // mf32 对应 f32
+ * // mf64 对应 f64
+ * // mf80 对应 f80
+ * mf64 f    = 1.5;
+ * 
+ * // 获取原始尾数部分
+ * u64  dec  = f.dec_bits;
+ * 
+ * // 获取原始指数部分
+ * u64  exp  = f.exp_bits;
+ * 
+ * // 获取符号位
+ * u64  sign = f.sign;
+ * 
+ * // 获取原始浮点值
+ * f64  fo   = f.value;
+ * 
+ * 注意：
+ * 原始的尾数部分不包含隐藏的 '1'（f32/f64 独有）
+ * 原始指数部分并非实际的指数，还需要进行一步运算才可得到
+ * 具体详情可搜索[浮点数]了解更多
+ * 
+ * TODO：==============================================================
+ * - 介绍浮点数
+ * - 浮点数的陷阱
+ */
+
 #ifndef xpack_define_mfxx
 #define xpack_define_mfxx
 #pragma push_macro("xuser")
@@ -36,15 +69,28 @@ namespace mixc::define_mfxx{
             float_type value;
         };
 
+        mfxx() : value(0) { }
+        mfxx(float_type value) : value(value) { }
+
+        constexpr mfxx(equivalent_type sign, equivalent_type exp, equivalent_type decimal) : 
+            decimal(decimal), exp(exp), sign(sign) {}
+
+        // 函数：设置浮点数实际的指数
+        // 参数：
+        // - value 为新的指数值
+        // 返回：当前对象的的引用
         the_t & real_exp(ixx value){
             the.exp = value + exp_offset;
             return the;
         }
 
+        // 函数：获取浮点数实际的指数[不带检查]
+        // 注意：如果该浮点数值为 0，则会得到错误的指数部分
         ixx real_exp_unsafe() const {
             return ixx(the.exp) - exp_offset;
         }
 
+        // 函数：获取浮点数实际的指数
         ixx real_exp() const {
             if (the == 0){
                 return 0;
@@ -52,8 +98,10 @@ namespace mixc::define_mfxx{
             return real_exp_unsafe();
         }
 
+        // 函数：获取浮点数实际的尾数部分[不带检查]
         // 注意：
-        // f80 与 f64, f32 的小数部分不同，f80 没有隐藏位
+        // - 如果该浮点数值为 0，则会得到错误的指数部分
+        // - f80 与 f64, f32 的小数部分不同，f80 没有隐藏位
         u64 real_dec_unsafe() const {
             if (not inc::is_same<float_type, f80>){
                 return u64(u64(1) << dec_bits | the.decimal);
@@ -63,6 +111,9 @@ namespace mixc::define_mfxx{
             }
         }
 
+        // 函数：获取浮点数实际的尾数部分
+        // 注意：
+        // - f80 与 f64, f32 的小数部分不同，f80 没有隐藏位
         u64 real_dec() const {
             if (the == 0){
                 return 0;
@@ -70,20 +121,19 @@ namespace mixc::define_mfxx{
             return real_dec_unsafe();
         }
 
-        mfxx() : value(0) { }
-        mfxx(float_type value) : value(value) { }
-
-        constexpr mfxx(equivalent_type sign, equivalent_type exp, equivalent_type decimal) : 
-            decimal(decimal), exp(exp), sign(sign) {}
-
+        // 函数：尾数部分的位数
         constexpr uxx decimal_bits(){
             return dec_bits;
         }
 
+        // 函数：包括隐藏位的尾数部分的尾数
+        // 注意：
+        // - f80 与 f64, f32 的小数部分不同，f80 没有隐藏位
         constexpr uxx decimal_bits_full(){
             return dec_bits + not inc::is_same<float_type, f80>;
         }
 
+        // 函数：该浮点数最大的精度（10进制）
         constexpr uxx precious(){
             return prec_dec;
         }
