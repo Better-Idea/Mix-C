f28e37bfb57fc99e44deb0ae01ad43b3a3850900
PowerfulCat <x4495@outlook.com>
2020-10-17 21:19:42 +0800

    给 docker/private/array.hpp 添加注释

--- docker/private/array.hpp
+++ docker/private/array.hpp
@@ -1,3 +1,10 @@
+/* 模块：array
+ * 类型：基础结构
+ * 功能：提供静态的一维或多维数组
+ * 用法：
+ * TODO======================================================================================
+ */
+
 #ifndef xpack_docker_array
 #define xpack_docker_array
 #pragma push_macro("xuser")
@@ -8,9 +15,17 @@
 #pragma pop_macro("xuser")
 
 namespace mixc::docker_array{
+    /* 静态数组类型 */
     template<class type, uxx count>
     using items_t = type[count];
 
+    /* 结构：静态数组
+     * 参数：
+     * - final 为接口结构类型
+     * - type 为第一维度元素类型
+     * - count 为第一维度元素个数
+     * - rest 为更高维度元素个数
+     */
     template<class final, class type, uxx count = 0, uxx ... rest>
     xstruct(
         xtmpl(array_t, final, type, count, rest...),
@@ -29,25 +44,39 @@ namespace mixc::docker_array{
         template<class finalx>
         array_t(array_t<finalx, type, count, rest...> const & self) : 
             array_t((the_t &)(array_t<finalx, type, count, rest...> &)self) {
-            static_assert(sizeof(self) == sizeof(the_t));
         }
 
+        /* 函数：下标随机访问
+         * 参数：
+         * - index 要访问元素的下标
+         * 返回：
+         * - 指定索引的元素的引用
+         */
         item_t & operator[] (uxx index) {
             return data[index];
         }
 
+        /* 函数：下标随机访问（const 修饰）
+         * 参数：
+         * - index 要访问元素的下标
+         * 返回：
+         * - 指定索引的元素的引用
+         */
         item_t const & operator[] (uxx index) const {
             return data[index];
         }
 
+        /* 函数：获取数组元素的首地址 */
         operator item_t *() {
             return data;
         }
 
+        /* 函数：获取数组元素的首地址（const 修饰） */
         operator item_t const *() const {
             return data;
         }
-        
+
+        /* 属性：当前维度数组元素的个数 */
         xpubgetx(length, uxx){
             return count;
         }
@@ -60,8 +89,7 @@ namespace mixc::docker_array{
 
     template<class final, class type, uxx count, uxx ... rest>
     using array = inc::adapter_array_access<
-        array_t<final, type, count, rest...>,
-        type
+        array_t<final, type, count, rest...>
     >;
 }
 
