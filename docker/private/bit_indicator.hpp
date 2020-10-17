c1861d9c07d4c397bf88cb17e631f50850b1e918
PowerfulCat <x4495@outlook.com>
2020-10-17 21:44:02 +0800

    给 docker/private/bit_indicator 补充说明

--- docker/private/bit_indicator.hpp
+++ docker/private/bit_indicator.hpp
@@ -1,3 +1,10 @@
+/* 模块：bit_indicator
+ * 类型：基础结构
+ * 功能：提供静态/动态的位图结构
+ * 用法：
+ * TODO======================================================================================
+ */
+
 #ifndef xpack_docker_bit_indicator
 #define xpack_docker_bit_indicator
 #pragma push_macro("xuser")
@@ -13,15 +20,15 @@
 #pragma pop_macro("xuser")
 
 namespace mixc::docker_bit_indicator{
-    // bits != 0 时该模板是使用静态内存分配
+    /* 结构：静态位图
+     * 参数：
+     * - final 为接口结构类型
+     * - bits 为总位数
+     */
     template<class final, uxx bits = 0>
     xstruct(
         xtmpl(bit_indicator_t, final, bits)
     )
-    public:
-        constexpr uxx total_bits(){
-            return bits;
-        }
     private:
         static constexpr uxx lv0 = bits / inc::bwidth + (bits % inc::bwidth != 0);
         static constexpr uxx lv1 = lv0 <= 1 ? 0 : lv0 / inc::bwidth + (lv0 % inc::bwidth != 0);
@@ -35,14 +42,21 @@ namespace mixc::docker_bit_indicator{
         static constexpr uxx lv9 = lv8 <= 1 ? 0 : lv8 / inc::bwidth + (lv8 % inc::bwidth != 0);
         static constexpr uxx lut[] = { lv0,lv1,lv2,lv3,lv4,lv5,lv6,lv7,lv8,lv9 };
     protected:
+        /* 属性：位图树每一级的元素个数
+         * 补充：lut[0] 表示第一级位图字的个数，对于 64bit 系统一个字有 64bit
+         *       位图是分层的，在 64bit 系统，上层用 1bit 表示下层 64bit 位组中是否有置位位
+         *       该属性的每一个元素分别指示了每次层一共有多少个字
+         */
         static const uxx * level_lut(){
             return lut;
         }
 
+        /* 属性：位图树中一共包含的字数 */
         static constexpr uxx size(){
             return lv0 + lv1 + lv2 + lv3 + lv4 + lv5 + lv6 + lv7 + lv8 + lv9;
         }
 
+        /* 属性：位图树的高度 */
         static constexpr uxx height(){
             return 
                 lv9 ? 10 : 
@@ -56,14 +70,23 @@ namespace mixc::docker_bit_indicator{
                 lv1 ? 2  : 1;
         }
 
+        /* 属性：位图的首地址 */
         uxx * bmp() const {
             return data;
         }
 
         mutable uxx data[size()] = {0};
+
+        /* 位图可访问的总位数 */
+        xpubgetx(total_bits, uxx){
+            return bits;
+        }
     $
 
-    // 动态内存分配的位图
+    /* 结构：动态位图
+     * 参数：
+     * - final 为接口结构类型
+     */
     template<class final>
     xstruct(
         xspec(bit_indicator_t, final),
@@ -76,6 +99,14 @@ namespace mixc::docker_bit_indicator{
         bit_indicator_t() : 
             pbmp(nullptr), pheight(0), psize(0) {}
 
+        /* 函数：构造函数（外部分配内存）
+         * 参数：
+         * - total_bits 为位图树的总位数
+         * - alloc 为分配回调函数，期望签名如下
+         *      uxx * alloc(uxx length);
+         * 注意：
+         * - 该方式需要外部释放内存
+         */
         bit_indicator_t(uxx total_bits, inc::can_alloc<uxx> alloc){
             uxx buf[sizeof(uxx) * 8 / inc::bwidth + 1];
 
@@ -104,6 +135,12 @@ namespace mixc::docker_bit_indicator{
             the.need_free(false);
         }
 
+        /* 函数：构造函数（内部分配内存）
+         * 参数：
+         * - total_bits 为位图树的总位数
+         * 注意：
+         * - 该方式由内部释放内存
+         */
         bit_indicator_t(uxx total_bits) : 
             bit_indicator_t(total_bits, [](uxx length){
                 return inc::alloc<uxx>(
@@ -129,6 +166,7 @@ namespace mixc::docker_bit_indicator{
             }
         }
 
+        /* 属性：位图树的高度 */
         xproget(height)
         xproget_prisetx(size, uxx)
             xr{ return the.psize >> 1; }
@@ -137,29 +175,36 @@ namespace mixc::docker_bit_indicator{
                 the.psize |= value << 1;
             }
 
+        /* 属性：位图树存放位图的首地址 */
         xproget_prisetx(bmp, uxx *)
             xr{ return the.pbmp; }
             xw{ the.pbmp = value + the.height(); }
 
+        /* 属性：是否需要在析构时释放内存 */
         xpriget_prisetx(need_free, bool)
             xr{ return the.psize & 1; }
             xw{ the.psize = value ? the.psize | 1 : the.psize & (uxx(-1) << 1); }
 
-        // 位图是分层的，在 64bit 系统，上层用 1bit 表示下层 64bit 位组中是否有置位位
-        // 该属性存放了每次层一共有多少个字
+        /* 属性：位图树每一级的元素个数
+         * 补充：lut[0] 表示第一级位图字的个数，对于 64bit 系统一个字有 64bit
+         *       位图是分层的，在 64bit 系统，上层用 1bit 表示下层 64bit 位组中是否有置位位
+         *       该属性的每一个元素分别指示了每次层一共有多少个字
+         */
         xprogetx(level_lut, uxx *){
             return the.pbmp - the.height();
         }
 
-        // 动态内存部分一共有多少个字
+        /* 属性：动态内存部分一共有多少个字 */
         xprigetx(cost_count, uxx){
             return the.size() + the.height();
         }
 
+        /* 属性：动态内存部分一共包含多少个字节 */
         xpubgetx(cost_bytes, inc::memory_size){
             return inc::memory_size(cost_count() * sizeof(uxx));
         }
 
+        /* 属性：位图可访问的总位数 */
         xpubget_priset(total_bits);
     $
 
