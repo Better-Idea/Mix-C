c3c31670a12dbe884783ef1beb0d8ae997974b93
PowerfulCat <x4495@outlook.com>
2020-10-23 12:09:38 +0800

    完善 chrono/time

---
+++ chrono/private/time.hpp
@@ -0,0 +1,73 @@
+#ifndef xpack_chrono_private_time
+#define xpack_chrono_private_time
+#include"mixc.hpp"
+
+namespace mixc::chrono_private_time::origin{
+    template<class final, class field_t = u32>
+    xstruct(
+        xtmpl(time, final, field_t),
+        xproc(pmilisecond, 10                      , field_t),
+        xproc(psecond    , 6                       , field_t),
+        xproc(pminute    , 6                       , field_t),
+        xproc(phour      , sizeof(field_t) * 8 - 22, field_t)
+    )
+        template<class, class> friend struct time;
+
+        time(time const &) = default;
+        
+        template<class now_t>
+        requires(now_t::im_now_t == true)
+        time(now_t):
+            time(now_t::time()){
+        }
+
+        time(field_t hour = 0, field_t minute = 0, field_t second = 0, field_t milisecond = 0) :
+            pmilisecond(milisecond),
+            psecond(second),
+            pminute(minute),
+            phour(hour){
+        }
+
+        template<class finalx, class f>
+        time(time<finalx, f> const & value) : 
+            time(
+                value.phour,
+                value.pminute,
+                value.psecond,
+                value.pmilisecond
+            ){
+        }
+
+        xpubget_pubset(milisecond)
+        xpubget_pubset(second)
+        xpubget_pubset(minute)
+        xpubget_pubset(hour)
+
+        xpubgetx(is_valid, bool){
+            return pminute <= 59 and psecond <= 59 and pmilisecond <= 999;
+        }
+
+        xpubget_pubsetx(total_milisecond, u64)
+            xr{
+                auto ms = 
+                    u64(pmilisecond) + 
+                    u64(psecond) * (1000) + 
+                    u64(pminute) * (60 * 1000) + 
+                    u64(phour)   * (60 * 60 * 1000);
+                return ms;
+            }
+            xw{
+                phour       = (value / (60 * 60 * 1000));
+                value      %= (60 * 60 * 1000);
+                pminute     = (value / (60 * 1000));
+                value      %= (60 * 1000);
+                psecond     = (value / (1000));
+                value      %= (1000);
+                pmilisecond = (value);
+            }
+    $
+}
+
+#endif
+
+#define xusing_chrono_time  ::mixc::chrono_private_time::origin
--- chrono/time.hpp
+++ chrono/time.hpp
@@ -1,80 +1,13 @@
-#ifndef xpack_chrono_time
-#define xpack_chrono_time
-#pragma push_macro("xuser")
-#undef  xuser
-#define xuser mixc::chrono_time
-#include"mixc.hpp"
-#pragma pop_macro("xuser")
-
-namespace mixc::chrono_time::origin{
-    template<class final, class field_t = u32>
-    xstruct(
-        xtmpl(time_t, final, field_t)
-    )
-        time_t(u32 hour = 0, u32 minute = 0, u32 second = 0, u32 milisecond = 0) :
-            pmilisecond(milisecond),
-            psecond(second),
-            pminute(minute),
-            phour(hour){
-        }
-
-        template<class finalx, class f>
-        time_t(time_t<finalx, f> const & value) : 
-            time_t(
-                value.phour,
-                value.pminute,
-                value.psecond,
-                value.pmilisecond
-            ){
-        }
-
-        template<class, class> friend struct time_t;
-
-        #include"chrono/private/xgen.hpp"
-        xgen(milisecond);
-        xgen(second);
-        xgen(minute);
-        xgen(hour);
-        #undef  xgen
-
-        xpubgetx(is_valid, bool){
-            return pminute <= 59 and psecond <= 59 and pmilisecond <= 999;
-        }
-
-        xpubget_pubsetx(total_milisecond, u64)
-            xr{
-                auto ms = 
-                    u64(pmilisecond) + 
-                    u64(psecond) * (1000) + 
-                    u64(pminute) * (60 * 1000) + 
-                    u64(phour)   * (60 * 60 * 1000);
-                return ms;
-            }
-            xw{
-                phour       = value / (60 * 60 * 1000);
-                value      %= (60 * 60 * 1000);
-                pminute     = value / (60 * 1000);
-                value      %= (60 * 1000);
-                psecond     = value / (1000);
-                value      %= (1000);
-                pmilisecond = value;
-            }
-
-    protected:
-        field_t pmilisecond     : 10;
-        field_t psecond         : 6;
-        field_t pminute         : 6;
-        field_t phour           : sizeof(field_t) * 8 - 22;
-    $
-
-    struct time : time_t<time, u64>{
-        using time_t<time, u64>::time_t;
-
-        static time now();
-    };
-}
+#ifdef xuser
+#ifndef xusing_chrono_time
+#include"chrono/private/time.hpp"
 #endif
+#include"macro/xfinal.hpp"
 
 namespace xuser::inc{
-    using namespace ::mixc::chrono_time::origin;
+    template<class field_t = u32>
+    xfinal_tmpl(xusing_chrono_time, time, field_t);
 }
+
+#undef  xusing_chrono_time
+#endif
