db26e203de35f8a56489d401cd9eeaa7ef10b0d4
PowerfulCat <x4495@outlook.com>
2020-10-26 17:08:27 +0800

    纠正 math/round 行为

--- math/round.hpp
+++ math/round.hpp
@@ -8,28 +8,28 @@
 #pragma pop_macro("xuser")
 
 namespace mixc::math_round{
-    inline f32 round_unsafe(f32 a, f32 b){
-        return adv::div_unsafe(a + 0.5, b).quotient;
+    inline f32 round_unsafe(f32 a){
+        return adv::div_unsafe(a + 0.5, 1).quotient;
     }
 
-    inline f64 round_unsafe(f64 a, f64 b){
-        return adv::div_unsafe(a + 0.5, b).quotient;
+    inline f64 round_unsafe(f64 a){
+        return adv::div_unsafe(a + 0.5, 1).quotient;
     }
 
-    inline f80 round_unsafe(f80 a, f80 b){
-        return adv::div_unsafe(a + 0.5, b).quotient;
+    inline f80 round_unsafe(f80 a){
+        return adv::div_unsafe(a + 0.5, 1).quotient;
     }
 
-    inline f32 round(f32 a, f32 b){
-        return inc::div(a + 0.5, b).quotient;
+    inline f32 round(f32 a){
+        return inc::div(a + 0.5, 1).quotient;
     }
 
-    inline f64 round(f64 a, f64 b){
-        return inc::div(a + 0.5, b).quotient;
+    inline f64 round(f64 a){
+        return inc::div(a + 0.5, 1).quotient;
     }
 
-    inline f80 round(f80 a, f80 b){
-        return inc::div(a + 0.5, b).quotient;
+    inline f80 round(f80 a){
+        return inc::div(a + 0.5, 1).quotient;
     }
 }
 
