02e615a2524e2735c446047d952280ab58b9df46
PowerfulCat <x4495@outlook.com>
2020-10-31 13:05:45 +0800

    增强 meta_ctr/cif

--- meta_ctr/cif.hpp
+++ meta_ctr/cif.hpp
@@ -3,7 +3,7 @@
 #pragma push_macro("xuser")
 #undef  xuser
 #define xuser mixc::meta_ctr_cif
-#include"define/base_type.hpp"
+#include"mixc.hpp"
 #pragma pop_macro("xuser")
 
 namespace mixc::meta_ctr_cif{
@@ -17,12 +17,38 @@ namespace mixc::meta_ctr_cif{
         using result = if_false;
     };
 
-    template<bool condition, class if_true, class if_false>
-    using cif = typename meta<condition, if_true, if_false>::result;
+    template<bool c1, class a1, bool c2>
+    struct ei{
+        template<class else_if_type>
+        struct select{
+        private:
+            using t = meta<c1, a1, else_if_type>::result;
+        public:
+            template<bool type_else_if_true>
+            using cei = ei<
+                c1 | c2, t, type_else_if_true
+            >;
+
+            template<class type_else>
+            using ces = meta<c1 | c2, t, type_else>::result;
+        };
+    };
+
+    template<bool if_condition>
+    struct cif{
+        template<class type_if_true>
+        struct select{
+            template<bool type_else_if_true>
+            using cei = ei<
+                if_condition, type_if_true, type_else_if_true
+            >;
+
+            template<class type_else>
+            using ces = meta<if_condition, type_if_true, type_else>::result;
+        };
+    };
 }
 
 #endif
 
-namespace xuser::inc{
-    using ::mixc::meta_ctr_cif::cif;
-}
+xexport(mixc::meta_ctr_cif::cif)
