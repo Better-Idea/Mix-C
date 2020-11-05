afac3f243979d73f50d0104c5fa9caa286171f11
PowerfulCat <x4495@outlook.com>
2020-11-05 15:00:39 +0800

    在 math/cos 中应用 math/private/parallel_lut 功能

--- math/cos.hpp
+++ math/cos.hpp
@@ -4,24 +4,51 @@
 #undef  xuser
 #define xuser mixc::math_cos::inc
 #include"define/base_type.hpp"
+#include"docker/array.hpp"
 #include"macro/xexport.hpp"
 #include"math/const.hpp"
-#include"math/div.hpp"
-#include"math/tan.hpp"
+#include"math/private/parallel_lut.hpp"
 #pragma pop_macro("xuser")
 
-namespace mixc::math_cos::origin{
-    // 注意：
-    // 此 unsafe 函数不带定义域检查，需要使用者保证 x 的范围属于 [-pi, +pi]
-    inline f64 cos_unsafe(f64 x){
-        f64 t  = inc::tan_unsafe(0.5 * x);
-        f64 tt = t * t;
-        return (1.0 - tt) / (1.0 + tt);
-    }
+namespace mixc::math_cos{
+    template<class type, uxx multiple>
+    constexpr auto lut = inc::parallel_lut<multiple>(
+        type(-5.00000000000000000e-01),
+        type(+4.16666666666666644e-02),
+        type(-1.38888888888888873e-03),
+        type(+2.48015873015872982e-05),
+        type(-2.75573192239858883e-07),
+        type(+2.08767569878680961e-09),
+        type(-1.14707455977297245e-11),
+        type(+4.77947733238738525e-14),
+        type(-1.56192069685862277e-16),
+        type(+4.11031762331216533e-19)
+    );
+}
 
-    inline f64 cos(f64 x){
-        x = inc::div(x, inc::pi).remainder;
-        return cos_unsafe(x);
+namespace mixc::math_cos::origin{
+    template<class type, class ... args>
+    inline auto cos(type x0, args ... xn) {
+        constexpr auto multiple = 1 + sizeof...(args);
+        constexpr auto step = sizeof(type) == sizeof(f32) ? 6 : 10;
+
+        auto && x2 = inc::array_view{x0, xn...};
+        auto && xm = inc::array_view{x0, xn...};
+        auto && rx = inc::array<type, multiple>();
+
+        x2.foreach([](type & value){
+            value *= value;
+        });
+
+        xm = x2;
+
+        for(uxx i = 0; i < step; i++){
+            for(uxx j = 0; j < multiple; j++){
+                rx[j] += xm[j] * lut<type, multiple>[i * multiple + j];
+                xm[j] *= x2[j];
+            }
+        }
+        return rx;
     }
 }
 
