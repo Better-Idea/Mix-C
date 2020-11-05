01cace0d9b1fb483afdae2c4364d77da5f637597
PowerfulCat <x4495@outlook.com>
2020-11-05 14:51:32 +0800

    在 math/abs 应用 xexport

--- math/abs.hpp
+++ math/abs.hpp
@@ -1,7 +1,8 @@
 #ifndef xpack_math_abs
 #define xpack_math_abs
+#include"macro/xexport.hpp"
 
-namespace mixc::math_abs{
+namespace mixc::math_abs::origin{
     template<class type>
     inline type abs(type const & value){
         return value < 0 ? -value : value;
@@ -10,6 +11,4 @@ namespace mixc::math_abs{
 
 #endif
 
-namespace xuser{
-    using ::mixc::math_abs::abs;
-}
+xexport_space(mixc::math_abs::origin)
