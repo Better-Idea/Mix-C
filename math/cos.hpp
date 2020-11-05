8d5161b5aca0e267b6af2bdf005c4acbefa19b5c
PowerfulCat <x4495@outlook.com>
2020-11-05 21:13:29 +0800

    让 math/cos 直接从 math/sin 演变

--- math/cos.hpp
+++ math/cos.hpp
@@ -6,49 +6,20 @@
 #include"define/base_type.hpp"
 #include"docker/array.hpp"
 #include"macro/xexport.hpp"
-#include"math/const.hpp"
 #include"math/private/parallel_lut.hpp"
+#include"math/const.hpp"
+#include"math/sin.hpp"
 #pragma pop_macro("xuser")
-
-namespace mixc::math_cos{
-    template<class type, uxx multiple>
-    constexpr auto lut = inc::parallel_lut<multiple>(
-        type(-5.00000000000000000e-01),
-        type(+4.16666666666666644e-02),
-        type(-1.38888888888888873e-03),
-        type(+2.48015873015872982e-05),
-        type(-2.75573192239858883e-07),
-        type(+2.08767569878680961e-09),
-        type(-1.14707455977297245e-11),
-        type(+4.77947733238738525e-14),
-        type(-1.56192069685862277e-16),
-        type(+4.11031762331216533e-19)
-    );
-}
 
 namespace mixc::math_cos::origin{
     template<class type, class ... args>
+    inline auto cos_unsafe(type x0, args ... xn) {
+        return inc::sin_unsafe(x0 + inc::pi / 2, (xn + inc::pi / 2)...);
+    }
+
+    template<class type, class ... args>
     inline auto cos(type x0, args ... xn) {
-        constexpr auto multiple = 1 + sizeof...(args);
-        constexpr auto step = sizeof(type) == sizeof(f32) ? 6 : 10;
-
-        auto && x2 = inc::array_view{x0, xn...};
-        auto && xm = inc::array_view{x0, xn...};
-        auto && rx = inc::array<type, multiple>();
-
-        x2.foreach([](type & value){
-            value *= value;
-        });
-
-        xm = x2;
-
-        for(uxx i = 0; i < step; i++){
-            for(uxx j = 0; j < multiple; j++){
-                rx[j] += xm[j] * lut<type, multiple>[i * multiple + j];
-                xm[j] *= x2[j];
-            }
-        }
-        return rx;
+        return inc::sin(x0 + inc::pi / 2, (xn + inc::pi / 2)...);
     }
 }
 
