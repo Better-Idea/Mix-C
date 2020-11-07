d1228d1267270862d29cc897d559fbe74083f59b
PowerfulCat <x4495@outlook.com>
2020-11-07 16:40:35 +0800

    完善 algo/sort::radix* 支持全系列整数

--- algo/sort.hpp
+++ algo/sort.hpp
@@ -7,6 +7,7 @@
 #include"docker/bit_indicator.hpp"
 #include"interface/can_compare.hpp"
 #include"interface/unified_seq.hpp"
+#include"math/const.hpp"
 #include"macro/xcmp.hpp"
 #include"memop/swap.hpp"
 #include"meta/item_origin_of.hpp"
@@ -14,21 +15,39 @@
 #pragma pop_macro("xuser")
 
 namespace mixc::algo_sort{
-    template<uxx i, class seq_t>
-    inline void radix_sort_core(seq_t r){
-        uxx sum0[256] = {0};
-        uxx sum1[256] = {0};
+    constexpr u08 type_unsigned = 0x00;
+    constexpr u08 type_signed   = 0x80;
+    constexpr u08 mode_asc      = 0x00;
+    constexpr u08 mode_des      = 0xff;
+
+    template<class counter_t, uxx i, u08 type, u08 mode, class seq_t>
+    inline void radix_sort_core(seq_t & r, uxx offset, uxx length){
+        using item_t        = inc::item_origin_of<seq_t>;
+
+        constexpr
+        uxx       top_i     = sizeof(item_t) - 1;
+        counter_t sum0[256];
+        counter_t sum1[256];
         inc::bit_indicator<256> idc;
 
-        for(uxx j = 0; j < r.length(); j++){
-            u08 hex     = u08p(xref r[j])[i];
-            sum0[hex]  += 1;
-            idc.set(hex);
+        for(uxx j = 0; j < length; j++){
+            u08 hex         = (u08p(xref r[j + offset])[i]);
+            u08 idx         = (hex + type) ^ mode;
+            // xhint(hex, idx)
+
+            if (idc.get(idx) == false){
+                idc.set(idx);
+                sum0[idx]   = 1;
+            }
+            else{
+                sum0[idx]  += (1);
+            }
         }
 
-        auto idct       = idc;
-        auto j          = idct.pop_first();
-        auto t          = sum0[j];
+        auto idct           = idc;
+        auto j              = idct.pop_first();
+        auto t              = sum0[j];
+        sum1[j]             = 0;
 
         // origin: 1 0 2 0 0 5 1 0
         // sum0  : 1 0 3 0 0 8 9 0
@@ -41,21 +60,27 @@ namespace mixc::algo_sort{
 
         idct = idc;
 
-        for(uxx j = 0, hex, ofs; hex != not_exist; ){
-            hex         = u08p(xref r[j])[i];
-            ofs         = sum1[hex];
+        for(uxx j = 0, idx, ofs;;){
+            u08 hex     = u08p(xref r[j + offset])[i];
+            idx         = u08((hex + type) ^ mode);
+            ofs         = sum1[idx];
+            // xhint(hex, idx, ofs)
 
             if (j != ofs){
-                inc::swap(xref r[j], xref r[ofs]);
+                inc::swap(xref r[j + offset], xref r[ofs + offset]);
             }
             else{
                 j      += 1;
             }
 
-            if (sum1[hex] += 1; sum1[hex] == sum0[hex]){
-                idc.reset(hex);
-                hex     = idc.index_of_first_set();
-                j       = sum1[hex];
+            if (sum1[idx] += 1; sum1[idx] == sum0[idx]){
+                idc.reset(idx);
+                idx     = idc.index_of_first_set();
+                j       = sum1[idx];
+
+                if (idx == not_exist){
+                    break;
+                }
             }
         }
 
@@ -63,9 +88,20 @@ namespace mixc::algo_sort{
             // 对子分区排序，按下一个字节排
             // 此时 sum0 = sum1
             for(t = 0; not_exist != (j = idct.pop_first()); t = sum0[j]){
-                if (auto new_length = sum0[j] - t - 1; new_length > 1){
-                    auto new_seq    = r.subseq(co{t, sum0[j]});
-                    radix_sort_core<i - 1, seq_t>(new_seq);
+                auto new_length = sum0[j] - t;
+                 //xhint(sum0[j], t);
+
+                if (new_length == 1){ // 只有一个元素不用排序
+                    continue;
+                }
+                if (new_length == 2){ // 两个元素
+                    if ((r[t + offset] > r[t + offset + 1]) ^ (mode == mode_des)){
+                        inc::swap(xref r[t + offset], xref r[t + offset + 1]);
+                    }
+                }
+                // TODO：else if (new_length < small_length) ========================================================================
+                else{
+                    radix_sort_core<counter_t, i - 1, type_unsigned, mode, seq_t>(r, t + offset, new_length);
                 }
             }
         }
@@ -98,15 +134,34 @@ namespace mixc::algo_sort{
             }
         }
 
-        // 注意：
-        // 实验性功能
-        // 目前只支持无符号整数的快速排序
+        #define xgen(type,mode)                                 \
+        if (seq.length() <= inc::max_value_of<type>){           \
+            radix_sort_core<type,                               \
+                (sizeof(item_t) - 1),                           \
+                (item_t(-1) > 0) ? type_unsigned : type_signed, \
+                (mode_asc)                                      \
+            >(                                                  \
+                (seq_t &)(seq), 0, seq.length()                 \
+            );                                                  \
+        } else
+
         template<inc::unified_seq_t seq_t>
-        static void radix(seq_t const & seq){
+        static void radix_asc(seq_t const & seq){
             using item_t = inc::item_origin_of<seq_t>;
-            radix_sort_core<sizeof(item_t) - 1>(
-                inc::unified_seq<seq_t>(seq)
-            );
+            xgen(u08, mode_asc)
+            xgen(u16, mode_asc)
+            xgen(u32, mode_asc)
+            xgen(u64, mode_asc);
+        }
+
+        template<inc::unified_seq_t seq_t>
+        static void radix_des(seq_t const & seq){
+            using item_t = inc::item_origin_of<seq_t>;
+
+            xgen(u08, mode_des)
+            xgen(u16, mode_des)
+            xgen(u32, mode_des)
+            xgen(u64, mode_des);
         }
     };
 }
