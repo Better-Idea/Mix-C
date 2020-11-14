9e9e522d1bcd676563072c56f853a1919a779d3c
PowerfulCat <x4495@outlook.com>
2020-11-14 20:57:00 +0800

    添加 interface/private/check.hpp 用于辅助 seqptr/ranger/unified_seq 进行类型检查

---
+++ interface/private/check.hpp
@@ -0,0 +1,52 @@
+#ifndef xpack_interface_private_check
+#define xpack_interface_private_check
+#pragma push_macro("xuser")
+#undef  xuser
+#define xuser mixc::interface_private_check::inc
+#include"define/base_type.hpp"
+#include"macro/xexport.hpp"
+#include"meta/item_origin_of.hpp"
+#pragma pop_macro("xuser")
+
+namespace mixc::interface_private_check::origin{
+    template<class seq_t>
+    concept check_initializer_list = requires(seq_t list, uxx size, void const * ptr){
+        size    = list.size();
+        ptr     = list.begin();
+    };
+
+    template<class seq_t, class item_t>
+    concept check_initializer_listx = requires(seq_t list, uxx size, item_t const * ptr){
+        size    = list.size();
+        ptr     = list.begin();
+    };
+
+    template<class seq_t>
+    concept check_array  = requires(seq_t list, void const * ptr){
+        ptr     = list;
+    };
+
+    template<class seq_t, class item_t>
+    concept check_arrayx = requires(seq_t list, item_t const * ptr){
+        ptr     = list;
+    };
+
+    template<class seq_t>
+    concept check_indexable = requires(seq_t list, inc::item_origin_of<seq_t> & (seq_t::*indexer)(uxx)){
+        indexer = & seq_t::operator[];
+    };
+
+    template<class seq_t, class item_t>
+    concept check_indexablex = requires(seq_t list, item_t & (seq_t::*indexer)(uxx)){
+        indexer = & seq_t::operator[];
+    };
+
+    template<class seq_t>
+    concept check_length = requires(seq_t list, uxx length){
+        length  = list.length();
+    };
+}
+
+#endif
+
+xexport_space(mixc::interface_private_check::origin)
