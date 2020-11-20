15bc6686bfd2a0f402133314342a5c24d790a7b6
PowerfulCat <x4495@outlook.com>
2020-11-20 13:54:29 +0800

    让 docker/tuple 支持包括原始字符串在内的原始数组初始化

--- docker/tuple.hpp
+++ docker/tuple.hpp
@@ -4,32 +4,56 @@
 #undef  xuser
 #define xuser mixc::docker_tuple::inc
 #include"define/nullref.hpp"
+#include"docker/array.hpp"
 #include"macro/xexport.hpp"
 #include"macro/xstruct.hpp"
+#include"meta/is_origin_array.hpp"
+#include"meta/item_origin_of.hpp"
+#include"meta/remove_ptr.hpp"
 #pragma pop_macro("xuser")
 #undef  xuser
+
+namespace mixc::docker_tuple{
+    template<class a0>
+    inline auto meta(){
+        if constexpr (inc::is_origin_array<a0>){
+            using item_t = inc::item_origin_of<a0>;
+            return (inc::array<item_t, sizeof(a0) / sizeof(item_t)> *)nullptr;
+        }
+        else{
+            return (a0 *)nullptr;
+        }
+    }
+
+    template<class a0>
+    using item_t = inc::remove_ptr<
+        decltype(meta<a0>())
+    >;
+}
 
 namespace mixc::docker_tuple::origin{
     template<class a0 = decltype(nullptr), class ... args>
     xstruct(
         xtmpl(tuple, a0, args...),
         xprob(tuple<args...>),
-        xprif(value, a0)
+        xprif(value, mutable item_t<a0>)
     )
+        using base = tuple<args...>;
+
         template<class ...>
         constexpr tuple(){}
 
         constexpr tuple(a0 const & first, args const & ... result) : 
-            value(first), tuple<args...>(result...){
+            value(*(item_t<a0> *)& first), base(result...){
         }
 
         template<uxx index>
-        auto & get(){
+        auto & get() const {
             if constexpr (index == 0){
                 return value;
             }
             else{
-                return tuple<args...>::template get<index - 1>();
+                return base::template get<index - 1>();
             }
         }
 
@@ -48,8 +72,10 @@ namespace mixc::docker_tuple::origin{
     xstruct(
         xspec(tuple, decltype(nullptr))
     )
+        constexpr tuple(){}
+
         template<uxx index>
-        auto & get(){
+        auto & get() const {
             return inc::nullref;
         }
     $
