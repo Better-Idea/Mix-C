40866778be0e995c62bff7e8fd4c9c1d8b4f89b1
PowerfulCat <x4495@outlook.com>
2020-11-20 21:15:55 +0800

    添加 lock/builtin_lock.hpp，提供基础结构内部锁

---
+++ lock/builtin_lock.hpp
@@ -0,0 +1,38 @@
+#ifndef xpack_lock_builtin_lock
+#define xpack_lock_builtin_lock
+#pragma push_macro("xuser")
+#undef  xuser
+#define xuser mixc::lock_builtin_lock::inc
+#include"define/base_type.hpp"
+#include"lock/mutex.hpp"
+#include"macro/xexport.hpp"
+#include"macro/xstruct.hpp"
+#pragma pop_macro("xuser")
+
+namespace mixc::lock_builtin_lock::origin{
+    xstruct(
+        xname(with_lwmutex)
+    )
+        constexpr with_lwmutex(){}
+
+        template<auto opr, class this_t, class invoke_t>
+        void lock(this_t * ptr, invoke_t const & call) const {
+            inc::mutex::lock(ptr, 0, call); // 第 0 位作为互斥位
+        }
+    $
+
+    xstruct(
+        xname(lock_free)
+    )
+        constexpr lock_free(){}
+
+        template<auto opr, class this_t, class callback>
+        void lock(this_t, callback const & call) const {
+            call();
+        }
+    $
+}
+
+#endif
+
+xexport_space(mixc::lock_builtin_lock::origin)
