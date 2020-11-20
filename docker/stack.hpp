b26549ae469f1e05de3285cde165cb08a8544219
PowerfulCat <x4495@outlook.com>
2020-11-20 21:20:06 +0800

    让 docker/stack.hpp 适应更改

--- docker/stack.hpp
+++ docker/stack.hpp
@@ -5,9 +5,11 @@
 #include"macro/xfinal.hpp"
 
 namespace xuser{
-    using xusing_docker_stack::with_stack_lock;
+    using xusing_docker_stack::with_stack_policy_lock;
+    using xusing_docker_stack::with_lwmutex;
+    using xusing_docker_stack::lock_free;
 
-    template<class item_t, class lock_t = void>
+    template<class item_t, xusing_docker_stack::is_attached_lock lock_t = xusing_docker_stack::lock_free>
     xfinal_tmpl(xusing_docker_stack, stack, item_t, lock_t);
 }
 
