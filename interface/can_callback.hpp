d6078c046d4d90fb585c853064ef392796618eb0
PowerfulCat <x4495@outlook.com>
2020-11-23 16:01:58 +0800

    让 interface/can_callback.hpp 应用 concept + xinterface 模式

--- interface/can_callback.hpp
+++ interface/can_callback.hpp
@@ -3,58 +3,25 @@
 #pragma push_macro("xuser")
 #undef  xuser
 #define xuser mixc::interface_can_callback::inc
-#include"macro/private/callable.hpp"
-#include"memop/signature.hpp"
-#include"memop/addressof.hpp"
-#include"meta/is_same.hpp"
-#include"meta/remove_membership.hpp"
-#include"mixc.hpp"
+#include"macro/xinterface.hpp"
+#include"macro/xnew.hpp"
 #pragma pop_macro("xuser")
 
-namespace mixc::interface_can_callback{
-    template<class func> struct can_callback;
-    template<class ret, class ... args> 
-    xstruct(
-        xspec(can_callback, ret(args...)),
-        xpubb(inc::callable)
-    )
-        using base_t = inc::callable;
-        using signature = inc::signature<ret(args...)>;
-        using base_t::operator=;
-        using base_t::operator==;
-
-        can_callback(){}
-        can_callback(decltype(nullptr)){}
-
-        // 在 requires 中使用 is_same 代替 more_fit 达到区分 lambda 的目的
-        template<class object> requires ( 
-            inc::is_same<
-                decltype(& object::operator()),
-                ret(object::*)(args...) const
-            > or 
-            inc::is_same<
-                decltype(& object::operator()),
-                ret(object::*)(args...)
-            > 
-        )
-        can_callback(object const & impl){
-            __object    = inc::addressof(impl);
-            __func_list = (void **)signature::check(& object::operator());
-        }
-
-        ret operator()(args ... list) const {
-            return signature::call(__object, (void *)__func_list, list...);
-        }
-    $
+namespace mixc::interface_can_callback::origin{
+    template<class func> struct icallback;
 
     template<class ret, class ... args> 
-    struct can_callback<ret(*)(args...)> : 
-        can_callback<ret(args...)>{
-        using the_t = can_callback<ret(args...)>;
-        using the_t::the_t;
+    xinterface(
+        xspec(icallback, ret(args...)),
+        xoper(ret(args...))
+    );
+
+    template<class functor_t, class signature>
+    concept can_callback = requires(functor_t const & call, icallback<signature> * ptr){
+        xnew(ptr) icallback<signature>(call);
     };
 }
 
 #endif
 
-xexport(mixc::interface_can_callback::can_callback)
+xexport_space(mixc::interface_can_callback::origin)
