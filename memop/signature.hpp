07f0079d177497ab0f92ae2df596bcc1b2c1507e
PowerfulCat <x4495@outlook.com>
2020-11-23 14:25:42 +0800

    重定义 memop/signature.hpp 职责

--- memop/signature.hpp
+++ memop/signature.hpp
@@ -10,50 +10,18 @@
 namespace mixc::memop_signature{
     template<class function> struct signature;
 
-    template<class ret, class ... args>
-    struct signature<ret(args...)>{
-        #define xgen(...)                                                               \
-            template<class object>                                                      \
-            static auto check(ret (object::* this_call)(args...) __VA_ARGS__){          \
-                union {                                                                 \
-                    voidp               result;                                         \
-                    decltype(this_call) mem;                                            \
-                }u;                                                                     \
-                u.mem = this_call;                                                      \
-                return u.result;                                                        \
-            }
-        xgen()
-        xgen(const)
-        #undef xgen
-
-        #define xgen(...)                                                               \
-            template<class object>                                                      \
-            static constexpr auto has(ret (object::*)(args...) __VA_ARGS__){            \
-                return true;                                                            \
-            }
-        xgen()
-        xgen(const)
-        #undef xgen
-
-        static ret call(const void * self, voidp this_call, args ... list){
-            struct foo{};
-            union {
-                voidp       mem;
-                ret (     * gnu_call)(const void *, args...);
-                ret (foo::* msvc_call)(args...);
-            } u;
-
-            u.mem = this_call;
-
-            // 应对不同平台具有差异的 this_call 
-            #if xis_msvc
-            using foop = foo *;
-            return (foop(self)->*u.msvc_call)(list...);
-            #else
-            return u.gnu_call(self, list...);
-            #endif
-        }
+    #define xgen(...)                                                               \
+    template<class ret, class ... args>                                             \
+    struct signature<ret(args...) __VA_ARGS__>{                                     \
+        template<class owner>                                                       \
+        static auto fetch(ret(owner::* member_func)(args...) __VA_ARGS__){          \
+            return member_func;                                                     \
+        }                                                                           \
     };
+
+    xgen()
+    xgen(const)
+    #undef xgen
 }
 
 #endif
