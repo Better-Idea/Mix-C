92694233f1aeda0f3d4397c08682b6061cedc67c
PowerfulCat <x4495@outlook.com>
2020-11-23 21:54:02 +0800

    在 macro/xsv.hpp 调整模板参数

--- macro/xsv.hpp
+++ macro/xsv.hpp
@@ -10,13 +10,13 @@
 #pragma pop_macro("xuser")
 
 namespace mixc::macro_xsv{
-    template<class item>
+    template<class item_t>
     struct static_string_holder {
     private:
         mixc::gc_private_token::origin::token_plus * info;
     public:
-        item * ptr() const {
-            return (item *)(info + 1);
+        item_t * ptr() const {
+            return (item_t *)(info + 1);
         }
         uxx length() const {
             return info->length;
@@ -24,18 +24,18 @@ namespace mixc::macro_xsv{
     };
 }
 
-#define __xsv__(item,padding,str)                                                   \
+#define __xsv__(item_t,padding,str)                                                 \
 struct sv{                                                                          \
     sv() : buf(padding str) {                                                       \
-        xnew (this) ::mixc::gc_token::token_plus(sizeof(str) / sizeof(item) - 1);   \
+        xnew (this) ::mixc::gc_token::token_plus(sizeof(str) / sizeof(item_t) - 1); \
     }                                                                               \
-    item buf[sizeof(padding str) / sizeof(item)];                                   \
+    item_t buf[sizeof(padding str) / sizeof(item_t)];                               \
 };                                                                                  \
 static sv   __mem__;                                                                \
 static sv * __tmp__ = & __mem__;                                                    \
-return *(mixc::macro_xsv::static_string_holder<item> *)(& __tmp__)
+return *(mixc::macro_xsv::static_string_holder<item_t> *)(& __tmp__)
 
-#define xsv(item,padding,str)       ([]() { __xsv__(item, padding, str); }())
+#define xsv(item_t,padding,str)       ([]() { __xsv__(item_t, padding, str); }())
 #endif
 
 xexport(mixc::macro_xsv::static_string_holder)
