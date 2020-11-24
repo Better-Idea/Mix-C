f4a9c1606c710ce5c67005368b2abf942c308440
PowerfulCat <x4495@outlook.com>
2020-11-24 18:57:36 +0800

    在 interface/unified_seq.hpp 中添加 can_unified_seqlizex

--- interface/unified_seq.hpp
+++ interface/unified_seq.hpp
@@ -36,6 +36,9 @@ namespace mixc::meta_unified_seq::origin{
     template<class seq_t>
     concept can_unified_seqlize = inc::can_seqptrlize<seq_t> or inc::can_rangerlize<seq_t>;
 
+    template<class seq_t, class item_t>
+    concept can_unified_seqlizex = inc::can_seqptrlizex<seq_t, item_t> or inc::can_rangerlizex<seq_t, item_t>;
+
     template<can_unified_seqlize seq_t>
     using unified_seq = decltype(
         meta<seq_t>()
