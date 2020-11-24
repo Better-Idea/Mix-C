686d48853c4fca32e1027c71f166304dcb0eef1f
PowerfulCat <x4495@outlook.com>
2020-11-24 18:02:43 +0800

    移除 lang/cxx/usage.cpp

--- lang/cxx/usage.cpp
+++
@@ -1,9 +0,0 @@
-#ifndef xpack_lang_cxx_usage
-#define xpack_lang_cxx_usage
-#define xuser mixc::lang_cxx_usage::inc
-#include"io/tty.hpp"
-#include"lang/cxx/+.hpp"
-#include"lang/cxx.hpp"
-#include"macro/xhint.hpp"
-
-xexport(mixc::lang_cxx_usage::usage)
