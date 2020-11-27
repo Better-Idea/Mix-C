a51763104878b21d8cf99db6d4f681b21454c6c8
PowerfulCat <x4495@outlook.com>
2020-11-27 20:15:37 +0800

    让 memop/cmp 行为符合 memop/ 系列规范

--- memop/cmp.hpp
+++ memop/cmp.hpp
@@ -7,6 +7,7 @@
 #include"meta/is_enum.hpp"
 #include"meta/is_same.hpp"
 #include"meta/is_union.hpp"
+#include"meta/has_comparator.hpp"
 #include"meta/unsigned_type.hpp"
 #include"memop/seqlize.hpp"
 #include"mixc.hpp"
@@ -15,18 +16,20 @@
 namespace mixc::memop_cmp::origin{
     template<class a>
     inline ixx cmp(a const & left, a const & right){
-        if constexpr (inc::is_class<a> or inc::is_enum<a> or inc::is_union<a>){
-            auto l = inc::seqlize(left);
-            auto r = inc::seqlize(right);
+        auto l = inc::seqlize(left);
+        auto r = inc::seqlize(right);
 
-            for (uxx i = 0; i < l.length(); i++){
-                if (ixx s = cmp(l[i], r[i]); s != 0){
-                    return s;
-                }
+        for (uxx i = 0; i < l.length(); i++){
+            if (ixx s = cmp(l[i], r[i]); s != 0){
+                return s;
             }
-            return 0;
         }
-        else if constexpr (inc::is_same<asciis, a> or inc::is_same<words, a>){
+        return 0;
+    }
+
+    template<class a>
+    inline ixx cmp_with_operator(a const & left, a const & right){
+        if constexpr (inc::is_same<asciis, a> or inc::is_same<words, a>){
             auto * l        = left;
             auto * r        = right;
             using item_t    = inc::unsigned_type<decltype(l[0])>;
@@ -38,10 +41,13 @@ namespace mixc::memop_cmp::origin{
             }
             return ixx(item_t(l[0]) - item_t(r[0]));
         }
-        else{
+        else if constexpr (inc::has_comparator<a>){
             return
                 left > right ? 1 : 
                 left < right ? -1 : 0;
+        }
+        else{
+            return cmp(left, right);
         }
     }
 }
