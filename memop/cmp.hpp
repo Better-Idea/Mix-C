1aa5b4d0e79d67d5ad992c91aa9d1d19dbc4702f
PowerfulCat <x4495@outlook.com>
2020-11-27 17:31:11 +0800

    在 memop/cmp.hpp 中特化原始字符串的比较

--- memop/cmp.hpp
+++ memop/cmp.hpp
@@ -5,12 +5,14 @@
 #define xuser mixc::memop_cmp::inc
 #include"meta/is_class.hpp"
 #include"meta/is_enum.hpp"
+#include"meta/is_same.hpp"
 #include"meta/is_union.hpp"
+#include"meta/unsigned_type.hpp"
 #include"memop/seqlize.hpp"
 #include"mixc.hpp"
 #pragma pop_macro("xuser")
 
-namespace mixc::memop_cmp{
+namespace mixc::memop_cmp::origin{
     template<class a>
     inline ixx cmp(a const & left, a const & right){
         if constexpr (inc::is_class<a> or inc::is_enum<a> or inc::is_union<a>){
@@ -24,6 +26,18 @@ namespace mixc::memop_cmp{
             }
             return 0;
         }
+        else if constexpr (inc::is_same<asciis, a> or inc::is_same<words, a>){
+            auto * l        = left;
+            auto * r        = right;
+            using item_t    = inc::unsigned_type<decltype(l[0])>;
+
+            for(; l[0] != '\0'; l++, r++){
+                if (l[0] != r[0]){
+                    break;
+                }
+            }
+            return ixx(item_t(l[0]) - item_t(r[0]));
+        }
         else{
             return
                 left > right ? 1 : 
@@ -34,26 +48,26 @@ namespace mixc::memop_cmp{
 
 #define __xcmpop__(friend_token,...)                                                            \
 friend_token inline bool operator >  (__VA_ARGS__ const & left, __VA_ARGS__ const & right){     \
-    return mixc::memop_cmp::cmp(left, right) > 0;                                               \
+    return ::mixc::memop_cmp::origin::cmp(left, right) > 0;                                     \
 }                                                                                               \
 friend_token inline bool operator >= (__VA_ARGS__ const & left, __VA_ARGS__ const & right){     \
-    return mixc::memop_cmp::cmp(left, right) >= 0;                                              \
+    return ::mixc::memop_cmp::origin::cmp(left, right) >= 0;                                    \
 }                                                                                               \
 friend_token inline bool operator <  (__VA_ARGS__ const & left, __VA_ARGS__ const & right){     \
-    return mixc::memop_cmp::cmp(left, right) <  0;                                              \
+    return ::mixc::memop_cmp::origin::cmp(left, right) <  0;                                    \
 }                                                                                               \
 friend_token inline bool operator <= (__VA_ARGS__ const & left, __VA_ARGS__ const & right){     \
-    return mixc::memop_cmp::cmp(left, right) <= 0;                                              \
+    return ::mixc::memop_cmp::origin::cmp(left, right) <= 0;                                    \
 }                                                                                               \
 friend_token inline bool operator == (__VA_ARGS__ const & left, __VA_ARGS__ const & right){     \
-    return mixc::memop_cmp::cmp(left, right) == 0;                                              \
+    return ::mixc::memop_cmp::origin::cmp(left, right) == 0;                                    \
 }                                                                                               \
 friend_token inline bool operator != (__VA_ARGS__ const & left, __VA_ARGS__ const & right){     \
-    return mixc::memop_cmp::cmp(left, right) != 0;                                              \
+    return ::mixc::memop_cmp::origin::cmp(left, right) != 0;                                    \
 }
 #endif
 
 #define xcmpop(...)             __xcmpop__(,__VA_ARGS__)
 #define xcmpop_friend(...)      __xcmpop__(friend,__VA_ARGS__)
 
-xexport(mixc::memop_cmp::cmp)
+xexport_space(mixc::memop_cmp::origin)
