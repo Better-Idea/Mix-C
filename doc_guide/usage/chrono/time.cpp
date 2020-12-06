7f9f363aa790385a45a534d8311a5ddecb146ef2
PowerfulCat <x4495@outlook.com>
2020-12-06 08:28:27 +0800

    添加 doc_guide/usage/chrono/time.cpp

---
+++ doc_guide/usage/chrono/time.cpp
@@ -0,0 +1,87 @@
+#define xuser mixc::powerful_cat
+#include"chrono/day.hpp"
+#include"chrono/time.hpp"
+#include"mixc.hpp"
+
+namespace xuser{
+    void demo(){
+        uxx demo = 0;
+
+        // 演示：创建时间
+        {
+            xhint(demo);
+            demo           += 1;
+            
+            // 创建时间
+            time<> the_time;
+
+            // 设置时间
+            the_time.hour(7).minute(22).second(40).milisecond(123);
+
+            // 打印时间
+            xhint(the_time.hour(), the_time.minute(), the_time.second(), the_time.milisecond());
+        }
+
+        // 演示：时间校验
+        {
+            xhint(demo);
+            demo           += 1;
+
+            // 创建时间
+            time<> the_time;
+
+            // hour 可以大于 23
+            the_time.hour(24).minute(zero).second(zero).milisecond(zero);
+            xhint(the_time.is_valid(), the_time.hour(), the_time.minute(), the_time.second(), the_time.milisecond());
+
+            // 但对于 24 小时制的时间 hour 不能大于 23
+            xhint(the_time.is_valid_24h_clock(), the_time.hour(), the_time.minute(), the_time.second(), the_time.milisecond());
+            the_time.hour(23);
+            xhint(the_time.is_valid_24h_clock(), the_time.hour(), the_time.minute(), the_time.second(), the_time.milisecond());
+
+            // minute 不能大于 59
+            the_time.hour(zero).minute(59).second(zero).milisecond(zero);
+            xhint(the_time.is_valid(), the_time.hour(), the_time.minute(), the_time.second(), the_time.milisecond());
+            the_time.minute(60);
+            xhint(the_time.is_valid(), the_time.hour(), the_time.minute(), the_time.second(), the_time.milisecond());
+
+            // second 不能大于 59
+            the_time.minute(59).second(59);
+            xhint(the_time.is_valid(), the_time.hour(), the_time.minute(), the_time.second(), the_time.milisecond());
+            the_time.second(60);
+            xhint(the_time.is_valid(), the_time.hour(), the_time.minute(), the_time.second(), the_time.milisecond());
+
+            // 当允许润秒时 second 最大可以为 60
+            xhint(the_time.is_valid(allow_leap_second), the_time.hour(), the_time.minute(), the_time.second(), the_time.milisecond());
+
+            // 但不能超过 60
+            the_time.second(61);
+            xhint(the_time.is_valid(allow_leap_second), the_time.hour(), the_time.minute(), the_time.second(), the_time.milisecond());
+
+            // milisecond 不能大于 999
+            the_time.second(zero).milisecond(999);
+            xhint(the_time.is_valid(), the_time.hour(), the_time.minute(), the_time.second(), the_time.milisecond());
+            the_time.milisecond(1000);
+            xhint(the_time.is_valid(), the_time.hour(), the_time.minute(), the_time.second(), the_time.milisecond());
+        }
+
+        // 演示：计算总毫秒数
+        {
+            xhint(demo);
+            demo           += 1;
+
+            // 创建时间
+            time<>  the_time;
+            u64     total_ms;
+            the_time.hour(1).minute(2).second(3).milisecond(4);
+
+            total_ms        = the_time.total_milisecond();
+            xhint(total_ms);
+        }
+    }
+}
+
+int main(){
+    xuser::demo();
+    return 0;
+}
