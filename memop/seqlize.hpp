a31e77cf9a96fb884d5d061f7e0ccc09a01dcd5c
PowerfulCat <x4495@outlook.com>
2020-12-06 12:02:55 +0800

    调整 memop/seqlize.hpp

--- memop/seqlize.hpp
+++ memop/seqlize.hpp
@@ -12,15 +12,17 @@ namespace mixc::memop_seqlize{
     // 把type转换成指定长度的无符号整数指针
     template<class type>
     inline auto seqlize(type const & value){
-        #define xgen(size)                                              \
-        if constexpr (sizeof(type) % size == 0){                        \
-            return inc::seqptr<type>(xref value, sizeof(type) / size);  \
+        #define xgen(unsigned_t)                                                \
+        if constexpr (sizeof(type) % sizeof(unsigned_t) == 0){                  \
+            return inc::seqptr<unsigned_t>(                                     \
+                (unsigned_t *)(xref value), sizeof(type) / sizeof(unsigned_t)   \
+            );                                                                  \
         } else
 
-        xgen(8)
-        xgen(4)
-        xgen(2)
-        xgen(1);
+        xgen(u64)
+        xgen(u32)
+        xgen(u16)
+        xgen(u08);
         #undef xgen
     }
 }
