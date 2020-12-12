ba261bedbc1a4bada0f67261c8e1f0c44558173d
PowerfulCat <x4495@outlook.com>
2020-12-12 22:57:13 +0800

    更新 doc_guide/usage/algo/heap_root.cpp

--- doc_guide/usage/algo/heap_root.cpp
+++ doc_guide/usage/algo/heap_root.cpp
@@ -61,6 +61,35 @@ namespace xuser{
                 xhint(v);
             }
         }
+
+        // 演示：heap_root 默认生成小根堆 pop 指定索引的元素
+        {
+            xhint(demo);
+            demo               += 1;
+
+            array<uxx, 6> ary;
+
+            xhint("origin order");
+
+            for(uxx i = 0; i < ary.length(); i++){
+                uxx length      = i;
+                uxx v           = random<uxx>() % 10;
+                heap_root::push(ary, length, v);
+                xhint(v);
+            }
+
+            xhint("asc order");
+
+            // 取出 ary[3]，并保持小根堆结构
+            uxx v3              = heap_root::pop_by_index(ary, ary.length(), 3);
+            xhint(v3);
+
+            for(uxx i = ary.length() - 1; i-- > 0;){
+                uxx length      = i + 1;
+                uxx v           = heap_root::pop(ary, length);
+                xhint(v);
+            }
+        }
     }
 }
 
