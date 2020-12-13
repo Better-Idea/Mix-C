f027b850792517d4a3ab83ce1c2d98e08dc915b7
PowerfulCat <x4495@outlook.com>
2020-12-13 20:13:41 +0800

    添加一些注释 @ math/random.hpp

--- math/random.hpp
+++ math/random.hpp
@@ -18,12 +18,16 @@ namespace mixc::math_random::inc{
     inline u64 random() {
         constexpr u64 change_period = 0x3f;
 
+        // 这里维持了随机的不确定性
         if ((x & change_period) == 0){
             x += inc::time_stamp();
         }
 
+        // 这里维持了随机的稳定性
         x += inc::ring_shift_left(y, uxx(x));
         y += inc::ring_shift_left(x, uxx(y));
+
+        // 只要没人知道它下一步会变成什么，那么它就是随机的。
         return y;
     }
 }
