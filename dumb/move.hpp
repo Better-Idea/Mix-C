a4fe8ae0667b816d18e2be1b39f2cf6718385b68
PowerfulCat <x4495@outlook.com>
2020-12-22 20:02:22 +0800

    添加 dumb/move.hpp 补充移动语义

---
+++ dumb/move.hpp
@@ -0,0 +1,18 @@
+#ifndef xpack_dumb_mirror
+#define xpack_dumb_mirror
+#pragma push_macro("xuser")
+#undef  xuser
+#define xuser mixc::dumb_move::inc
+#include"macro/xexport.hpp"
+#pragma pop_macro("xuser")
+
+namespace mixc::dumb_move::origin{
+    template<class type>
+    inline type && move(type const & value){
+        return (type &&)(type &)value;
+    }
+}
+
+#endif
+
+xexport_space(mixc::dumb_move::origin)
