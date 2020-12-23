ab63fc476be6d2240a42569492e4afb1d0e58094
PowerfulCat <x4495@outlook.com>
2020-12-23 16:03:28 +0800

    调整行为 @ docker/shared.hpp

--- docker/shared.hpp
+++ docker/shared.hpp
@@ -4,6 +4,7 @@
 #undef  xuser
 #define xuser mixc::docker_shared::inc
 #include"gc/ref.hpp"
+#include"meta/has_constructor.hpp"
 #include"mixc.hpp"
 #pragma pop_macro("xuser")
 
@@ -13,48 +14,45 @@ namespace mixc::docker_shared{
     struct shared : inc::ref_ptr<shared<type>, type>{
         using the_t  = shared<type>;
         using base_t = inc::ref_ptr<shared<type>, type>;
-        using base_t::operator=;
         using base_t::operator==;
         using base_t::operator!=;
     public:
-        shared() = default;
+        shared()                            = default;
+        shared(the_t const &)               = default;
+        shared(the_t &&)                    = default;
+        the_t & operator=(the_t const &)    = default;
+        the_t & operator=(the_t &&)         = default;
 
-        shared(type const & value) : 
+        explicit shared(type const & value) : 
             base_t(::init_now, value){
         }
 
-        explicit shared(::init_t) : 
-            base_t(::init_now) {}
-
         template<class ... args>
+        requires(inc::has_constructor<type, void(args const & ...)>)
         explicit shared(::init_t, args const & ... list) : 
             base_t(::init_now, list...) {}
 
         template<class ... args>
+        requires(inc::has_constructor<type, void(args const & ...)>)
         shared<type> & operator()(::init_t, args const & ... list){
             the_t{init_now, list...}.swap(this);
             return the;
         }
 
-        operator type & () {
+        type & operator * () {
             return * base_t::operator->();
         }
 
-        operator type const & () const {
+        type const & operator * () const {
             return * base_t::operator->();
-        }
-
-        type const * operator->() const {
-            return base_t::operator->();
         }
 
         type * operator->() {
             return base_t::operator->();
         }
 
-        type const & operator= (type const & value){
-            operator type & () = value;
-            return value;
+        type const * operator->() const {
+            return base_t::operator->();
         }
     };
 }
