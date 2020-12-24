79ff34b386b9b3ee5190654ccb4a7d40fec88f71
PowerfulCat <x4495@outlook.com>
2020-12-24 21:31:48 +0800

    添加 utils/tick.hpp 用于时钟滴答计数

---
+++ utils/tick.hpp
@@ -0,0 +1,33 @@
+#ifndef xpack_utils_tick
+#define xpack_utils_tick
+#pragma push_macro("xuser")
+#undef  xuser
+#define xuser mixc::utils_tick::inc
+#include"define/base_type.hpp"
+#include"instruction/time_stamp.hpp"
+#include"mixc.hpp"
+#pragma pop_macro("xuser")
+
+namespace mixc::utils_tick::origin{
+    xstruct(
+        xname(tick),
+        xprif(value, u64)
+    )
+        constexpr tick(){
+            restart();
+        }
+
+        void restart(){
+            value = inc::time_stamp();
+        }
+
+        operator u64 () const {
+            u64 clocks = inc::time_stamp() - value;
+            return clocks;
+        }
+    $
+}
+
+#endif
+
+xexport_space(mixc::utils_tick::origin)
