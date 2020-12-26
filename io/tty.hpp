38ac5452b0e81fe5f37bf5fd274b93e7f651a6dc
PowerfulCat <x4495@outlook.com>
2020-12-26 11:28:19 +0800

    适应更改 @ io/tty.hpp

--- io/tty.hpp
+++ io/tty.hpp
@@ -3,6 +3,7 @@
 #pragma push_macro("xuser")
 #undef  xuser
 #define xuser mixc::io_tty::inc
+#include"configure.hpp"
 #include"define/base_type.hpp"
 #include"interface/can_alloc.hpp"
 #include"io/private/tty.hpp"
@@ -12,6 +13,7 @@
 #include"lang/cxx.hpp"
 #include"macro/xexport.hpp"
 #include"memory/allocator.hpp"
+#include"meta/has_cast.hpp"
 #pragma pop_macro("xuser")
 
 namespace mixc::io_tty{
@@ -51,30 +53,48 @@ namespace mixc::io_tty{
             inc::print_flush();
         }
 
+        void write(char chr){
+            inc::print_core(& chr, 1);
+        }
+
         template<class a0, class ... args>
         void write(a0 const & first, args const & ... list) const {
             using namespace inc::ph;
+            inc::c08  content;
             char      buf_stack[128];
             char *    buf_heap   = nullptr;
-            inc::c08  content    = phg { first, list... } >> [&](uxx length) {
-                auto ptr         = buf_stack;
+            auto &&   format     = [&](inc::c08 fmt, auto && ph){
+                return ph.format(fmt, [&](uxx length) {
+                    auto ptr         = buf_stack;
 
-                if (length > sizeof(buf_stack) / sizeof(buf_stack[0])){
-                    ptr = buf_heap = inc::alloc<char>(
-                        inc::memory_size(length)
-                    );
-                }
-                return ptr;
+                    if (length > sizeof(buf_stack) / sizeof(buf_stack[0])){
+                        ptr = buf_heap = inc::alloc<char>(
+                            inc::memory_size(length)
+                        );
+                    }
+                    return ptr;
+                });
             };
+
+            if constexpr (inc::has_cast<inc::c08, a0 const &> and sizeof...(list) > 0){
+                content         = format(first, v{list...});
+            }
+            else{
+                content         = format(inc::c08{}, v{first, list...});
+            }
 
             if (inc::print_core(asciis(content), content.length()); buf_heap != nullptr){
                 inc::free(buf_heap, inc::memory_size(content.length()));
             }
         }
+
+        void write_line(){
+            inc::print_core(xendline, 1);
+        }
         
-        template<class ... args>
-        void write_line(args const & ... list) const {
-            write(list..., '\n');
+        template<class a0, class ... args>
+        void write_line(a0 const & first, args const & ... list) const {
+            write(first, list..., xendline);
         }
 
         void read_line(inc::ialloc<char> alloc) const {
