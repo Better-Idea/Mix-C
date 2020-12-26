8da3c794a09309a895c7093681de5478b4e17363
PowerfulCat <x4495@outlook.com>
2020-12-26 20:34:09 +0800

    适应更改 @ discuss/talk_about_gc.cpp

--- discuss/talk_about_gc.cpp
+++ discuss/talk_about_gc.cpp
@@ -8,9 +8,9 @@
 #include"macro/xhint.hpp"
 #include"memory/allocator.hpp"
 #include"mixc.hpp"
-#pragma pop_macro("xuser")
+#include"utils/counter.hpp"
 
-namespace mixc::talk_about_gc::inc{
+namespace xuser{
     struct ax;
     struct bx;
 
@@ -57,20 +57,21 @@ namespace mixc::talk_about_gc::inc{
     ) $
 
     void test(){
-        using namespace inc;
-
+        // CAT WARNING：
+        // 以下千层饼结构仅用于演示指针指针在作用域内外的行为，实际代码编写请避免以下风格
+        counter step = 0;
         {
-            xhint("step 1:", used_bytes());
-            shared<uxx> c(init_now);
+            xhint(step, used_bytes());
+            shared<uxx> c { init_by_default };
             {
-                xhint("step 2:", used_bytes());
-                shared<ax> x(init_now);
+                xhint(step, used_bytes());
+                shared<ax> x { init_by_default };
                 {
-                    xhint("step 3:", used_bytes());
-                    shared<ax> a(init_now);
-                    xhint("step 4:", used_bytes());
-                    shared<bx> b(init_now);
-                    xhint("step 5:", used_bytes());
+                    xhint(step, used_bytes());
+                    shared<ax> a{ init_by_default };
+                    xhint(step, used_bytes());
+                    shared<bx> b{ init_by_default };
+                    xhint(step, used_bytes());
                     x->a = a;
                     x->b = b;
                     x->c = c;
@@ -79,21 +80,35 @@ namespace mixc::talk_about_gc::inc{
                     a->c = c;
                     b->a = x;
                     b->b = b;
-                    xhint("step 6:", used_bytes());
+                    xhint(step, used_bytes());
                 }
-                xhint("step 7:", used_bytes());
+                xhint(step, used_bytes());
             }
-            xhint("step 8:", used_bytes());
+            xhint(step, used_bytes());
         }
-        xhint("step 9:", used_bytes());
+        xhint(step, used_bytes());
 
         {
-            shared<N1_t> n1(init_now);{ n1->name = "n1";
-                shared<N2_t> n2_1(init_now);{ n2_1->name = "n2_1";
-                    shared<N2_t> n2_2(init_now);{ n2_2->name = "n2_2";
-                        shared<N2_t> n2_3(init_now);{ n2_3->name = "n2_3";
-                            shared<N2_t> n2_4(init_now);{ n2_3->name = "n2_4";
-                                shared<N3_t> n3(init_now); n3->name = "n3";
+            shared<N1_t> n1{ init_by_default };{ 
+                n1->name = "n1"; 
+                xhint(step, used_bytes());
+                shared<N2_t> n2_1{ init_by_default };{ 
+                    n2_1->name = "n2_1";
+                    xhint(step, used_bytes());
+                    shared<N2_t> n2_2{ init_by_default };{ 
+                        n2_2->name = "n2_2";
+                        xhint(step, used_bytes());
+                        shared<N2_t> n2_3{ init_by_default };{
+                            n2_3->name = "n2_3";
+                            xhint(step, used_bytes());
+                            shared<N2_t> n2_4{ init_by_default };{
+                                n2_3->name = "n2_4";
+                                xhint(step, used_bytes());
+
+                                shared<N3_t> n3{ init_by_default }; 
+                                n3->name = "n3";
+                                xhint(step, used_bytes());
+
                                 n1->na      = n2_1;
                                 n2_1->na    = n2_2;
                                 n2_2->na    = n2_3;
@@ -102,40 +117,40 @@ namespace mixc::talk_about_gc::inc{
                                 n2_4->na    = n2_2;
                                 n2_3->nc    = n3;
                             }
-                            xhint("step:10", used_bytes());
+                            xhint(step, used_bytes());
                         }
-                        xhint("step:11", used_bytes());
+                        xhint(step, used_bytes());
                     }
-                    xhint("step:12", used_bytes());
+                    xhint(step, used_bytes());
                 }
-                xhint("step:13", used_bytes());
+                xhint(step, used_bytes());
             }
-            xhint("step:13", used_bytes());
+            xhint(step, used_bytes());
         }
-        xhint("step:14", used_bytes());
+        xhint(step, used_bytes());
 
         {
             // 其实我们更推荐这么写
             // 这样可以减少栈上环对象的个数，避免无用的析构操作
-            shared<N3_t> n3(init_now);
-            shared<N1_t> n1(init_now);
-            auto & n2_1 = n1->na(init_now);
-            auto & n2_2 = n2_1->na(init_now);
-            auto & n2_3 = n2_2->na(init_now);
-            auto & n2_4 = n2_3->na(init_now);
-            n2_1->nb = n2_3;
-            n2_3->nc = n3;
-            n2_4->na = n2_2;
-            xhint("step:15", used_bytes());
+            shared<N3_t> n3{ init_by_default };
+            shared<N1_t> n1{ init_by_default };
+            auto & n2_1 = n1->na    = { init_by_default };
+            auto & n2_2 = n2_1      = { init_by_default };
+            auto & n2_3 = n2_2      = { init_by_default };
+            auto & n2_4 = n2_3      = { init_by_default };
+            n2_1->nb                = n2_3;
+            n2_3->nc                = n3;
+            n2_4->na                = n2_2;
+            xhint(step, used_bytes());
         }
-        xhint("step:16", used_bytes());
+        xhint(step, used_bytes());
 
         {
-            shared<N4_t> n4(init_now);
-            n4->n(length{4}, n4); // 给数组每个都赋值 n4
-            xhint("step:17", used_bytes());
+            shared<N4_t> n4         = { init_by_default };
+            n4->n                   = { n4, n4, n4, n4 }; // 创建长度为 4 的数组，并将每个元素赋值为 n4
+            xhint(step, used_bytes());
         }
-        xhint("step:18", used_bytes());
+        xhint(step, used_bytes());
     }
 }
 
