663facfbd6334049c1477ead45451455ada90a1d
PowerfulCat <x4495@outlook.com>
2021-01-31 21:36:53 +0800

    禁用 xnew 警告 @ macro/xnew.hpp

--- macro/xnew.hpp
+++ macro/xnew.hpp
@@ -1,5 +1,10 @@
 #pragma once
+#include"configure.hpp"
 #include"define/base_type.hpp"
+
+#if xis_msvc_compatible
+#pragma warning(disable:4291)
+#endif
 
 inline voidp operator new(decltype(sizeof(0)), void * ptr, asciis file, uxx line){
     return ptr;
