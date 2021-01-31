d77533b61d56ca39f4adbefbeea3b1a4d0c65c97
PowerfulCat <x4495@outlook.com>
2021-01-31 11:28:20 +0800

    适应 macro/xinterface.hpp 更改，lambda 接口使用独立的实现 @ interface/can_callback.hpp

--- interface/can_callback.hpp
+++ interface/can_callback.hpp
@@ -4,21 +4,42 @@
 #undef  xuser
 #define xuser mixc::interface_can_callback::inc
 #include"macro/xinterface.hpp"
-#include"macro/xnew.hpp"
+#include"memop/signature.hpp"
 #pragma pop_macro("xuser")
 
 namespace mixc::interface_can_callback::origin{
     template<class func> struct icallback;
 
-    template<class ret, class ... args> 
-    xinterface(
-        xspec(icallback, ret(args...)),
-        xoper(ret(args...))
-    );
+    template<class ret, class ... args>
+    struct icallback<ret(args...)>{
+        template<class object>
+        requires(inc::signature<ret(args...)>::has(& object::operator()))
+        constexpr icallback(object const & this_ref){
+            struct closure{
+                static ret shell(object * this_ptr, args ... list){
+                    return this_ptr->operator()(list...);
+                }
+            };
+            
+            this_ptr    = xref this_ref;
+            this_func   = func_t(xref closure::shell);
+        }
+
+        constexpr icallback(){}
+
+        ret operator()(args ... list){
+            return this_func(this_ptr, list...);
+        }
+
+    private:
+        using func_t = ret(*)(args...);
+        voidp   this_ptr  = nullptr;
+        func_t  this_func = nullptr;
+    };
 
     template<class functor_t, class signature>
-    concept can_callback = requires(functor_t const & call, icallback<signature> * ptr){
-        xnew(ptr) icallback<signature>(call);
+    concept can_callback = requires(functor_t const & call){
+        icallback<signature>(call);
     };
 }
 
