2a3633256fbab53259ab7cc1fe582e8548ee7b5b
PowerfulCat <x4495@outlook.com>
2021-01-31 11:32:39 +0800

    适应更改 @ interface/can_compare.hpp

--- interface/can_compare.hpp
+++ interface/can_compare.hpp
@@ -2,37 +2,33 @@
 #define xpack_interface_can_compare
 #pragma push_macro("xuser")
 #undef  xuser
-#define xuser mixc::interface_can_compare
+#define xuser mixc::interface_can_compare::inc
 #include"define/base_type.hpp"
+#include"interface/can_callback.hpp"
 #include"macro/xcmp.hpp"
-#include"macro/xnew.hpp"
-#include"macro/xinterface.hpp"
 #include"memop/cmp.hpp"
 #include"mixc.hpp"
 #pragma pop_macro("xuser")
 
 namespace mixc::interface_can_compare::origin{
     template<class item_t>
-    xinterface(
-        xtmpl(icompare, item_t),
-        xoper(ixx(item_t const & left, item_t const & right))
-    );
+    using icompare = inc::icallback<
+        ixx(item_t const & left, item_t const & right)
+    >;
 
     template<class functor_t, class item_t>
-    concept can_compare = requires(functor_t const & call, icompare<item_t> * ptr){
-        xnew(ptr) icompare<item_t>(call);
-    };
+    concept can_compare = inc::can_callback<functor_t, ixx(item_t const &, item_t const &)>;
 
     template<class type>
     constexpr auto default_compare = 
         [] xcmp(type){
-            return cmp_with_operator<type>(left, right);
+            return inc::cmp_with_operator<type>(left, right);
         };
 
     template<class type>
     constexpr auto default_compare_neg = 
         [] xcmp(type){
-            return cmp_with_operator<type>(right, left);
+            return inc::cmp_with_operator<type>(right, left);
         };
 }
 #endif
