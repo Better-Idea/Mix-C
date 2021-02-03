da620d8fb4654fea10217784903956de93d7185b
PowerfulCat <x4495@outlook.com>
2021-02-03 21:31:44 +0800

    补上漏掉的实现 @ meta_seq/tpush.hpp

--- meta_seq/tpush.hpp
+++ meta_seq/tpush.hpp
@@ -3,10 +3,15 @@
 #pragma push_macro("xuser")
 #undef  xuser
 #define xuser mixc::meta_seq_tpush::inc
-#include"define/base_type.hpp"
-#include"macro/xexport.hpp"
 #include"meta_seq/tappend.hpp"
+#include"mixc.hpp"
 #pragma pop_macro("xuser")
+
+namespace mixc::meta_seq_tpush{
+    template<class tlist, class item_t>
+    using tpush = inc::tappend<tlist, item_t>;
+}
+
 #endif
 
 xexport(mixc::meta_seq_tpush::tpush)
