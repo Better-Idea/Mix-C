756d9b2f9635800460232439ba2d1fcdd90c96aa
PowerfulCat <x4495@outlook.com>
2021-02-04 23:11:04 +0800

    兼容 msvc @ instruction/count_of_set.hpp

--- instruction/count_of_set.hpp
+++ instruction/count_of_set.hpp
@@ -4,22 +4,43 @@
 #undef  xuser
 #define xuser mixc::instruction_count_of_set::inc
 #include"configure.hpp"
+#include"memop/cast.hpp"
+#include"meta/unsigned_type.hpp"
+#include"meta/is_number.hpp"
 #include"mixc.hpp"
 #pragma pop_macro("xuser")
 
 namespace mixc::instruction_count_of_set{
-    template<class type>
-    inline uxx count_of_set(type v){
-        #if xis_os64
-            return __builtin_popcountll(v);
-        #elif xis_os32
-        if constexpr (sizeof(v) == 8){
-            return __builtin_popcount(v & u32(-1)) + __builtin_popcount(u64(v) >> 32);
-        }
-        else {
-            return __builtin_popcount(v);
-        }
+    template<inc::is_number number_t>
+    inline uxx count_of_set(number_t v){
+        // 避免有符号数的符号位扩展
+        using u_t   = inc::unsigned_type<number_t>;
+        u_t uv      = inc::cast<u_t>(v);
+
+        #if xis_msvc_native
+            #if xis_os64
+                return __popcnt64(uv);
+            #elif xis_os32
+                if constexpr (sizeof(uv) == 8){
+                    return __popcnt(uv & u32(-1)) + __popcnt(uv >> 32);
+                }
+                else {
+                    return __popcnt(uv);
+                }
+            #endif
+        #else
+            #if xis_os64
+                return __builtin_popcountll(uv);
+            #elif xis_os32
+                if constexpr (sizeof(uv) == 8){
+                    return __builtin_popcount(uv & u32(-1)) + __builtin_popcount(u64(uv) >> 32);
+                }
+                else {
+                    return __builtin_popcount(uv);
+                }
+            #endif
         #endif
+
     }
 }
 
