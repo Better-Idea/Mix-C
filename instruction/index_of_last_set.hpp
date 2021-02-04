feec4909bbff09f5367c2b397adca749aa3262c1
PowerfulCat <x4495@outlook.com>
2021-02-04 23:17:49 +0800

    兼容 msvc @ instruction/index_of_last_set.hpp

--- instruction/index_of_last_set.hpp
+++ instruction/index_of_last_set.hpp
@@ -4,28 +4,60 @@
 #undef  xuser
 #define xuser mixc::instruction_index_of_last_set::inc
 #include"configure.hpp"
+#include"memop/cast.hpp"
+#include"meta/unsigned_type.hpp"
+#include"meta/is_number.hpp"
 #include"mixc.hpp"
 #pragma pop_macro("xuser")
 
 namespace mixc::instruction_index_of_last_set{
-    template<class type>
-    inline uxx index_of_last_set(type v){
-        uxx i;
-        #if xis_x86
+    template<inc::is_number number_t>
+    inline uxx index_of_last_set(number_t v){
+        // 避免有符号数的符号位扩展
+        using u_t   = inc::unsigned_type<number_t>;
+        u_t uv      = inc::cast<u_t>(v);
+
+        if (uv == 0) {
+            return not_exist;
+        }
+
+        #if xis_msvc_native
+            unsigned long i;
+
             #if xis_os64
-                i = 63 - __builtin_clzll(v);
-            #else
-                if constexpr (sizeof(v) == 8){
-                    i = u64(v) >> 32 ? 
-                        31 - __builtin_clz(u64(v) >> 32) + 32:
-                        31 - __builtin_clz(u32(v));
+                _BitScanReverse64(& i, uv);
+            #elif xis_os32
+                if constexpr (sizeof(uv) == 8) {
+                    if (u32(uv >> 32) != 0) {
+                        _BitScanReverse(& i, u32(uv >> 32));
+                        i += 32;
+                    }
+                    else{
+                        _BitScanReverse(& i, u32(uv));
+                    }
                 }
                 else {
-                    i = 31 - __builtin_clz(v);
+                    _BitScanReverse(& i, u32(uv));
+                }
+            #else
+                #error "mismatch"
+            #endif
+
+            return i;
+        #else
+            #if xis_os64
+                return 63 - __builtin_clzll(uv);
+            #else
+                if constexpr (sizeof(uv) == 8){
+                    return u64(uv) >> 32 ? 
+                        31 - __builtin_clz(u64(uv) >> 32) + 32:
+                        31 - __builtin_clz(u32(uv));
+                }
+                else {
+                    return 31 - __builtin_clz(uv);
                 }
             #endif
         #endif
-        return v == 0 ? not_exist : i;
     }
 }
 
