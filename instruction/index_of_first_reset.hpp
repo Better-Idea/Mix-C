2e74627ac44f5cb25ff3dfb7d4b75599fa0b5ea5
PowerfulCat <x4495@outlook.com>
2021-02-04 23:19:38 +0800

    顺带改一下 instruction/index_of_first_reset.hpp 和 instruction/index_of_last_reset.hpp

--- instruction/index_of_first_reset.hpp
+++ instruction/index_of_first_reset.hpp
@@ -4,13 +4,15 @@
 #undef  xuser
 #define xuser mixc::instruction_index_of_first_reset::inc
 #include"instruction/index_of_first_set.hpp"
+#include"meta/is_number.hpp"
+#include"mixc.hpp"
 #pragma pop_macro("xuser")
 
 namespace mixc::instruction_index_of_first_reset{
-    template<class type>
-    inline uxx index_of_first_reset(type v){
+    template<inc::is_number number_t>
+    inline uxx index_of_first_reset(number_t v){
         // ~v 可能会由 unsigned char 之类的类型变为 int 类型，所以需要显式指定模板参数
-        return inc::index_of_first_set<type>(~v);
+        return inc::index_of_first_set<number_t>(~v);
     }
 }
 
--- instruction/index_of_last_reset.hpp
+++ instruction/index_of_last_reset.hpp
@@ -3,14 +3,16 @@
 #pragma push_macro("xuser")
 #undef  xuser
 #define xuser mixc::instruction_index_of_last_reset::inc
-#include"instruction/index_of_last_set.hpp
+#include"instruction/index_of_first_set.hpp"
+#include"meta/is_number.hpp"
+#include"mixc.hpp"
 #pragma pop_macro("xuser")
 
 namespace mixc::instruction_index_of_last_reset{
-    template<class type>
-    inline uxx index_of_last_reset(type v){
+    template<inc::is_number number_t>
+    inline uxx index_of_last_reset(number_t v){
         // ~v 可能会由 unsigned char 之类的类型变为 int 类型，所以需要显式指定模板参数
-        return inc::index_of_last_set<type>(~v);
+        return inc::index_of_last_set<number_t>(~v);
     }
 }
 
