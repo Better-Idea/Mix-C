8fcaf03f6976cc3b3ec477bc19cdf392c4c8a338
PowerfulCat <x4495@outlook.com>
2021-02-08 22:20:46 +0800

    utils/bit_indicator.hpp -> utils/bits_indicator.hpp

--- utils/bits_indicator.hpp
+++ utils/bits_indicator.hpp
@@ -1,12 +1,12 @@
 #ifdef xuser
 #ifndef xusing_utils_bit_indicator
-#include"utils/private/bit_indicator.hpp"
+#include"utils/private/bits_indicator.hpp"
 #endif
 #include"macro/xfinal.hpp"
 
 namespace xuser{
     template<uxx bits = 0>
-    xfinal_tmpl(xusing_utils_bit_indicator, bit_indicator, bits);
+    xfinal_tmpl(xusing_utils_bit_indicator, bits_indicator, bits);
 }
 
 #undef xusing_utils_bit_indicator
