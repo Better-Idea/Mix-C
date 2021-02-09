a0a22f17b2959c519ce543985ee3f6ce76fbf9a8
PowerfulCat <x4495@outlook.com>
2021-02-09 22:35:37 +0800

    纠正符号 @ lock/atom_xor.hpp

--- lock/atom_xor.hpp
+++ lock/atom_xor.hpp
@@ -1,17 +1,16 @@
-#ifndef xpack_lock_atom_or
-#define xpack_lock_atom_or
+#ifndef xpack_lock_atom_xor
+#define xpack_lock_atom_xor
 #pragma push_macro("xuser")
 #undef  xuser
-#define xuser mixc::lock_atom_or::inc
+#define xuser mixc::lock_atom_xor::inc
 #include"mixc.hpp"
 #pragma pop_macro("xuser")
 
-namespace mixc::lock_atom_or{
-    extern u64 atom_xor(voidp a, u64 b, uxx bytes);
-
+namespace mixc::lock_atom_xor{
     template<class a>
     inline a atom_xor(a * left, a right){
         #if xis_msvc_compatible
+            extern u64 atom_xor(voidp a, u64 b, uxx bytes);
             return (a)atom_xor(left, u64(right), sizeof(a));
         #else
             return __atomic_xor_fetch(left, right, 5);
@@ -21,4 +20,4 @@ namespace mixc::lock_atom_or{
 
 #endif
 
-xexport(mixc::lock_atom_or::atom_xor)
+xexport(mixc::lock_atom_xor::atom_xor)
