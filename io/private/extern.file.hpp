5f3d234c0f03b812694f0cd53982f37322561029
PowerfulCat <x4495@outlook.com>
2021-02-09 20:25:38 +0800

    应用 io/private/cmd.hpp 并适应 lang/cxx/ 更改 @ io/private/extern.file.hpp

--- io/private/extern.file.hpp
+++ io/private/extern.file.hpp
@@ -32,6 +32,7 @@ namespace mixc::io_file::origin{
 #include<sys/file.h>
 #include<sys/stat.h>
 #include<unistd.h>
+#include"cmd.hpp"
 #endif
 
 namespace mixc::io_file::origin{
@@ -55,7 +56,7 @@ namespace mixc::io_file::origin{
         #endif
     }
 
-    file & file::open(inc::c08 path, access_mode_t mode, bstate_t * result) const {
+    file::final & file::open(inc::c08 path, access_mode_t mode, bstate_t * result) const {
         auto type = 0;
         auto i    = cpp::c08{path}.index_of_last({'/','\\'});
 
@@ -76,10 +77,24 @@ namespace mixc::io_file::origin{
         case access_mode_t::read_write: type = GENERIC_READ | GENERIC_WRITE;    break;
         }
 
-        the.fd      = (ixx)::CreateFileA(path, type, FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE, NULL, CREATE_NEW, 0, NULL);
+        the.fd      = (ixx)::CreateFileA(
+            asciis(path), 
+            type, 
+            FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE, NULL, CREATE_NEW, 
+            0, 
+            NULL
+        );
 
         if (the.fd == -1){
-            the.fd  = (ixx)::CreateFileA(path, type, FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE, NULL, OPEN_EXISTING, 0, NULL);
+            the.fd  = (ixx)::CreateFileA(
+                asciis(path), 
+                type, 
+                FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE, 
+                NULL, 
+                OPEN_EXISTING, 
+                0, 
+                NULL
+            );
         }
 
         #elif xis_linux
@@ -97,29 +112,31 @@ namespace mixc::io_file::origin{
         #endif
 
         if (result != nullptr){
-            result[0] = the.fd <= 0 ? bstate_t::fail : bstate_t::success;
+            result[0] = the.fd < 0 ? bstate_t::fail : bstate_t::success;
         }
         return thex;
     }
 
-    file & file::close() const {
-        if (cpp::atom_swap<ixx>(& fd, 0) <= 0){
-            #if xis_windows
-            ::CloseHandle(HANDLE(fd));
-            #elif xis_linux
-            ::close(fd);
-            #else
-            #error "os miss match"
-            #endif
+    file::final & file::close() const {
+        if (cpp::atom_swap<ixx>(& fd, -1) < 0){
+            return thex;
         }
+
+        #if xis_windows
+        ::CloseHandle(HANDLE(fd));
+        #elif xis_linux
+        ::close(fd);
+        #else
+        #error "os miss match"
+        #endif
         return thex;
     }
 
-    file & file::forward(u64 offset) const {
+    file::final & file::forward(u64 offset) const {
         return backward(u64(0) - offset);
     }
 
-    file & file::backward(u64 offset) const {
+    file::final & file::backward(u64 offset) const {
         #if xis_windows
         ::SetFilePointerEx(HANDLE(the.fd), *PLARGE_INTEGER(& offset), NULL, FILE_CURRENT);
         #elif xis_linux
@@ -130,7 +147,7 @@ namespace mixc::io_file::origin{
         return thex;
     }
 
-    file & file::seek(i64 offset) const {
+    file::final & file::seek(i64 offset) const {
         #if xis_windows
 
         if (offset < 0){
@@ -142,7 +159,22 @@ namespace mixc::io_file::origin{
         }
 
         #elif xis_linux
-        ::lseek(the.fd, offset, offset > 0 ? SEEK_SET : SEEK_END);
+        ::lseek(int(the.fd), offset, offset > 0 ? SEEK_SET : SEEK_END);
+        #else
+        #error "os miss match"
+        #endif
+        return thex;
+    }
+
+    file::final & file::flush() const {
+        if (the.fd < 0){
+            return thex;
+        }
+
+        #if xis_windows
+        ::FlushFileBuffers(HANDLE(the.fd));
+        #elif xis_linux
+        ::fsync(int(the.fd));
         #else
         #error "os miss match"
         #endif
@@ -150,14 +182,13 @@ namespace mixc::io_file::origin{
     }
 
     uxx file::read(voidp buffer, uxx bytes) const {
-        if (the.fd <= 0){
+        if (the.fd < 0){
             the.open(the.path, access_mode_t::read_only);
         }
 
         u64 size = 0;
 
         #if xis_windows
-
         (void)::ReadFile(HANDLE(the.fd), buffer, DWORD(bytes), LPDWORD(& size), NULL);
         #elif xis_linux
         size = (uxx)::read(the.fd, buffer, bytes);
@@ -172,7 +203,7 @@ namespace mixc::io_file::origin{
     }
 
     uxx file::write(void const * buffer, uxx bytes) const {
-        if (the.fd <= 0){
+        if (the.fd < 0){
             the.open(the.path, access_mode_t::write_only);
         }
 
@@ -181,7 +212,7 @@ namespace mixc::io_file::origin{
         #if xis_windows
         ::WriteFile(HANDLE(the.fd), buffer, DWORD(bytes), LPDWORD(& size), NULL);
         #elif xis_linux
-        size = (uxx)::write(the.fd, buffer, bytes);
+        size = (uxx)::write(int(the.fd), buffer, bytes);
         #else
         #error "os miss match"
         #endif
@@ -197,9 +228,9 @@ namespace mixc::io_file::origin{
         auto    source  = buf.alloc(the.path);
 
         #if xis_windows
-        ::DeleteFileA(source);
+        ::DeleteFileA(asciis(source));
         #elif xis_linux
-        ::remove(source);
+        ::remove(asciis(source));
         #else
         #error "os miss match"
         #endif
@@ -212,7 +243,7 @@ namespace mixc::io_file::origin{
         auto    source  = buf.alloc(the.path);
         auto    target  = buf.alloc(new_path);
         #if xis_windows
-        ::MoveFileA(source, target);
+        ::MoveFileA(asciis(source), asciis(target));
         #elif xis_linux
         ::rename(source, target);
         #else
@@ -222,20 +253,34 @@ namespace mixc::io_file::origin{
         buf.free(source, the.path);
     }
 
+    void file::move_to_forcedly(inc::c08 new_path) const {
+        auto && buf     = cpp::path_buffer{};
+        auto    source  = buf.alloc(the.path);
+        auto    target  = buf.alloc(new_path);
+        #if xis_windows
+        ::MoveFileExA(asciis(source), asciis(target), 
+            MOVEFILE_COPY_ALLOWED |     // 允许跨分区移动，避免移动失败后丢失
+            MOVEFILE_REPLACE_EXISTING   // 允许覆盖
+        );
+        #elif xis_linux
+        cmd("/bin/mv", "mv", source, target);
+        #else
+        #error "os miss match"
+        #endif
+
+        buf.free(target, new_path);
+        buf.free(source, the.path);
+    }
+
     void file::copy_to(inc::c08 new_path) const{
         auto && buf     = cpp::path_buffer{};
-        auto    source  = (asciis)buf.alloc(the.path);
-        auto    target  = (asciis)buf.alloc(new_path);
+        auto    source  = buf.alloc(the.path);
+        auto    target  = buf.alloc(new_path);
 
         #if xis_windows
-        ::CopyFileA(source, target, true/*override*/);
+        ::CopyFileA(asciis(source), asciis(target), true/*override*/);
         #elif xis_linux
-
-        if (vfork() == 0){ // this thread will wait child finished
-            execl("/bin/cp", "cp", source, target, NULL);
-            _exit(-1);
-        }
-
+        cmd("/bin/cp", "cp", source, target);
         #else
         #error "os miss match"
         #endif
@@ -250,14 +295,14 @@ namespace mixc::io_file::origin{
 
         #if xis_windows
 
-        auto    word    = GetFileAttributesA(the.path);
+        auto    word    = GetFileAttributesA(asciis(the.path));
         return  word != INVALID_FILE_ATTRIBUTES and 0 == (word & FILE_ATTRIBUTE_DIRECTORY);
 
         #elif xis_linux
 
         typedef struct stat meta_t;
         auto && meta    = meta_t{};
-        auto    exist   = stat(source, & meta) != -1 and (meta.st_mode & S_IFDIR) != 0;
+        auto    exist   = stat(asciis(source), & meta) != -1 and (meta.st_mode & S_IFDIR) != 0;
         return  exist;
 
         #else
