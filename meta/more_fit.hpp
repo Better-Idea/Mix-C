c4e1b9c2be430356604fc1f630675eeb25a15aa9
PowerfulCat <x4495@outlook.com>
2021-02-11 00:29:47 +0800

    改善适配准确度 @ meta/more_fit.hpp

--- meta/more_fit.hpp
+++ meta/more_fit.hpp
@@ -10,6 +10,8 @@
 #include"meta/has_operator_cast.hpp"
 #include"meta/is_based_on.hpp"
 #include"meta/is_same.hpp"
+#include"meta/is_signed.hpp"
+#include"meta/is_unsigned.hpp"
 #include"meta/remove_ref.hpp"
 #pragma pop_macro("xuser")
 
@@ -21,15 +23,17 @@ namespace mixc::meta_more_fit{
         ixx   size_different        = min_value_of<ixx>,
         bool  full_match_v          = false,
         bool  has_operator_cast_v   = false,
-        bool  has_implicit_cast_v   = false
+        bool  has_implicit_cast_v   = false,
+        bool  is_narrow_or_widen_v  = false
     >
     struct record{
-        using type                              = the_target;
-        static constexpr uxx  index             = index_value;
-        static constexpr ixx  different         = size_different;
-        static constexpr bool full_match        = full_match_v;
-        static constexpr bool has_operator_cast = has_operator_cast_v;
-        static constexpr bool has_implicit_cast = has_implicit_cast_v;
+        using type                                  = the_target;
+        static constexpr uxx  index                 = index_value;
+        static constexpr ixx  different             = size_different;
+        static constexpr u08  full_match            = full_match_v;
+        static constexpr u08  has_operator_cast     = has_operator_cast_v;
+        static constexpr u08  has_implicit_cast     = has_implicit_cast_v;
+        static constexpr u08  is_narrow_or_widen    = is_narrow_or_widen_v;
     };
 
     template<uxx index, class source, class ... target> struct meta;
@@ -43,11 +47,19 @@ namespace mixc::meta_more_fit{
     template<uxx index, class source, class first, class ... target>
     struct meta<index, source, first, target...> {
         static auto invoke(){
-            constexpr ixx diff = ixx(sizeof(first) - sizeof(source));
-
             using next = decltype(
                 meta<index + 1, source, target...>::invoke()
             );
+
+            constexpr u08 is_narrow_or_widen    = 
+                (inc::is_signed<source>   and inc::is_signed<first>) or
+                (inc::is_unsigned<source> and inc::is_unsigned<first>);
+            constexpr ixx  diff                 = ixx(sizeof(first) - sizeof(source));
+            constexpr bool cond0 = (next::has_implicit_cast);
+            constexpr bool cond1 = (next::different >= 0 and (diff < 0 or diff > next::different));
+            constexpr bool cond2 = (next::different <  0 and  diff < next::different);
+            constexpr bool cond3 = (next::different == diff and next::is_narrow_or_widen > is_narrow_or_widen);
+            constexpr bool cond4 = (cond1 or cond2 or cond3);
 
             if constexpr (is_same<first, source>){
                 return record<first, index, 0, true>();
@@ -62,19 +74,11 @@ namespace mixc::meta_more_fit{
                 is_based_on<first, source>){
                 return record<first, index, 0, false, true>();
             }
-            else if constexpr (has_cast<first, source>){
-                if constexpr 
-                    (next::has_implicit_cast and (
-                    (next::different >= 0 and (diff <= 0 or diff >= next::different)) or
-                    (next::different < 0 and diff <= next::different))) {
-                    return next();
-                }
-                else{
-                    return record<first, index, diff, false, false, true>();
-                }
+            else if constexpr (not has_cast<first, source> or (cond0 and cond4)) {
+                return next();
             }
             else{
-                return next();
+                return record<first, index, diff, false, false, true, is_narrow_or_widen>();
             }
         }
     };
