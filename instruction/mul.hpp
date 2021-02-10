2ebff9dc999ac109fe4a45b5ca7a46317e49c3f4
PowerfulCat <x4495@outlook.com>
2021-02-10 13:35:51 +0800

    调整 @ instruction/mul.hpp

--- instruction/mul.hpp
+++ instruction/mul.hpp
@@ -8,17 +8,21 @@
 #pragma pop_macro("xuser")
 
 namespace mixc::instruction_mul{
+    #if xis_msvc_native
+    extern u128 mul(u64 a, u64 b);
+    #else
     inline u128 mul(u64 a, u64 b){
         #if xis_x86
             #if xis_os64
-                u128 r{};
-                asm(""::"a"(a),"d"(0), "c"(b));
-                asm("mul %rcx");
-                asm("":"=a"(r.low),"=d"(r.high));
-                return r;
+            u128 r{};
+            asm(""::"a"(a),"d"(0), "c"(b));
+            asm("mul %rcx");
+            asm("":"=a"(r.low),"=d"(r.high));
+            return r;
             #endif
         #endif
     }
+    #endif
 }
 
 #endif
