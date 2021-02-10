20c338b90f9d9edefd1cd17a96379ace15f0bf02
PowerfulCat <x4495@outlook.com>
2021-02-10 13:37:25 +0800

    使用 xis_msvc_native @ macro/xalign.hpp

--- macro/xalign.hpp
+++ macro/xalign.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include"configure.hpp"
 
-#if xis_msvc_compatible
+#if xis_msvc_native
     #define xalign(...)     __declspec(align(__VA_ARGS__))
 #else
     #define xalign(...)     __attribute__((aligned(__VA_ARGS__)))
