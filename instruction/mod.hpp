bc962fe0573b9f61d0abea97efa5fd6bbd6090e9
PowerfulCat <x4495@outlook.com>
2021-02-10 13:35:17 +0800

    调整 @ instruction/mod.hpp

--- instruction/mod.hpp
+++ instruction/mod.hpp
@@ -9,22 +9,23 @@
 
 namespace mixc::instruction_mod{
     extern f64 mod(f64 a, f64 b);
+
+    #if xis_msvc_native
+    extern u64 mod(u128 a, u64 b);
+    #else
     inline u64 mod(u128 a, u64 b){
         #if xis_x86
             #if xis_os64
-                #if xis_msvc_native
-                extern u64 mod_core(u128 a, u64 b);
-                return mod_core(a, b);
-                #else
-                u64 r = 0;
-                asm(""::"a"(a.low),"d"(a.high),"c"(b));
-                asm("div %rcx");
-                asm("":"=d"(r));
-                return r;
-                #endif
+            u64 r = 0;
+            asm(""::"a"(a.low),"d"(a.high),"c"(b));
+            asm("div %rcx");
+            asm("":"=d"(r));
+            return r;
             #endif
         #endif
     }
+    #endif
+
 }
 
 #endif
