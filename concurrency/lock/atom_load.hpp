7cbaa289786f24b184e7d55814b20336cdf6c6e1
PowerfulCat <x4495@outlook.com>
2021-02-16 18:32:57 +0000

    转换成数值类型 @ concurrency/lock/atom_load.hpp

--- concurrency/lock/atom_load.hpp
+++ concurrency/lock/atom_load.hpp
@@ -4,13 +4,17 @@
 #undef  xuser
 #define xuser mixc::concurrency_lock_atom_load::inc
 #include"concurrency/lock/atom_fetch_or.hpp"
+#include"meta/unsigned_type.hpp"
+#include"memop/cast.hpp"
 #include"mixc.hpp"
 #pragma pop_macro("xuser")
 
 namespace mixc::concurrency_lock_atom_load{
     template<class a>
     inline a atom_load(a * left){
-        return inc::atom_fetch_or<a>(left, 0);
+        using u_t = inc::unsigned_type<a>;
+        u_t v = inc::atom_fetch_or<u_t>((u_t *)left, 0);
+        return inc::cast<a>(v);
     }
 }
 
