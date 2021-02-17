1f21b161292711781a012470adc375a8da3fdac7
PowerfulCat <x4495@outlook.com>
2021-02-17 22:27:20 +0800

    支持 windows @ concurrency/private/extern.thread_self.hpp

--- concurrency/private/extern.thread_self.hpp
+++ concurrency/private/extern.thread_self.hpp
@@ -4,25 +4,43 @@
 
 #define xuser mixc::concurrency_thread_self::inc
 #include"concurrency/thread_self.hpp"
+#include"configure.hpp"
+
+#if xis_windows
+#include<windows.h>
+#elif xis_linux
 #include<pthread.h>
 #include<time.h>
 #include<unistd.h>
+#endif
 
 namespace mixc::concurrency_thread_self::origin{
     void thread_self::sleep(uxx millisecond){
-        while(true){
-            if (millisecond > 1000){
-                usleep(1000 * 1000);
-                millisecond -= 1000;
+        #ifdef xis_windows
+            Sleep(millisecond);
+        #elif 
+            while(true){
+                if (millisecond > 1000){
+                    usleep(1000 * 1000);
+                    millisecond -= 1000;
+                }
+                else{
+                    usleep(millisecond * 1000);
+                    break;
+                }
             }
-            else{
-                usleep(millisecond * 1000);
-                break;
-            }
-        }
+        #else
+            #error""
+        #endif
     }
 
     void thread_self::yield(){
-        pthread_yield();
+        #if xis_windows
+            Sleep(0);
+        #elif xis_linux
+            pthread_yield();
+        #else
+            #error""
+        #endif
     }
 }
