1af5f0828b53ba5d3c7454eb8b98dc8495990f88
PowerfulCat <x4495@outlook.com>
2021-02-18 08:26:54 +0000

    实现 u128 mod u64 @ instruction/mod.hpp

--- instruction/mod.hpp
+++ instruction/mod.hpp
@@ -4,6 +4,8 @@
 #undef  xuser
 #define xuser mixc::instruction_mod::inc
 #include"configure.hpp"
+#include"instruction/index_of_last_set.hpp"
+#include"math/min.hpp"
 #include"mixc.hpp"
 #pragma pop_macro("xuser")
 
@@ -16,12 +18,51 @@ namespace mixc::instruction_mod{
     inline u64 mod(u128 a, u64 b){
         #if xis_x86
             #if xis_os64
-            u64 r = 0;
-            asm(""::"a"(a.low),"d"(a.high),"c"(b));
-            asm("div %rcx");
-            asm("":"=d"(r));
-            return r;
+                #define xhas_invoked
+                u64 r = 0;
+                asm(""::"a"(a.low),"d"(a.high),"c"(b));
+                asm("div %rcx");
+                asm("":"=d"(r));
+                return r;
             #endif
+        #endif
+
+        #ifdef xhas_invoked
+            #undef xhas_invoked
+        #else
+            if (a.high == 0){
+                return a.low % b;
+            }
+
+            auto shl = [](u128 a, uxx shift){
+                u64 shr_low     = (shift == 0 ? 0/*未用到*/ : a.low >> (64 - shift));
+                u64 shl_low     = (shift == 0 ? a.low : a.low << (shift));
+                a.high          = (a.high << shift) | shr_low;
+                a.low           = (shl_low);
+                return a;
+            };
+
+            for(uxx rest_bits = 64;;){
+                uxx i           = inc::index_of_last_set(a.high); 
+                i               = 63 - i;
+                i               = inc::min(i, rest_bits);
+                rest_bits      -= i;
+                a               = shl(a, i);
+
+                if (rest_bits == 0){
+                    break;
+                }
+                if (a.high < b){
+                    a           = shl(a, 1);
+                    rest_bits  -= 1;
+                    a.high     -= b;
+                }
+                else{
+                    a.high     %= b;
+                }
+            }
+            auto r              = a.high % b;
+            return r;
         #endif
     }
     #endif
