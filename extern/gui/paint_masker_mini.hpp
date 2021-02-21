7636e2e07f31c5d52a237c844c2b46b8aa4fedcc
PowerfulCat <x4495@outlook.com>
2021-02-21 15:30:40 +0800

    符号对齐 @ extern/gui/paint_masker_mini.hpp

--- extern/gui/paint_masker_mini.hpp
+++ extern/gui/paint_masker_mini.hpp
@@ -23,32 +23,33 @@ namespace mixc::extern_gui_paint_masker_mini::origin {
     )
     private:
         enum{
-            pixel_per_row                   = height_v / rows_v,
-            pixel_per_column                = width_v / columns_v
+            pixel_per_row       = height_v / rows_v,
+            pixel_per_column    = width_v / columns_v
         };
 
-        using final_t                       = the_t;
-        using idc_t                         = inc::bits_indicator<pixel_per_row * pixel_per_column>;
+        using final_t           = the_t;
+        using idc_t             = inc::bits_indicator<pixel_per_row * pixel_per_column>;
 
         idc_t   idc;
         u16     pixel_count[rows_v * columns_v] = {};
-        u16     i                           = rows_v * columns_v - 1;
+        u16     i               = rows_v * columns_v - 1;
     public:
         void analysis(uxx x, uxx y){
-            x                              /= pixel_per_column;
-            y                              /= pixel_per_row;
-            pixel_count[x * columns_v + y]   += 1;
+            x                  /= pixel_per_column;
+            y                  /= pixel_per_row;
+            pixel_count[x * columns_v + y]
+                               += 1;
         }
 
         try_paint_result_t try_paint(uxx x, uxx y){
             uxx index;
             uxx r;
             uxx c;
-            c                               = x % pixel_per_column;
-            x                               = x / pixel_per_column;
-            r                               = y % pixel_per_row;
-            y                               = y / pixel_per_row;
-            index                           = x * columns_v + y;
+            c                   = x % pixel_per_column;
+            x                   = x / pixel_per_column;
+            r                   = y % pixel_per_row;
+            y                   = y / pixel_per_row;
+            index               = x * columns_v + y;
 
             while (true){
                 if (i == not_exist){
@@ -66,7 +67,7 @@ namespace mixc::extern_gui_paint_masker_mini::origin {
                 return try_paint_result_t::cannot_draw;
             }
             else{
-                pixel_count[i]             -= 1;
+                pixel_count[i] -= 1;
                 idc.set(idx);
                 return try_paint_result_t::can_draw;
             }
@@ -77,15 +78,15 @@ namespace mixc::extern_gui_paint_masker_mini::origin {
             uxx r;
             uxx c;
             uxx offset;
-            uxx start       = 0;
-            uxx end         = idc.pop_first();
+            uxx start           = 0;
+            uxx end             = idc.pop_first();
 
             for(; end != not_exist; start  += 1, end = idc.pop_first()){
                 if (end == start){
                     continue;
                 }
                 if (end == not_exist){
-                    end     = pixel_per_row * pixel_per_column;
+                    end         = pixel_per_row * pixel_per_column;
                     clean(co{start, end});
                     break;
                 }
@@ -104,12 +105,12 @@ namespace mixc::extern_gui_paint_masker_mini::origin {
         }
 
         xpubgetx(block_begin_x, uxx){
-            uxx offset_x    = i % columns_v * pixel_per_column;
+            uxx offset_x        = i % columns_v * pixel_per_column;
             return offset_x;
         }
 
         xpubgetx(block_begin_y, uxx){
-            uxx offset_y    = i / columns_v * pixel_per_row;
+            uxx offset_y        = i / columns_v * pixel_per_row;
             return offset_y;
         }
 
