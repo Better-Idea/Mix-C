bea048aa2d62bed69e4fd2372b3e1a1bc2db6835
PowerfulCat <x4495@outlook.com>
2021-02-21 20:21:02 +0800

    支持附加命名空间 @ macro/xexport.hpp

--- macro/xexport.hpp
+++ macro/xexport.hpp
@@ -1,3 +1,5 @@
 #pragma once
-#define xexport_space(...)      namespace xuser{ using namespace ::__VA_ARGS__; }
-#define xexport(...)            namespace xuser{ using ::__VA_ARGS__; }
+#define xexport_space(...)              namespace xuser{ using namespace ::__VA_ARGS__; }
+#define xexport_spacex(addition,...)    namespace xuser::addition{ using namespace ::__VA_ARGS__; }
+#define xexport(...)                    namespace xuser{ using ::__VA_ARGS__; }
+#define xexportx(addition,...)          namespace xuser::addition{ using ::__VA_ARGS__; }
