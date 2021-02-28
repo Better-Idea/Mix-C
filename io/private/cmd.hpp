313a4e1b5ae21835013db828a138c6426c4ae203
PowerfulCat <x4495@outlook.com>
2021-02-28 18:46:21 +0800

    应用模板命名规范 @ io/private/cmd.hpp

--- io/private/cmd.hpp
+++ io/private/cmd.hpp
@@ -2,8 +2,8 @@
 #include<unistd.h>
 #include"define/base_type.hpp"
 
-template<class ... args>
-inline void cmd(asciis exe_path, asciis exe, args const & ... list){
+template<class ... args_t>
+inline void cmd(asciis exe_path, asciis exe, args_t const & ... list){
     if (vfork() == 0){ // this thread will wait child finished
         execl(exe_path, exe, (asciis)list..., NULL);
         _exit(-1);
