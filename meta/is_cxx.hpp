8947baebfcd9c52d0e1002a8f70feeb86b44079f
PowerfulCat <x4495@outlook.com>
2021-03-05 23:47:13 +0800

    添加 meta/is_cxx.hpp

---
+++ meta/is_cxx.hpp
@@ -0,0 +1,17 @@
+#ifndef xpack_meta_is_cxx
+#define xpack_meta_is_cxx
+#pragma push_macro("xuser")
+#undef  xuser
+#define xuser mixc::meta_is_cxx::inc
+#include"define/base_type.hpp"
+#include"macro/xexport.hpp"
+#pragma pop_macro("xuser")
+
+namespace mixc::meta_is_cxx::origin{
+    template<class may_cxx_t>
+    concept is_cxx = sizeof(typename may_cxx_t::im_cxx) != 0;
+}
+
+#endif
+
+xexport_space(mixc::meta_is_cxx::origin)
