c279d5a0319b5ed548a9c1d52d193d71f3c26b19
PowerfulCat <x4495@outlook.com>
2021-03-07 16:26:48 +0800

    适应更改 @ test/algo/mmu.hpp

--- test/algo/mmu.hpp
+++ test/algo/mmu.hpp
@@ -21,76 +21,76 @@ namespace mixc::test_algo_mmu{
 
     xtest("mmu_var_array_1") {
         #define xa_initial_length   1
-        #include"test/algo/template/mmu.hpp"
+        #include"test/algo/private/xgen.mmu.hpp"
     };
 
     xtest("mmu_var_array_2") {
         #define xa_initial_length   2
-        #include"test/algo/template/mmu.hpp"
+        #include"test/algo/private/xgen.mmu.hpp"
     };
 
     xtest("mmu_var_array_4") {
         #define xa_initial_length   4
-        #include"test/algo/template/mmu.hpp"
+        #include"test/algo/private/xgen.mmu.hpp"
     };
 
     xtest("mmu_var_array_8") {
         #define xa_initial_length   8
-        #include"test/algo/template/mmu.hpp"
+        #include"test/algo/private/xgen.mmu.hpp"
     };
 
     xtest("mmu_var_array_16") {
         #define xa_initial_length   16
-        #include"test/algo/template/mmu.hpp"
+        #include"test/algo/private/xgen.mmu.hpp"
     };
 
     xtest("mmu_var_array_32") {
         #define xa_initial_length   32
-        #include"test/algo/template/mmu.hpp"
+        #include"test/algo/private/xgen.mmu.hpp"
     };
 
     xtest("mmu_var_array_64") {
         #define xa_initial_length   64
-        #include"test/algo/template/mmu.hpp"
+        #include"test/algo/private/xgen.mmu.hpp"
     };
 
     xtest("mmu_var_array_1s") {
         #define xa_initial_length   1
         #define xa_use_stack_table
-        #include"test/algo/template/mmu.hpp"
+        #include"test/algo/private/xgen.mmu.hpp"
     };
 
     xtest("mmu_var_array_2s") {
         #define xa_initial_length   2
         #define xa_use_stack_table
-        #include"test/algo/template/mmu.hpp"
+        #include"test/algo/private/xgen.mmu.hpp"
     };
 
     xtest("mmu_var_array_4s") {
         #define xa_initial_length   4
         #define xa_use_stack_table
-        #include"test/algo/template/mmu.hpp"
+        #include"test/algo/private/xgen.mmu.hpp"
     };
 
     xtest("mmu_var_array_8s") {
         #define xa_initial_length   8
         #define xa_use_stack_table
-        #include"test/algo/template/mmu.hpp"
+        #include"test/algo/private/xgen.mmu.hpp"
     };
 
     xtest("mmu_var_array_16s") {
         #define xa_initial_length 16
-        #include"test/algo/template/mmu.hpp"
+        #include"test/algo/private/xgen.mmu.hpp"
     };
 
     xtest("mmu_var_array_32s") {
         #define xa_initial_length 32
-        #include"test/algo/template/mmu.hpp"
+        #include"test/algo/private/xgen.mmu.hpp"
     };
 
     xtest("mmu_var_array_64s") {
         #define xa_initial_length 64
-        #include"test/algo/template/mmu.hpp"
+        #include"test/algo/private/xgen.mmu.hpp"
     };
 }
 
