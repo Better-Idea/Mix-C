ca6e86130014292818066fdb70b6b8cfb2fee511
PowerfulCat <x4495@outlook.com>
2021-03-07 13:13:03 +0800

    接手 lang/cxx/index_of_last 关于循环查找的部分职责 @ lang/cxx/find_reserve.hpp

---
+++ lang/cxx/find_reserve.hpp
@@ -0,0 +1,34 @@
+#ifndef xusing_lang_cxx
+#include"lang/private/cxx.hpp"
+#endif
+
+#ifndef xpack_lang_cxx_find_reserve
+#define xpack_lang_cxx_find_reserve
+#pragma push_macro("xuser")
+#pragma push_macro("xusing_lang_cxx")
+#undef  xusing_lang_cxx
+#undef  xuser
+#define xuser mixc::lang_cxx_find_reserve::inc
+#include"interface/can_compare.hpp"
+#include"lang/cxx/private/find_mix.hpp"
+#include"lang/cxx/private/find_reserve.hpp"
+#include"macro/xitr_foreach.hpp"
+#include"mixc.hpp"
+#pragma pop_macro("xusing_lang_cxx")
+#pragma pop_macro("xuser")
+
+namespace mixc::lang_cxx_find_reserve{
+    #define xa_name             find_reserve
+    #define xa_find_mix_type    1/*从后往前找*/
+    #include"lang/cxx/private/xgen.find.hpp"
+}
+
+#endif
+
+namespace mixc::lang_cxx_find_reserve::xuser {
+    template<class final_t, class item_t>
+    using cxx = meta<final_t, xusing_lang_cxx::cxx<final_t, item_t>, item_t>;
+}
+
+#undef  xusing_lang_cxx
+#define xusing_lang_cxx ::mixc::lang_cxx_find_reserve::xuser
