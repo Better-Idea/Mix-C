e9ca9c2140476015fb1790b29eec357c6b5f14df
PowerfulCat <x4495@outlook.com>
2021-03-07 16:24:25 +0800

    新增静态页表模式的测试 @ test/algo/template/mmu.hpp

--- test/algo/template/mmu.hpp
+++ test/algo/template/mmu.hpp
@@ -1,15 +1,26 @@
-#ifdef xinitial_length
+#ifdef xa_initial_length
 using namespace inc;
 
 auto alloc = [](uxx bytes){
+    xhint(used_bytes());
     return inc::alloc<void>(inc::memory_size{bytes});
 };
 
 auto free = [](voidp ptr, uxx bytes){
+    xhint(used_bytes());
     inc::free(ptr, inc::memory_size{bytes});
 };
 
+#ifdef xa_use_stack_table
+#undef xa_use_stack_table
+using var = var_array<xa_initial_length, with_fixed_page_table>;
+item *  stack_table[32];
+item ** table   = stack_table;
+#else
+using var       = var_array<xa_initial_length>;
 item ** table   = nullptr;
+#endif
+
 uxx     length  = 0;
 item    meta;
 uxx     actual;
@@ -17,61 +28,61 @@ uxx     wanted;
 uxx     cost = used_bytes();
 meta.counter = 0;
 
-for(uxx i = 0; i < xinitial_length * 32; i++){
+for(uxx i = 0; i < xa_initial_length * 32; i++){
     for(uxx j = 0; j <= i; j++){
         meta.value = j;
-        var_array<xinitial_length>::push(xref table, xref length, meta, alloc, free);
+        var::push(xref table, xref length, meta, alloc, free);
         xfail_ifne(j + 1, length);
     }
     for(uxx j = 0; j <= i; j++){
-        auto & current = var_array<xinitial_length>::access(table, j);
+        auto & current = var::access(table, j);
         xfail_ifne(j, current.value);
     }
 
-    var_array<xinitial_length>::clear(xref table, xref length, free);
+    var::clear(xref table, xref length, free);
     xfail_ifne(i + 1, meta.counter);
     xfail_ifne(cost, used_bytes());
     meta.counter = 0;
 }
 
-for(uxx i = 0; i < xinitial_length * 32; i++){
+for(uxx i = 0; i < xa_initial_length * 32; i++){
     for(uxx j = 0; j <= i; j++){
         meta.value = j;
-        var_array<xinitial_length>::push(xref table, xref length, meta, alloc, free);
+        var::push(xref table, xref length, meta, alloc, free);
     }
     for(uxx j = 0; j <= i; j++){
-        auto & current = var_array<xinitial_length>::access(table, j);
+        auto & current = var::access(table, j);
         xfail_ifne(j, current.value);
     }
 
     auto destroy_count = i + 1;
 
     for(uxx j = 0, k = length / 2; j < k; j++){
-        var_array<xinitial_length>::pop(xref table, xref length, xref meta, alloc, free);
+        var::pop(xref table, xref length, xref meta, alloc, free);
         xfail_ifne(1, meta.counter);
         xfail_ifne(length, meta.value);
         meta.counter    = 0;
         destroy_count  -= 1;
     }
 
-    inc::var_array<xinitial_length>::clear(xref table, xref length, free);
+    var::clear(xref table, xref length, free);
     xfail_ifne(destroy_count, meta.counter);
     xfail_ifne(cost, used_bytes());
     meta.counter = 0;
 }
 
-for(uxx i = 0; i < xinitial_length * 32; i++){
+for(uxx i = 0; i < xa_initial_length * 32; i++){
     meta.value = i;
-    var_array<xinitial_length>::push(xref table, xref length, meta, alloc, free);
+    var::push(xref table, xref length, meta, alloc, free);
 }
 
 while(length != 0){
-    var_array<xinitial_length>::pop(xref table, xref length, xref meta, alloc, free);
+    var::pop(xref table, xref length, xref meta, alloc, free);
     xfail_ifne(length, meta.value);
     xfail_ifne(1, meta.counter);
     meta.counter = 0;
 }
 xfail_ifne(cost, used_bytes());
 
-#undef xinitial_length
+#undef xa_initial_length
 #endif
