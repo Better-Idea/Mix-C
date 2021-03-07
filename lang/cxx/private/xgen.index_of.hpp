66ec8f40a215d67466c2e9377c855fac6fd562fb
PowerfulCat <x4495@outlook.com>
2021-03-07 13:04:56 +0800

    添加 lang/cxx/private/xgen.index_of.hpp 生成代码

---
+++ lang/cxx/private/xgen.index_of.hpp
@@ -0,0 +1,36 @@
+// #define xa_name         index_of_first
+
+template<class final_t, class base_t, class item_t>
+struct meta : base_t {
+    using base_t::base_t;
+    using the_t         = core<item_t>;
+    using default_cmp_t = decltype(inc::default_compare<item_t>);
+
+    template<class cmp_t = default_cmp_t>
+    requires(inc::can_compare<cmp_t, item_t>)
+    uxx xa_name(item_t value, cmp_t const & compare = inc::default_compare<item_t>) const {
+        return the.xlink(xa_name, _char)(the_t{xref value, 1}, compare);
+    }
+
+    template<
+        class seq_t         = inc::initializer_list<item_t>,
+        class cmp_t         = default_cmp_t, 
+        class seq_item_t    = inc::item_origin_of<seq_t>
+    >
+    requires(
+        inc::can_unified_seqlize<seq_t> and 
+        inc::has_cast<item_t, seq_item_t> and
+        inc::is_cxx<seq_t> == false // 只要不是 cxx 字符串，就假定是字符数组
+    )
+    uxx xa_name(seq_t const & char_seq, cmp_t const & compare = inc::default_compare<item_t>) const {
+        return the.xlink(xa_name, _char)(inc::unified_seq<seq_t>{char_seq}, compare);
+    }
+
+    template<class cmp_t = default_cmp_t>
+    requires(inc::can_compare<cmp_t, item_t>)
+    uxx xa_name(final_t value, cmp_t const & compare = inc::default_compare<item_t>) const {
+        return the.xa_name(value, compare);
+    }
+};
+
+#undef  xa_name
