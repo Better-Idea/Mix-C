71acfb5d088c3a21b1bb3551f69022fb7da20fb4
PowerfulCat <x4495@outlook.com>
2021-03-07 12:49:38 +0800

    使用 itr_switch 函数代替 xitr_switch 宏，给 xitr_foreach 宏中的符号加下划线前缀避免符号冲突 @ macro/xitr_foreach.hpp

--- macro/xitr_foreach.hpp
+++ macro/xitr_foreach.hpp
@@ -4,6 +4,7 @@
 #undef  xuser
 #define xuser mixc::macro_iterator::inc
 #include"define/base_type.hpp"
+#include"macro/xexport.hpp"
 #pragma pop_macro("xuser")
 
 namespace mixc::macro_iterator{
@@ -35,41 +36,66 @@ namespace mixc::macro_iterator{
     };
 
     template<class invoke_t, class ... args_t>
-    constexpr auto detect_mode = 
+    constexpr auto itr_detect_mode = 
         // 保持判定的顺序
         full_version<invoke_t, args_t...>     == true ? itr_has_loop_control | itr_has_index :
         has_loop_control<invoke_t, args_t...> == true ? itr_has_loop_control :
         has_index<invoke_t, args_t...>        == true ? itr_has_index : 
         reduced_version<invoke_t, args_t...>  == true ? itr_reduced : itr_miss_match;
 
-    #define xitr_switch(mode,index,state,invoke,...) {                                                                 \
-        using namespace ::mixc::macro_iterator;                                                                        \
-        if constexpr (mode == (itr_reduced)) invoke(__VA_ARGS__);                                                      \
-        if constexpr (mode == (itr_has_index)) invoke(index, __VA_ARGS__), index += 1;                                 \
-        if constexpr (mode == (itr_has_loop_control)) state = invoke(__VA_ARGS__);                                     \
-        if constexpr (mode == (itr_has_loop_control | itr_has_index)) state = invoke(index, __VA_ARGS__), index += 1;  \
+    template<auto mode_v, class invoke_t, class ... args_t>
+    inline loop_t itr_switch(uxx * index, invoke_t const & invoke, args_t const & ... args){
+        if constexpr (mode_v == (itr_reduced)){
+            invoke(args...);
+            return loop_t::go_on;
+        }
+        if constexpr (mode_v == (itr_has_index)){
+            invoke(index[0], args...);
+            index[0]   += 1;
+            return loop_t::go_on;
+        }
+        if constexpr (mode_v == (itr_has_loop_control)){
+            return invoke(args...);
+        }
+        if constexpr (mode_v == (itr_has_loop_control | itr_has_index)){
+            loop_t
+            state       = invoke(index[0], args...);
+            index[0]   += 1;
+            return state;
+        }
     }
 
     #define xitr_foreach(...)                                                                                          \
     template<                                                                                                          \
-        class           iterator_t,                                                                                    \
-        auto            mode = ::mixc::macro_iterator::detect_mode<iterator_t, __VA_ARGS__>                            \
+        class           __iterator_t,                                                                                  \
+        auto            __mode = ::mixc::macro_iterator::itr_detect_mode<__iterator_t, __VA_ARGS__>                    \
     >                                                                                                                  \
-    requires(mode != ::mixc::macro_iterator::itr_miss_match)                                                           \
-    void foreach(iterator_t const & invoke) const {                                                                    \
-        foreach_template<mode, iterator_t>(invoke);                                                                    \
+    requires(__mode != ::mixc::macro_iterator::itr_miss_match)                                                         \
+    void foreach(__iterator_t const & invoke) const {                                                                  \
+        foreach_template<__mode, __iterator_t>(invoke);                                                                \
     }
 
     #define xitr_foreachx(...)                                                                                         \
     template<                                                                                                          \
-        class           iterator_t,                                                                                    \
-        can_interval    interval_t,                                                                                    \
-        auto            mode = ::mixc::macro_iterator::detect_mode<iterator_t, __VA_ARGS__>                            \
+        class           __iterator_t,                                                                                  \
+        can_interval    __interval_t,                                                                                  \
+        auto            __mode = ::mixc::macro_iterator::itr_detect_mode<__iterator_t, __VA_ARGS__>                    \
     >                                                                                                                  \
-    requires(mode != ::mixc::macro_iterator::itr_miss_match)                                                           \
-    void foreach(interval_t const & itv, iterator_t const & invoke) const {                                            \
-        foreach_template<mode, iterator_t>(invoke, itv);                                                               \
+    requires(__mode != ::mixc::macro_iterator::itr_miss_match)                                                         \
+    void foreach(__interval_t const & itv, __iterator_t const & invoke) const {                                        \
+        foreach_template<__mode, __iterator_t>(invoke, itv);                                                           \
     }
 }
 
+namespace mixc::macro_iterator::origin{
+    using mixc::macro_iterator::itr_reduced;
+    using mixc::macro_iterator::itr_has_index;
+    using mixc::macro_iterator::itr_has_loop_control;
+    using mixc::macro_iterator::itr_miss_match;
+    using mixc::macro_iterator::itr_detect_mode;
+    using mixc::macro_iterator::itr_switch;
+}
+
 #endif
+
+xexport_space(mixc::macro_iterator::origin)
