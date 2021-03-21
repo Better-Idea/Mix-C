7c29cbf819070d8475258d78acd914ea0de14059
PowerfulCat <x4495@outlook.com>
2021-03-21 21:31:40 +0800

    为 msvc 特化原子操作 @ concurrency/private/xgen.atomop.hpp

---
+++ concurrency/private/xgen.atomop.hpp
@@ -0,0 +1,47 @@
+#if defined xa_name && defined xa_func && defined xa_addition
+#include"configure.hpp"
+#include"define/base_type.hpp"
+#include"macro/xlink.hpp"
+#include<intrin.h>
+
+#ifdef xa_is_sub
+    #undef  xa_is_sub
+    #define xa_b        (0 - b)
+#else
+    #define xa_b        (b)
+#endif
+
+namespace mixc::xlink2(concurrency_lock_, xa_name){
+    extern u08 atom(u08p a, u08 b){
+        return (u08)(xlink2(xa_func, 8)(
+            (char *)(a), static_cast<char>(xa_b)
+        ) xa_addition);
+    }
+
+    extern u16 atom(u16p a, u16 b){
+        return (u16)(xlink2(xa_func, 16)(
+            (short *)(a), static_cast<short>(xa_b)
+        ) xa_addition);
+    }
+
+    extern u32 atom(u32p a, u32 b){
+        return (u32)(xa_func(
+            (long *)(a), static_cast<long>(xa_b)
+        ) xa_addition);
+    }
+
+    #if xis_os32
+    #else
+    extern u64 atom(u64p a, u64 b){
+        return (u64)(xlink2(xa_func, 64)(
+            (__int64 *)(a), static_cast<__int64>(xa_b)
+        ) xa_addition);
+    }
+    #endif
+}
+
+#undef xa_name
+#undef xa_func
+#undef xa_addition
+#undef xa_b
+#endif
