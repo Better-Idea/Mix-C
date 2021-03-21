70d79803ffe38b6d1b28068ce7a95671677f469c
PowerfulCat <x4495@outlook.com>
2021-03-21 21:56:56 +0800

    使用原子操作计数 @ memory/private/extern.allocator.hpp

--- memory/private/extern.allocator.hpp
+++ memory/private/extern.allocator.hpp
@@ -4,38 +4,42 @@
 
 #define xuser mixc::memory_alloctor::inc
 #include"configure.hpp"
-#include<malloc.h>
+#include"concurrency/lock/atom_add.hpp"
+#include"concurrency/lock/atom_sub.hpp"
+#include"concurrency/lock/atom_load.hpp"
 
 #if xuse_libc_malloc
+#include<malloc.h>
 
 namespace mixc::memory_alloctor{
-    inline thread_local uxx pused_bytes      = 0;
-    inline thread_local uxx pneed_free_count = 0;
+    inline uxx pused_bytes      = 0;
+    inline uxx pneed_free_count = 0;
 
     extern voidp tiny_alloc(uxx bytes){
-        pused_bytes        += bytes;
-        pneed_free_count   += 1；
+        inc::atom_add(xref pused_bytes, bytes);
+        inc::atom_add(xref pneed_free_count, 1);
         return ::malloc(bytes);
     }
 
     extern void tiny_free(voidp ptr, uxx bytes){
-        pused_bytes        -= bytes;
-        pneed_free_count   -= 1；
+        inc::atom_sub(xref pused_bytes, bytes);
+        inc::atom_sub(xref pneed_free_count, 1);
         ::free(ptr);
     }
 }
 
 namespace mixc::memory_alloctor::origin{
     extern uxx used_bytes(){
-        return pused_bytes;
+        return inc::atom_load(xref pused_bytes);
     }
 
     extern uxx need_free_count(){
-        return pneed_free_count;
+        return inc::atom_load(xref pneed_free_count);
     }
 
     extern uxx alive_pages(){
-        return pused_bytes / 4096 + (pused_bytes % 4096 != 0);
+        auto bytes = inc::atom_load(xref pused_bytes);
+        return bytes / 4096 + (bytes % 4096 != 0);
     }
 }
 #else
@@ -43,7 +47,7 @@ namespace mixc::memory_alloctor::origin{
 #include"memory/private/tiny_allocator.hpp"
 
 namespace mixc::memory_alloctor{
-    thread_local inline inc::tiny_allocator mem;
+    inline thread_local inc::tiny_allocator mem;
 
     extern voidp tiny_alloc(uxx bytes){
         return mem.alloc(bytes);
