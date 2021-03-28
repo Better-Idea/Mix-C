df0779af4c2cc77e5bb9ba05c2fb24628fc40c12
PowerfulCat <x4495@outlook.com>
2021-03-28 03:31:52 +0000

    让 gc 队列响应阈值可配置

--- configure/switch.hpp
+++ configure/switch.hpp
@@ -6,5 +6,8 @@
 #define xuse_xdebug_fail            1
 #define xuse_xdebug_short_path      1
 
-#define xuse_libc_malloc            1
+#define xuse_libc_malloc            0
+
+#define xgc_queue_threshold         (xgc_queue_depth / 64)
+
 #endif
