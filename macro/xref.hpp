61551728f58444c3d0cc8f28850d91b6cb37e003
PowerfulCat <x4495@outlook.com>
2021-06-03 21:19:47 +0800

    完善 @ macro/xref.hpp

--- macro/xref.hpp
+++ macro/xref.hpp
@@ -1,2 +1,24 @@
 #pragma once
-#define xref(...)       ((decltype(__VA_ARGS__) *) (& (void* &)(decltype(__VA_ARGS__) &)(__VA_ARGS__)))
+
+namespace mixc::macro_xref{
+    template<class type_t> struct meta{
+        using type = type_t;
+    };
+
+    template<class type_t> struct meta<type_t &>{
+        using type = type_t;
+    };
+
+    template<class type_t> struct meta<type_t const &>{
+        using type = type_t;
+    };
+
+    template<class type_t>
+    using remove_ref = typename meta<type_t>::type;
+}
+
+#define xref(...)                                                                               \
+    (                                                                                           \
+        (::mixc::macro_xref::remove_ref<decltype(__VA_ARGS__)> *)                               \
+        (& (void* &)(::mixc::macro_xref::remove_ref<decltype(__VA_ARGS__)> &)(__VA_ARGS__))     \
+    )
