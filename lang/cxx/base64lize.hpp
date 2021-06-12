f4a027c8a21ed586f7b7301b4be11520ade1521a
PowerfulCat <x4495@outlook.com>
2021-06-12 22:22:40 +0800

    新增 base64 编码逻辑 @ lang/cxx/base64lize.hpp

---
+++ lang/cxx/base64lize.hpp
@@ -0,0 +1,106 @@
+#ifndef xusing_lang_cxx
+#include"lang/private/cxx.hpp"
+#endif
+
+#ifndef xpack_lang_cxx_base64lize
+#define xpack_lang_cxx_base64lize
+#pragma push_macro("xuser")
+#pragma push_macro("xusing_lang_cxx")
+#undef  xusing_lang_cxx
+#undef  xuser
+#define xuser mixc::lang_cxx_base64lize::inc
+#include"define/base_type.hpp"
+#include"interface/can_alloc.hpp"
+#include"interface/seqptr.hpp"
+#include"interface/unified_seq.hpp"
+#include"lang/cxx/private/base64.hpp"
+#include"lang/cxx.hpp"
+#pragma pop_macro("xusing_lang_cxx")
+#pragma pop_macro("xuser")
+
+namespace mixc::lang_cxx_base64lize::lookup{
+    using namespace inc::lookup;
+}
+
+namespace mixc::lang_cxx_base64lize{
+    template<class item_t>
+    struct core {
+        using the_t = inc::cxx<item_t>;
+
+        template<class seq_t, class alloc_t>
+        auto base64lize(seq_t const & seq, asciis lut, alloc_t const & alloc){
+            auto width          = seq.length() * 4;
+            auto size           = width / 3;
+            auto total_length   = ~0x3 & (size + 3);
+            auto buffer         = alloc(total_length);
+            auto temp           = u32(0);
+            auto len            = seq.length();
+            auto i              = uxx(0);
+            auto j              = uxx(0);
+            auto k              = uxx(0);
+            auto l              = uxx(0);
+
+            while(i + 3 <= len){
+                temp            = temp << 8 | u08(seq[i++]);
+                temp            = temp << 8 | u08(seq[i++]);
+                temp            = temp << 8 | u08(seq[i++]);
+                buffer[j++]     = lut[(temp >> 18) & 0x3f];
+                buffer[j++]     = lut[(temp >> 12) & 0x3f];
+                buffer[j++]     = lut[(temp >> 06) & 0x3f];
+                buffer[j++]     = lut[(temp >> 00) & 0x3f];
+            }
+
+            if (i != len){
+                for(temp = 0, k = i; i < len; ){
+                    temp        = temp << 8 | u08(seq[i++]);
+                }
+
+                for(temp <<= (4 - (i - k)) * 8, l = j + i - k + 1; j < l; ){
+                    buffer[j++] = lut[temp >> (32 - 6)];
+                    temp      <<= 6;
+                }
+
+                if (lut != lookup::base64url) while(j & 0x3){
+                    buffer[j++] = '=';
+                }
+            }
+
+            the = the_t{ buffer, j };
+        }
+    };
+
+    template<class final_t, class base_t, class item_t>
+    struct meta : base_t{
+        using base_t::base_t;
+        using the_t = core<item_t>;
+
+        template<class seq_t, class alloc_t>
+        requires(
+            inc::can_unified_seqlize<seq_t> and
+            inc::can_alloc<alloc_t, item_t>
+        )
+        meta(seq_t const & seq, inc::base64_t mode, alloc_t const & alloc){
+            auto source     = inc::unified_seq<seq_t>(seq);
+            the.base64lize(source, mode, alloc);
+        }
+
+        template<class alloc_t>
+        requires(inc::can_alloc<alloc_t, item_t>)
+        meta(item_t const * seq, inc::base64_t mode, alloc_t const & alloc){
+            auto source     = inc::seqptr<item_t>(base_t{seq});
+            the.base64lize(source, mode, alloc);
+        }
+    };
+}
+
+#endif
+
+namespace mixc::lang_cxx_base64lize::xuser{
+    template<class final_t, class item_t>
+    using cxx = meta<final_t, xusing_lang_cxx::cxx<final_t, item_t>, item_t>;
+}
+
+xexport_space(mixc::lang_cxx_base64lize::lookup)
+
+#undef  xusing_lang_cxx
+#define xusing_lang_cxx ::mixc::lang_cxx_base64lize::xuser
