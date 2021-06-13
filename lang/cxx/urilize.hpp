0f2da85090f8d5861f215fc1da28e2a6c39863e4
PowerfulCat <x4495@outlook.com>
2021-06-13 17:57:43 +0800

    命名调整 @ lang/cxx/urilize.hpp

--- lang/cxx/urilize.hpp
+++ lang/cxx/urilize.hpp
@@ -13,6 +13,7 @@
 #include"interface/can_alloc.hpp"
 #include"interface/seqptr.hpp"
 #include"interface/unified_seq.hpp"
+#include"lang/cxx/private/uri.hpp"
 #include"lang/cxx/encode_first_as.hpp"
 #include"lang/cxx.hpp"
 #include"lang/wxx/length_if_as_utf8.hpp"
@@ -21,50 +22,11 @@
 #include"meta/char_type.hpp"
 #include"meta/is_base_char.hpp"
 #include"meta/unsigned_type.hpp"
-#include"utils/bits_bind.hpp"
 #pragma pop_macro("xusing_lang_cxx")
 #pragma pop_macro("xuser")
 
-namespace mixc::lang_cxx_urilize::inc{
-    struct url_t{
-    private:
-        enum{
-            bwidth = sizeof(uxx) * 8,
-        };
-
-        uxx items[128 / bwidth];
-    public:
-        constexpr url_t(url_t const &) = default;
-        constexpr url_t():
-            items{}{
-
-            for(auto & item : items){
-                item            = uxx(-1);
-            }
-        }
-
-        inc::bits_bind<uxx> operator[](uxx i) const {
-            return inc::bits_bind<uxx>{ xref(items[i / bwidth]), i % bwidth };
-        }
-
-        friend constexpr url_t make_url_lut();
-    };
-
-    constexpr u08 url_lut[] = { 33,39,40,41,42,45,46,48,49,50,51,52,53,54,55,56,57,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,95,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,126 };
-
-    constexpr url_t make_url_lut(){
-        url_t result{};
-
-        for(auto i : url_lut){
-            result.items[i / url_t::bwidth]
-                           ^= uxx(1) << (i % url_t::bwidth);
-        }
-        return result;
-    }
-}
-
 namespace mixc::lang_cxx_urilize::lookup{
-    constexpr inc::url_t url = inc::make_url_lut();
+    using namespace inc::lookup;
 }
 
 namespace mixc::lang_cxx_urilize{
@@ -73,7 +35,7 @@ namespace mixc::lang_cxx_urilize{
         using the_t = inc::cxx<item_t>;
 
         template<class seq_t, class alloc_t>
-        auto urilize(seq_t const & seq, inc::url_t const & need_convert, alloc_t const & alloc){
+        auto urilize(seq_t const & seq, inc::uri_t const & need_convert, alloc_t const & alloc){
             using char_t            = inc::char_type<decltype(seq[0])>;
             using unsigned_t        = inc::unsigned_type<char_t>;
             auto total_length       = uxx(0);
@@ -131,14 +93,14 @@ namespace mixc::lang_cxx_urilize{
             inc::can_unified_seqlize<seq_t> and
             inc::can_alloc<alloc_t, item_t>
         )
-        meta(seq_t const & seq, inc::url_t mode, alloc_t const & alloc){
+        meta(seq_t const & seq, inc::uri_t const & mode, alloc_t const & alloc){
             auto source     = inc::unified_seq<seq_t>(seq);
             the.urilize(source, mode, alloc);
         }
 
         template<inc::is_base_char base_char_t, class alloc_t>
         requires(inc::can_alloc<alloc_t, item_t>)
-        meta(base_char_t const * seq, inc::url_t mode, alloc_t const & alloc){
+        meta(base_char_t const * seq, inc::uri_t const & mode, alloc_t const & alloc){
             auto source     = inc::seqptr<base_char_t>(inc::cxx<base_char_t>{seq});
             the.urilize(source, mode, alloc);
         }
