3e22b1fa5d290df4f1780c87a518585d5c6a24e8
PowerfulCat <x4495@outlook.com>
2021-08-16 00:06:55 +0800

    GC 问题修复 + 新增功能 @ gc/private/background.hpp - 暂时使用动态 GC 队列代替原有的静态队列 - 暂时不使用 hashmap 保存经历过的节点，而在对象头部新增一个字段专门存储额外的遍历信息 - 已经遍历过的对象或标记为待释放的对象将直接跳过 - 新增对象释放线程，用于协助可释放对象筛选线程处理内存释放操作

--- gc/private/background.hpp
+++ gc/private/background.hpp
@@ -3,6 +3,7 @@
 #pragma push_macro("xuser")
 #undef  xuser
 #define xuser mixc::gc_private_background::inc
+#include"define/base_type.hpp"
 #include"concurrency/lock/atom_fetch_add.hpp"
 #include"concurrency/lock/atom_add.hpp"
 #include"concurrency/lock/atom_load.hpp"
@@ -13,110 +14,71 @@
 #include"concurrency/thread.hpp"
 #include"concurrency/thread_self.hpp"
 #include"configure/buffer.hpp"
-#include"docker/hashmap.hpp"
 #include"gc/private/token.hpp"
+#include"macro/xdefer.hpp"
 #include"macro/xexport.hpp"
+#include"macro/xinit.hpp"
 #include"macro/xref.hpp"
-#include"macro/xstruct.hpp"
-#include"utils/allocator.hpp"
+#include"utils/memory.hpp"
 #pragma pop_macro("xuser")
 
 namespace mixc::gc_private_background::origin{
-    xstruct(
-        xname(gc_info_t)
-    )
-        uxx can_arrive_root : 1;
-        uxx visited         : sizeof(uxx) * 8 - 1;
-
-        gc_info_t() : 
-            can_arrive_root(0), visited(0){}
-
-        operator uxx (){
-            return uxxp(this)[0];
-        }
-    $
-
-    using visited_ptr_t     = voidp;
-    using gc_map_t          = inc::hashmap<visited_ptr_t, gc_info_t>;
     using release_invoke    = void(*)(voidp mem);
 
-    xstruct(
-        xname(release_pair)
-    )
+    struct release_pair{
         inc::token *        mem;
         release_invoke      release;
-    $
-
-    xstruct(
-        xname(free_node)
-    )
-        free_node *         previous;
-        inc::memory_size    bytes;
-    $;
-
-    xstruct(
-        xname(gc_exit_raiser),
-        xpubf(exit, bool)
-    )
-        ~gc_exit_raiser();
-    $;
-
-    using free_nodep            = free_node *;
+    };
 
     constexpr uxx gc_queue_depth_mask   = xgc_queue_depth - 1;
     static_assert((gc_queue_depth_mask & ~gc_queue_depth_mask) == 0, "gc_queue_depth_mask not aligned");
 
-    // 控制 gc_thread 中的[内析构]不再推送给自己，而是直接执行
-    // 该值只会在 gc_thread 中设置，其他线程读到的都是 false
+    // 控制 g_gc_filter_thread 中的[内析构]不再推送给自己，而是直接执行
+    // 该值只会在 g_gc_filter_thread 中设置，其他线程读到的都是 false
     inline thread_local bool    l_in_release;
     inline thread_local voidp   l_wait_mem;
     inline thread_local uxx     l_i_wait;
 
-    // 保存 gc 经过的节点
-    inline gc_map_t             gc_map;
+    using free_nodep = inc::token::free_nodep;
+    using release_pairp = release_pair *;
 
-    // 当前[潜质类型]的出度入度差
-    // 如果该[潜质类型]可以通过子节点再次回到自身且 degree_dvalue 为 0 则表示可以进行[内析构]
-    inline uxx                  degree_dvalue;
+    // 释放列表，先添加到列表中
+    inline free_nodep           g_free_list;
 
-    // gc 遍历时的根节点
-    inline visited_ptr_t        root;
-
-    // 释放列表，先添加到列表中，后批量释放
-    inline free_nodep           free_list;
+    // 等待 GC 同步的线程数
+    inline uxx                  g_pending;
 
     // gc 缓冲队列
     // 前台推送，后台释放
-    inline release_pair         gc_que[xgc_queue_depth];
+    inline release_pair *       g_gc_que;
 
-    // 存放索引，当前推送的内容存放到 gc_que 的哪一个元素中
-    inline uxx                  i_push_gc_que;
+    // 存放索引，当前推送的内容存放到 g_gc_que 的哪一个元素中
+    inline uxx                  g_i_push_gc_que;
 
-    // 取出索引，gc_thread 处理了多少 gc_que 中的请求
-    inline uxx                  i_pop_gc_que;
+    // 取出索引，gc_thread 处理了多少 g_gc_que 中的请求
+    inline uxx                  g_i_pop_gc_que;
 
-    inline uxx                  pending_count;
+    inline uxx                  g_gc_exit;
 
     // gc 退出触发器，当全局析构时触发
-    inline gc_exit_raiser       gc_signal;
-
-    inline inc::thread          gc_thread;
-
-    inline gc_exit_raiser::~gc_exit_raiser() {
-        for(inc::atom_store(xref(exit), true), gc_thread.resume();
-            inc::atom_load(xref(gc_signal.exit)); 
+    xdefer_global{
+        for(inc::atom_store(xref(g_gc_exit), true);
+            inc::atom_load(xref(g_gc_exit)); 
             inc::thread_self::yield()){
         }
-    }
+    };
+
+    inline inc::thread          g_gc_filter_thread;
+    inline inc::thread          g_gc_release_thread;
 
     inline void gc_execute(){
-        while(inc::atom_load(xref(gc_signal.exit)) == false){
-            auto i_push         = inc::atom_load(xref(i_push_gc_que));
-            auto i_pop          = i_pop_gc_que;
-            auto dis            = i_push - i_pop_gc_que;
+        while(inc::atom_load(xref(g_gc_exit)) == false){
+            auto i_push         = inc::atom_load(xref(g_i_push_gc_que));
+            auto i_pop          = g_i_pop_gc_que;
+            auto dis            = i_push - g_i_pop_gc_que;
 
             if (dis >= xgc_queue_depth) {
-                i_push          = i_pop_gc_que + xgc_queue_depth;
+                i_push          = g_i_pop_gc_que + xgc_queue_depth;
             }
 
             if (dis == 0){
@@ -125,7 +87,7 @@ namespace mixc::gc_private_background::origin{
             }
 
             // 等待超时就响应一下
-            if (inc::atom_load(xref(pending_count)) == 0 and dis < xgc_queue_threshold){
+            if (inc::atom_load(xref(g_pending)) == 0 and dis < xgc_queue_threshold){
                 inc::thread_self::suspend(64/*ms*/);
             }
 
@@ -144,7 +106,7 @@ namespace mixc::gc_private_background::origin{
             // 批量让引用计数器减一
             while(i != i_begin){
                 i              -= 1;
-                item            = xref(gc_que[i & gc_queue_depth_mask]);
+                item            = xref(g_gc_que[i & gc_queue_depth_mask]);
 
                 // mem 为 nullptr 指示当前元素在对应的线程中还未完成设置
                 while(true){
@@ -165,8 +127,15 @@ namespace mixc::gc_private_background::origin{
                 }
             }
 
-            while(i != i_end){
-                item            = xref(gc_que[i & gc_queue_depth_mask]);
+            // 如果 g_gc_release_thread 还未接收上一个 g_free_list，这里就等待一下
+            // 大部分时候 g_gc_release_thread 的任务都比 g_gc_filter_thread 轻松且更快完成
+            // 所以很少有 filter 等待 release 的情况
+            if (g_free_list = inc::atom_load(xref(g_free_list)); g_free_list){
+                inc::thread_self::suspend();
+            }
+
+            for(inc::token::new_term(); i != i_end;) {
+                item            = xref(g_gc_que[i & gc_queue_depth_mask]);
                 i              += 1;
                 i_pop          += 1;
                 release         = item->release;
@@ -174,58 +143,48 @@ namespace mixc::gc_private_background::origin{
 
                 // 不执行重复无效的遍历
                 // 只有该节点是在本轮 gc 中第一次访问，才可以进行接下来的[外析构]
-                if (mem and mem->in_gc_queue()){
+                if (mem and not mem->is_visited() and not mem->under_release()){
                     release(mem);
+                }
 
-                    // 后设置 in_gc_queue = false
-                    // {内析构}会根据 in_gc_queue 是否等于 true 来执行释放操作
-                    // 在执行[内析构]时会将 in_gc_queue 置为 false
-                    // 如果经过一次 gc 发现 mem 不可以释放([内析构]没有执行)
-                    // 则此处需要再复位一下 in_gc_queue，以便下一轮 gc 可以正确响应
+                if (mem) {
                     mem->in_gc_queue(false);
                 }
 
-                // 需要先置空，再设置 i_pop_gc_que
+                // 需要先置空，再设置 g_i_pop_gc_que
                 // 只有下一次读到 mem 值不为 nullptr 时才响应
                 inc::atom_store(xref(item->mem), nullptr);
 
-                // 让 gc_que 立即腾出可用的空间
-                inc::atom_store(xref(i_pop_gc_que), i_pop);
+                // 让 g_gc_que 立即腾出可用的空间
+                inc::atom_store(xref(g_i_pop_gc_que), i_pop);
             }
 
-            // 然后做释放内存等耗时操作
-            while(free_list != nullptr){
-                current         = free_list;
-                free_list       = free_list->previous;
-                inc::free(current, current->bytes);
-            }
-            gc_map.clear();
+            inc::atom_store(xref(g_free_list), g_free_list);
+            g_gc_release_thread.resume();
         }
-
-        inc::atom_store(xref(gc_signal.exit), false);
+        g_gc_release_thread.resume();
     }
 
     inline void gc_sync(){
-        inc::atom_add(xref(pending_count), 1);
-        gc_thread.resume();
+        inc::atom_add(xref(g_pending), 1);
+        g_gc_filter_thread.resume();
 
         // 如果 gc 还未处理完该 mem 就自旋等待
-        while(inc::atom_load(xref(gc_que[l_i_wait].mem)) == l_wait_mem){
+        while(inc::atom_load(xref(g_gc_que[l_i_wait].mem)) == l_wait_mem){
             inc::thread_self::yield();
         }
-
-        inc::atom_sub(xref(pending_count), 1);
+        inc::atom_sub(xref(g_pending), 1);
     }
 
     // 注意：mem 不能为 nullptr
     inline void gc_push(inc::token * mem, release_invoke release){
-        uxx i_push              = inc::atom_fetch_add(xref(i_push_gc_que), 1);
+        uxx i_push              = inc::atom_fetch_add(xref(g_i_push_gc_que), 1);
         uxx i_pop               = 0;
         uxx i                   = 0;
         uxx dis                 = 0;
 
         while(true){
-            i_pop               = inc::atom_load(xref(i_pop_gc_que));
+            i_pop               = inc::atom_load(xref(g_i_pop_gc_que));
             i                   = i_push & gc_queue_depth_mask;
             dis                 = i_push - i_pop;
 
@@ -243,13 +202,53 @@ namespace mixc::gc_private_background::origin{
         l_wait_mem              = mem;
 
         // 后设置 mem，该字段不为 nullptr 指示当前元素设置完成
-        inc::atom_store(xref(gc_que[i].release), release);
-        inc::atom_store(xref(gc_que[i].mem), mem);
+        inc::atom_store(xref(g_gc_que[i].release), release);
+        inc::atom_store(xref(g_gc_que[i].mem), mem);
 
         if (i == xgc_queue_threshold){
-            gc_thread.resume();
+            g_gc_filter_thread.resume();
         }
     }
+
+    xinit(inc::the_gc_filter){
+        uxx bytes               = xgc_queue_depth * sizeof(release_pair);
+        g_gc_que                = release_pairp(inc::memory::malloc(bytes));
+
+        for(uxx i = 0; i < xgc_queue_depth; i++){
+            xnew(& g_gc_que[i]) release_pair();
+        }
+
+        g_gc_filter_thread      = inc::thread(xdetached{
+            gc_execute();
+        });
+    };
+
+    xinit(inc::the_gc_release){
+        g_gc_release_thread     = inc::thread(xdetached{
+            while(true){
+                // 如果遇到退出标志，可以立即退出，因为纯内存释放操作可以不用在系统退出时执行
+                if (inc::thread_self::suspend();
+                    inc::atom_load(xref(g_gc_exit))){
+                    break;
+                }
+
+                auto free_list  = free_nodep{};
+                auto current    = free_nodep{};
+                free_list       = inc::atom_swap(xref(g_free_list), free_list);
+                g_gc_filter_thread.resume();
+
+                // 然后做释放内存等耗时操作
+                while(free_list != nullptr){
+                    current     = free_list;
+                    free_list   = free_list->prev();
+                    inc::memory::free(current, inc::memory::size{current->bytes()});
+                }
+            }
+
+            // 回应一下主线程，退出以及完成
+            inc::atom_store(xref(g_gc_exit), false);
+        });
+    };
 }
 
 #endif
