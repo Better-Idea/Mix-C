50b3910e6d6e965d7848a8356f6a5746d455c25c
PowerfulCat <x4495@outlook.com>
2021-08-22 23:48:15 +0800

    新增两个常量 @ configure/buffer.hpp

--- configure/buffer.hpp
+++ configure/buffer.hpp
@@ -1,7 +1,10 @@
 #ifndef pack_configure_buffer
 #define pack_configure_buffer
 
-// gc 队列元素个数（需要是 2^n）
-#define xgc_queue_depth             65536
+// gc filter 队列元素个数（需要是 2^n）
+#define xgc_filter_queue_depth          (65536)
 
+#define xgc_free_list_queue_depth       (8)
+
+#define xthread_limit                   (65536)
 #endif
